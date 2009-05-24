d47d0945c37d077fa8ce2fc3516faeabd7d1213f
wdconinc <wdconinc@b9700a98-9937-0410-b777-f318d6eb5bff>
2009-05-24 19:08:47 +0000

    Cleanup of tracking header (obsolete members removed), removed magical array dimensions.
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@182 b9700a98-9937-0410-b777-f318d6eb5bff

--- Tracking/include/tracking.h
+++ Tracking/include/tracking.h
@@ -58,19 +58,19 @@ class Bridge {
   public:
 
     double xOff, yOff, ySOff;	/*!< Offset expected from lookup */
-    double ySlopeMatch;		/*!< BENDING: slope match in Y   */
+    double ySlopeMatch;		/*!< Bending: slope match in Y   */
     double yMatch;		/*!< match in y */
     double ySMatch;		/*!< matching in Yslope */
     double xMatch;		/*!< match in x */
     double Momentum;		/*!< momentum  */
-    Hit    *hits[9];		/*!< matching points in magnet */
+    Hit    *hits;		/*!< matching hits */
+    QwHitContainer qwhits;	/*!< matching hits */
     //pointList *point;
     int ID;
 
   private:
 
 };
-
 
 
 //____________________________________________
@@ -84,11 +84,11 @@ class Bridge {
 class PartTrack {
 
   public:
-    double x,y;			/*!< position at magnet_center */
-    double mx,my;		/*!< slope in x and y       */
+    double x, y;		/*!< position at MAGNET_CENTER */
+    double mx, my;		/*!< slope in x and y       */
     double Cov_Xv[4][4];	/*!< covariance matrix      */
     double chi;			/*!< combined chi square    */
-    QwTrackingTreeLine *tline[3];		/*!< tree line in u v and x */
+    QwTrackingTreeLine *tline[kNumDirections];		/*!< tree line in u v and x */
     //Cluster  *cluster;	/*!< cluster pointed to by track */
     double  ECalor;		/*!< energy in this cluster */
     double  clProb;		/*!< prob. that this cluster belongs to track */
@@ -96,8 +96,6 @@ class PartTrack {
     double  pathlenslo;		/*!< pathlength slope  */
     PartTrack *next;		/*!< linked list */
     Bridge *bridge;		/*!< magnetic field bridging */
-    //enum Emethod method;		/*!< reconstruction method used */
-    //struct _trdcollect *trdcol;	/*!< trd information */
     int    Used;			/*!< used (part of a Track)  */
     int    nummiss;		/*!< missing hits */
     int    numhits;		/*!< used hits */
@@ -106,6 +104,7 @@ class PartTrack {
 
     int    triggerhit;		/*!< Did this track pass through the trigger?*/
     double trig[2];		/*!< x-y position at trigger face */
+
     int cerenkovhit;		/*!< Did this track pass through the cerenkov bar? */
     double cerenkov[2];		/*!< x-y position at Cerenkov bar face */
 
@@ -122,16 +121,16 @@ class Track {
 
   public:
 
-    double ZVx,TVx;		/*!< Vertex posistion in Z and transverse */
+    double ZVx, TVx;		/*!< Vertex posistion in Z and transverse */
     double The, Phi;		/*!< theta and phi of track */
     int    AngleCorr;		/*!< true if theta and phi are corrected for
 				     the holding field */
     double rDXSl;		/*!< bending in the magnet (x direction) */
     double chi;			/*!< combined chi square */
 
-    double P, ECalor;		/*!< spect. and calorimeter Energy */
+    double P;			/*!< spectrometer and calorimeter Energy */
 
-    double XBj, Y, Q2, W2, Nu;	/*!< kinematix */
+    double XBj, Y, Q2, W2, Nu;	/*!< kinematics */
 
     int    RecoEvent;		/*!< track from mcHits instead of digits */
 
@@ -146,19 +145,11 @@ class Track {
     PartTrack *front;		/*!< front partial track */
     PartTrack *back;		/*!< back partial track */
 
-
     Vertex    *vertex;		/*!< vertex of this track */
-
-    //HodoscopeInfo H0;		/*!< H0        data associated with this track*/
-    //HodoscopeInfo Hodoscope;	/*!< Hodoscope data associated with this track*/
-    //HodoscopeInfo Preshower;	/*!< Preshower data associated with this track*/
-    //CerenkovInfo  Cerenkov;	/*!< Cerenkov  data associated with this track*/
 
     bool   Used;		/*!< used (part of usedTrack list) */
     bool   inBounds;		/*!< lookup table was usable */
-    //enum Emethod method;	/*!< treesearch method in front track */
 
-    //particleID PId;		/*!< PID info */
     int    iCharge;		/*!< charge of particle */
 
     int ID;
@@ -179,15 +170,15 @@ class Event {
   public:
 
     int ID;
-    QwTrackingTreeLine  *treeline[2][3][4][4];	/*!< [upper/lower][region][type][u/v/x/y] */
-    //Cluster   *cluster[2];		/*!< [upper/lower] */
-    //Cluster   *lumicluster[2];	/*!< [right/left] */
-    PartTrack *parttrack[2][3][4];	/*!< [meth][upper/lower][forw/back] */
-    Track     *track[2];		/*!< [upper/lower] */
-    Track     *usedtrack[2];		/*!< applying chi^2 cuts to the above list */
+    QwTrackingTreeLine  *treeline[kNumPackages][kNumRegions][kNumTypes][kNumDirections];	/*!< [upper/lower][region][type][u/v/x/y] */
+    PartTrack *parttrack[kNumPackages][kNumRegions][kNumTypes];	/*!< [meth][upper/lower][forw/back] */
+    Track     *track[kNumPackages];		/*!< [upper/lower] */
+    Track     *usedtrack[kNumPackages];		/*!< applying chi^2 cuts to the above list */
     Vertex    *vertex;			/*!< all vertices */
+
+    Event     *next;			/*!< next event */
+
     int       goodguess;		/*!< guessed right in electron PID */
-    struct    _event *next;		/*!< next event */
 
   private:
 
