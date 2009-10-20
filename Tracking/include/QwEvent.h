e1c579a1a8e7c8bea89f4d2ff5e7f1ce544638b0
wdconinc <wdconinc@b9700a98-9937-0410-b777-f318d6eb5bff>
2009-10-20 05:55:02 +0000

    Another attempt at making a 'transparent', browsable TTree with tracking event data...  It's not plugged into any release-quality executable yet but it sort-of works here on my laptop.  A cleaned-up port of qwsimtracking with demo should follow soon, time permitting.
    The structure is as follows:
    
    TFile file (could have a separate tree for slow or raw hits data)
    |
    TTree tree
    |
    |__ TBranch events of QwEvent (may be split, a.k.a. row-wise or column-wise)
        |
        |__ TClonesArray of QwHits (all QwHits have the same size)
        |  [so far identical to what is in QwHitRootContainer now]
        |
        |__ QwEventHeader with event number, time, etc...
        |
        |__ TClonesArray of QwPartialTrack (variable size objects...)
            |
            |__ TClonesArray of QwTreeLine (variable size objects...)
                | '
                | '
                |_'_ std::vector<QwHit> (I have no clue why TClonesArray
                  '                                 doesn't work here)
                  '
                  '
    This is basically what you see in the ROOT TBrowser, with some other
    leafs floating around that I am evaluating for performance.  To some
    extent you can do the double-clicking (yay!), BUT there is apparently
    an inherent limitation in TTreeFormula that prevents this clicking
    from working more than 2 levels deep.  The deepest QwHit list (in a
    tree line) can not be easily evaluated.  And the plan was in fact to
    go a level deeper by putting a layer TClonesArray of QwTrack between
    the QwEvent level and the QwPartialTrack level, so we have to come up
    with a solution or just dump the deep double-clicking.  (Limit of
    TTreeFormula is indicated with dashed line.)
    
    In concreto, this is what you cannot do:
    $ tree->Draw("events.fQwPartialTracks.fQwTreeLines.fQwHits.fResidual")
    
    That said, macros that load the shared object libraries of the class
    definitions should just work, and compiled code should also work.
    
    Some info about the TClonesArray implementation.  It actually keeps
    a static TClonesArray in memory and the TClonesArray that gets written
    is a subset of this static TClonesArray.  This has as big advantage
    that you don't have to new/delete any objects (expensive), but it
    requires that your objects have the same size so you can jump to a
    particular one with a simple sizeof().  QwHits are simple enough that
    they are always the same size.  Anything else has variable numbers of
    'things' inside which makes them not as efficient for TClonesArrays.
    But (nowadays) TclonesArray knows how to handle variable size objects.
    
    Other options are:
    - TObjArray: does not support browsing because it contains anything,
      even apples and oranges in the same array (well, pointers to any
      object that inherits from a TObject, to be precise).  Expensive in
      terms of new/delete.
    - TRefArray: stores references only.  This could be useful if we would
      want to store references to the main (raw) hitlist in, for example,
      the tree lines.  However, we will likely write to the hits, and
      it is not nice to change the raw hits during tracking (especially
      because we might want to evaluate two settings simultaneously, and
      that would become difficult).
    
    Probably related to the limitation in TTreeFormula, the browser also
    doesn't seem to understand TClonesArrays that are deeper than two
    levels.  However, std::vector works there (and might work everywhere
    else too, what was the problem again with writing QwHitContainers
    directly?).
    
    
    I had to add a LinkDef for dictionary generation.  They should go in the
    include directory, not in the dictionary directory.  You will need to do
    a make config to get your dependencies right.
    
    
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@463 b9700a98-9937-0410-b777-f318d6eb5bff

--- Tracking/include/QwEvent.h
+++ Tracking/include/QwEvent.h
@@ -1,8 +1,8 @@
 /**
  * \class	QwEvent	QwEvent.h
- * \brief Contains a tracked event, including information from hits to tracks.
+ * \brief Contains a tracked event, i.e. all information from hits to tracks
  *
- * A QwEvent contains all event information, from hits via partial track to
+ * A QwEvent contains all event information, from hits over partial track to
  * complete tracks.  It serves as the final product of the tracking code.
  *
  * \ingroup QwTrackingAnl
@@ -12,43 +12,176 @@
 #ifndef QWEVENT_H
 #define QWEVENT_H
 
-#include <list>
+// C and C++ headers
 #include <vector>
 
+// ROOT headers
 #include "TObject.h"
+#include "TClonesArray.h"
+#include "TRefArray.h"
+#include "TObjArray.h"
 
+// Qweak headers
 #include "QwTypes.h"
+#include "QwTrackingTreeLine.h"
 
 // Forward declarations
+class QwHit;
+class QwHitContainer;
 class QwTrackingTreeLine;
 class QwPartialTrack;
 class QwTrack;
 class QwVertex;
 
+
+class QwEventHeader: public TObject {
+
+  private:
+
+    UInt_t fRunNumber;		///< Run number
+    //
+    ULong_t fEventNumber;	///< Event number
+    ULong_t fEventTime;		///< Event time (unix time? some time in boost?)
+    //
+    UInt_t fEventType;		///< Event type (probably bit pattern)
+    UInt_t fEventTrigger;	///< Event trigger (probably bit pattern)
+    //
+    EQwHelicity fBeamHelicity;	///< Beam helicity (from MPS pattern phase)
+
+  public:
+
+    QwEventHeader(){};
+    virtual ~QwEventHeader(){};
+
+    void SetRunNumber(const UInt_t runnumber) { fRunNumber = runnumber; };
+    UInt_t GetRunNumber() const { return fRunNumber; };
+
+    void SetEventNumber(const ULong_t eventnumber) { fEventNumber = eventnumber; };
+    ULong_t GetEventNumber() const { return fEventNumber; };
+
+    void SetEventTime(const ULong_t eventtime) { fEventTime = eventtime; };
+    ULong_t GetEventTime() const { return fEventTime; };
+
+    void SetEventType(const UInt_t eventtype) { fEventType = eventtype; };
+    UInt_t GetEventType() const { return fEventType; };
+
+    void SetEventTrigger(const UInt_t eventtrigger) { fEventTrigger = eventtrigger; };
+    UInt_t GetEventTrigger() const { return fEventTrigger; };
+
+    void SetBeamHelicity(const EQwHelicity helicity) { fBeamHelicity = helicity; };
+    EQwHelicity GetBeamHelicity() const { return fBeamHelicity; };
+
+  ClassDef(QwEventHeader,1);
+};
+
+
 class QwEvent: public TObject {
 
   public:
 
+    // Event header
+    QwEventHeader* fEventHeader;
+
+    // Hits
+    #define QWEVENT_MAX_NUM_HITS 1000
+    Int_t fNQwHits; ///< Number of QwHits in the array
+    TClonesArray        *fQwHits; ///< Array of QwHits
+    static TClonesArray *gQwHits; ///< Static array of QwHits
+
+    // Tree lines
+    #define QWEVENT_MAX_NUM_TREELINES 1000
+    Int_t fNQwTreeLines; ///< Number of QwTreeLines in the array
+    TClonesArray        *fQwTreeLines; ///< Array of QwTreeLines
+    static TClonesArray *gQwTreeLines; ///< Static array of QwTreeLines
+
+    // Tree lines (2nd attempt at getting this into the TBrowser)
+    Int_t fNQwTreeLines2; ///< Number of QwTreeLines in the array
+    TObjArray *fQwTreeLines2; ///< Array of QwTreeLines
+
+    // Tree lines (3rd attempt)
+    std::vector <QwTrackingTreeLine> fQwTreeLines3;
+
+    // Partial tracks
+    #define QWEVENT_MAX_NUM_PARTIALTRACKS 1000
+    Int_t fNQwPartialTracks; ///< Number of QwPartialTracks in the array
+    TClonesArray        *fQwPartialTracks; ///< Array of QwPartialTracks
+    static TClonesArray *gQwPartialTracks; ///< Static array of QwPartialTracks
+
+
+  public:
+
     QwEvent();
-    ~QwEvent();
+    virtual ~QwEvent();
+
+    // Event header
+    QwEventHeader* GetEventHeader() { return fEventHeader; };
+
+    // Housekeeping methods for lists
+    void Clear(Option_t *option = ""); // Clear the current event
+    void Reset(Option_t *option = ""); // Reset the static arrays (periodically)
+
+    // Creating, adding, and getting hits
+    QwHit* CreateNewHit();
+    void AddHit(QwHit* hit);
+    void ClearHits(Option_t *option = "");
+    void ResetHits(Option_t *option = "");
+    // Adding, and getting hit containers
+    void AddHitContainer(QwHitContainer* hitlist);
+    QwHitContainer* GetHitContainer();
+    // Get the number of hits
+    Int_t GetNumberOfHits() const { return fNQwHits; };
+    // Print the list of hits
+    void PrintHits();
+
+    // Creating and adding tree lines
+    QwTrackingTreeLine* CreateNewTreeLine();
+    void AddTreeLine(QwTrackingTreeLine* treeline);
+    void ClearTreeLines(Option_t *option = "");
+    void ResetTreeLines(Option_t *option = "");
+    // Get the number of tree lines
+    Int_t GetNumberOfTreeLines() const { return fNQwTreeLines; };
+    // Print the list of tree lines
+    void PrintTreeLines();
+
+    // Creating and adding tree lines
+    QwTrackingTreeLine* CreateNewTreeLine2();
+    void AddTreeLine2(QwTrackingTreeLine* treeline);
+    void ClearTreeLines2(Option_t *option = "");
+    void ResetTreeLines2(Option_t *option = "");
+    // Get the number of tree lines
+    Int_t GetNumberOfTreeLines2() const { return fNQwTreeLines2; };
+    // Print the list of tree lines
+    void PrintTreeLines2();
+
+    // Creating and adding partial tracks
+    QwPartialTrack* CreateNewPartialTrack();
+    void AddPartialTrack(QwPartialTrack* partialtrack);
+    void ClearPartialTracks(Option_t *option = "");
+    void ResetPartialTracks(Option_t *option = "");
+    // Get the number of partial tracks
+    Int_t GetNumberOfPartialTracks() const { return fNQwPartialTracks; };
+    // Print the list of partial tracks
+    void PrintPartialTracks();
+
+    // Output function
+    void Print();
 
   public:
 
     /*! list of tree lines [upper/lower][region][type][u/v/x/y] */
-    QwTrackingTreeLine* treeline[kNumPackages][kNumRegions][kNumTypes][kNumDirections];
+    QwTrackingTreeLine* treeline[kNumPackages][kNumRegions][kNumTypes][kNumDirections]; //!
 
     /*! list of partial tracks [package][region][type] */
-    //std::vector < QwPartialTrack* > parttrack[kNumPackages][kNumRegions][kNumTypes];
-    QwPartialTrack* parttrack[kNumPackages][kNumRegions][kNumTypes];
+    QwPartialTrack* parttrack[kNumPackages][kNumRegions][kNumTypes]; //!
 
     /*! list of complete tracks [package] */
-    QwTrack* track[kNumPackages];
+    QwTrack* track[kNumPackages]; //!
 
     /*! list of vertices in this event */
-    std::vector< QwVertex* > vertex;
+    //std::vector< QwVertex* > vertex; //!
 
-  //ClassDef(QwEvent,1);
+  ClassDef(QwEvent,1);
 
-};
+}; // class QwEvent
 
-#endif
+#endif // QWEVENT_H
--- Tracking/src/QwEvent.cc
+++ Tracking/src/QwEvent.cc
@@ -1,15 +1,49 @@
 #include "QwEvent.h"
-//ClassImp(QwEvent);
+ClassImp(QwEvent);
+ClassImp(QwEventHeader);
 
-
+// Qweak headers
+#include "QwHit.h"
+#include "QwHitContainer.h"
 #include "QwTrackingTreeLine.h"
 #include "QwPartialTrack.h"
 #include "QwTrack.h"
 #include "QwVertex.h"
 
+// Initialize the static lists
+TClonesArray* QwEvent::gQwHits = 0;
+TClonesArray* QwEvent::gQwTreeLines = 0;
+TClonesArray* QwEvent::gQwPartialTracks = 0;
 
 QwEvent::QwEvent()
 {
+  // Create an event header
+  fEventHeader = new QwEventHeader();
+
+  // Create the static TClonesArray for the hits if not existing yet
+  if (! gQwHits)
+    gQwHits = new TClonesArray("QwHit", QWEVENT_MAX_NUM_HITS);
+  // Set local TClonesArray to static TClonesArray and zero hits
+  fQwHits = gQwHits;
+  fNQwHits = 0;
+
+  // Create the static TClonesArray for the tree lines if not existing yet
+  if (! gQwTreeLines)
+    gQwTreeLines = new TClonesArray("QwTrackingTreeLine", QWEVENT_MAX_NUM_TREELINES);
+  // Set local TClonesArray to static TClonesArray and zero hits
+  fQwTreeLines = gQwTreeLines;
+  fNQwTreeLines = 0;
+
+  // Create the TObjArray for the tree lines
+  fQwTreeLines2 = new TObjArray(QWEVENT_MAX_NUM_TREELINES);
+  fNQwTreeLines2 = 0;
+
+  // Create the static TClonesArray for the partial tracks if not existing yet
+  if (! gQwPartialTracks)
+    gQwPartialTracks = new TClonesArray("QwPartialTrack", QWEVENT_MAX_NUM_PARTIALTRACKS);
+  // Set local TClonesArray to static TClonesArray and zero hits
+  fQwPartialTracks = gQwPartialTracks;
+  fNQwPartialTracks = 0;
 }
 
 
@@ -39,4 +73,216 @@ QwEvent::~QwEvent()
       } // end of loop over types
     } // end of loop over regions
   } // end of loop over packages
+}
+
+
+// Clear the local TClonesArrays
+void QwEvent::Clear(Option_t *option)
+{
+  ClearHits();
+  ClearTreeLines();
+  ClearTreeLines2();
+  ClearPartialTracks();
+};
+
+// Delete the static TClonesArrays
+void QwEvent::Reset(Option_t *option)
+{
+  ResetHits();
+  ResetTreeLines();
+  ResetTreeLines2();
+  ResetPartialTracks();
+};
+
+
+// Print the event
+void QwEvent::Print()
+{
+  PrintHits();
+  PrintTreeLines2();
+  PrintPartialTracks();
+}
+
+
+// Create a new QwHit
+QwHit* QwEvent::CreateNewHit()
+{
+  TClonesArray &hits = *fQwHits;
+  QwHit *hit = new (hits[fNQwHits++]) QwHit();
+  return hit;
+};
+
+// Add an existing QwHit
+void QwEvent::AddHit(QwHit* hit)
+{
+  QwHit* newhit = CreateNewHit();
+  *newhit = *hit;
+};
+
+// Clear the local TClonesArray of hits
+void QwEvent::ClearHits(Option_t *option)
+{
+  fQwHits->Clear(option); // Clear the local TClonesArray
+  fNQwHits = 0; // No hits in local TClonesArray
+};
+
+// Delete the static TClonesArray of hits
+void QwEvent::ResetHits(Option_t *option)
+{
+  delete gQwHits;
+  gQwHits = 0;
+}
+
+// Print the hits
+void QwEvent::PrintHits()
+{
+  TIterator* iterator = fQwHits->MakeIterator();
+  QwHit* hit = 0;
+  while ((hit = (QwHit*) iterator->Next()))
+    std::cout << *hit << std::endl;
+}
+
+
+// Add the hits of a QwHitContainer to the TClonesArray
+void QwEvent::AddHitContainer(QwHitContainer* hitlist)
+{
+  ClearHits();
+  for (QwHitContainer::iterator hit = hitlist->begin();
+       hit != hitlist->end(); hit++) {
+    QwHit* p = &(*hit);
+    AddHit(p);
+  }
+}
+
+// Get the hits from the TClonesArray to a QwHitContainer
+QwHitContainer* QwEvent::GetHitContainer()
+{
+  QwHitContainer* hitlist = new QwHitContainer();
+  TIterator* iterator = fQwHits->MakeIterator();
+  QwHit* hit = 0;
+  while ((hit = (QwHit*) iterator->Next())) {
+    hitlist->push_back(*hit);
+  }
+  return hitlist;
+}
+
+
+// Create a new QwTreeLine
+QwTrackingTreeLine* QwEvent::CreateNewTreeLine()
+{
+  TClonesArray &treelines = *fQwTreeLines;
+  QwTrackingTreeLine *treeline = new (treelines[fNQwTreeLines++]) QwTrackingTreeLine();
+  return treeline;
+};
+
+// Add an existing QwTreeLine
+void QwEvent::AddTreeLine(QwTrackingTreeLine* treeline)
+{
+  QwTrackingTreeLine* newtreeline = CreateNewTreeLine();
+  *newtreeline = *treeline;
+};
+
+// Clear the local TClonesArray of tree lines
+void QwEvent::ClearTreeLines(Option_t *option)
+{
+  fQwTreeLines->Clear(option); // Clear the local TClonesArray
+  fNQwTreeLines = 0; // No tree lines in local TClonesArray
+};
+
+// Delete the static TClonesArray of tree lines
+void QwEvent::ResetTreeLines(Option_t *option)
+{
+  delete gQwTreeLines;
+  gQwTreeLines = 0;
+}
+
+// Print the tree lines
+void QwEvent::PrintTreeLines()
+{
+  TIterator* iterator = fQwTreeLines->MakeIterator();
+  QwTrackingTreeLine* treeline = 0;
+  while ((treeline = (QwTrackingTreeLine*) iterator->Next()))
+    std::cout << *treeline << std::endl;
+}
+
+
+// Create a new QwTreeLine
+QwTrackingTreeLine* QwEvent::CreateNewTreeLine2()
+{
+  //TClonesArray &treelines = *fQwTreeLines;
+  //QwTrackingTreeLine *treeline = new (treelines[fNQwTreeLines++]) QwTrackingTreeLine();
+  //return treeline;
+  return 0;
+};
+
+// Add an existing QwTreeLine
+void QwEvent::AddTreeLine2(QwTrackingTreeLine* treeline)
+{
+  //QwTrackingTreeLine* newtreeline = CreateNewTreeLine();
+  //*newtreeline = *treeline;
+  //fQwTreeLines2->Add(treeline);
+  //fNQwTreeLines2++;
+  fQwTreeLines3.push_back(*treeline);
+};
+
+// Clear the local TClonesArray of tree lines
+void QwEvent::ClearTreeLines2(Option_t *option)
+{
+  fQwTreeLines2->Clear(option); // Clear the local TClonesArray
+  fNQwTreeLines2 = 0; // No tree lines in local TClonesArray
+};
+
+// Delete the static TClonesArray of tree lines
+void QwEvent::ResetTreeLines2(Option_t *option)
+{
+  //delete gQwTreeLines;
+  //gQwTreeLines = 0;
+}
+
+// Print the tree lines
+void QwEvent::PrintTreeLines2()
+{
+  TIterator* iterator = fQwTreeLines2->MakeIterator();
+  QwTrackingTreeLine* treeline = 0;
+  while ((treeline = (QwTrackingTreeLine*) iterator->Next()))
+    std::cout << *treeline << std::endl;
+}
+
+
+// Create a new QwPartialTrack
+QwPartialTrack* QwEvent::CreateNewPartialTrack()
+{
+  TClonesArray &partialtracks = *fQwPartialTracks;
+  QwPartialTrack *partialtrack = new (partialtracks[fNQwPartialTracks++]) QwPartialTrack();
+  return partialtrack;
+};
+
+// Add an existing QwPartialTrack
+void QwEvent::AddPartialTrack(QwPartialTrack* partialtrack)
+{
+  QwPartialTrack* newpartialtrack = CreateNewPartialTrack();
+  *newpartialtrack = *partialtrack;
+};
+
+// Clear the local TClonesArray of hits
+void QwEvent::ClearPartialTracks(Option_t *option)
+{
+  fQwPartialTracks->Clear(option); // Clear the local TClonesArray
+  fNQwPartialTracks = 0; // No partial tracks in local TClonesArray
+};
+
+// Delete the static TClonesArray of hits
+void QwEvent::ResetPartialTracks(Option_t *option)
+{
+  delete gQwPartialTracks;
+  gQwPartialTracks = 0;
+}
+
+// Print the partial tracks
+void QwEvent::PrintPartialTracks()
+{
+  TIterator* iterator = fQwPartialTracks->MakeIterator();
+  QwPartialTrack* partialtrack = 0;
+  while ((partialtrack = (QwPartialTrack*) iterator->Next()))
+    std::cout << *partialtrack << std::endl;
 }
