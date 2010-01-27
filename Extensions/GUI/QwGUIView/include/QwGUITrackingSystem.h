42b8e2bc754c1e8c8cf31d932fbb3fdd19a96a37
jhlee <jhlee@b9700a98-9937-0410-b777-f318d6eb5bff>
2010-01-27 22:00:49 +0000

    add some buttons and check button
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@667 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/GUI/QwGUIView/include/QwGUITrackingSystem.h
+++ Extensions/GUI/QwGUIView/include/QwGUITrackingSystem.h
@@ -57,31 +57,57 @@ class QwHit;
 class QwHitContainer;
 class QwHitRootContainer;
 
+
+#define HFRAME_NUM             2
+#define GFRAME_NUM             1
+#define G0HFRAME_NUM           2
+#define G0VFRAME_NUM           1
+
+#define NUM_ENTRY_NUM          3
+#define LABEL_NUM              2
+#define TXT_BUTTON_NUM         2
+#define CHK_BUTTON_NUM         1
+
+
+
+
+
 class QwGUITrackingSystem : public TGCompositeFrame {
 
  private:
 
-  TGHorizontalFrame   *hframe[2];
-  TGGroupFrame        *gframe[1];
-  TGHorizontalFrame   *g0hframe[2];
+  TGHorizontalFrame   *hframe[HFRAME_NUM];
+  TGGroupFrame        *gframe[GFRAME_NUM];
+  TGHorizontalFrame   *g0hframe[G0HFRAME_NUM];
   TGVerticalFrame     *g0vframe[1];
 
-  TGNumberEntry       *num_entry[3];
-  TGLabel             *num_label[2];
-  TGTextButton        *txt_button[2];
+  TGNumberEntry       *num_entry[NUM_ENTRY_NUM];
+  TGLabel             *num_label[LABEL_NUM];
+  TGTextButton        *txt_button[TXT_BUTTON_NUM];
+  TGCheckButton       *chk_button[CHK_BUTTON_NUM];
 
   Pixel_t             green;
   Pixel_t             red;
 
   UInt_t              run_number;
+  UInt_t              event_range[2];
+  UInt_t        default_event_range[2];
 
-  
+  Bool_t              event_range_status;
+  Bool_t              load_rootfile_status;
+
  public:
   
   QwGUITrackingSystem(const TGWindow *main, TGCompositeFrame *parent, UInt_t w, UInt_t h);
   ~QwGUITrackingSystem();
   void    CreateFrame          (TGCompositeFrame *parent, UInt_t w, UInt_t h);
   virtual Bool_t ProcessMessage(Long_t msg, Long_t parm1, Long_t parm2);
+
+
+  TString GetFileName();
+  Bool_t  SetEventRange();
+  void    DefaultEventRange(Bool_t entry_status);
+  void    ResetEventRange(Bool_t entry_status);
 
   ClassDef(QwGUITrackingSystem,0);
 };
--- Extensions/GUI/QwGUIView/src/QwGUITrackingSystem.cc
+++ Extensions/GUI/QwGUIView/src/QwGUITrackingSystem.cc
@@ -11,7 +11,7 @@ enum QwGUITrackingSystemIndentificator {
   BA_EVENT_NUMBER_START, BA_EVENT_NUMBER_END, 
   BA_OUTPUTTYPE1,
   BA_COMBO,
-  BA_DEBUGONE,
+  BA_CHKBUTTON_ONE,
   BA_LISTBOX_ON
 };
 
@@ -23,16 +23,26 @@ QwGUITrackingSystem::QwGUITrackingSystem(const TGWindow *main, TGCompositeFrame 
   gClient->GetColorByName("red",   red);
 
   run_number = 1567;
+  event_range[0] = 0;
+  event_range[1] = 0;
 
-  for(Short_t i=0; i<2;i++)  hframe [i] = NULL;
-  gframe [0] = NULL;
-  for(Short_t i=0; i<2;i++)  g0hframe[i] = NULL;
-  g0vframe[0] = NULL;
+  default_event_range[0] = 0;
+  default_event_range[1] = 1000;
 
 
-  for(Short_t i=0; i<3;i++) num_entry[i] = NULL;
-  for(Short_t i=0; i<2;i++) num_label[i] = NULL;
-  for(Short_t i=0; i<2;i++) txt_button[i] = NULL;
+  for(Short_t i=0; i<HFRAME_NUM;  i++) hframe  [i] = NULL;
+  for(Short_t i=0; i<GFRAME_NUM;  i++) gframe  [0] = NULL;
+  for(Short_t i=0; i<G0HFRAME_NUM;i++) g0hframe[i] = NULL;
+  for(Short_t i=0; i<G0VFRAME_NUM;i++) g0vframe[0] = NULL;
+
+
+  for(Short_t i=0; i<NUM_ENTRY_NUM; i++) num_entry [i] = NULL;
+  for(Short_t i=0; i<LABEL_NUM;     i++) num_label [i] = NULL;
+  for(Short_t i=0; i<TXT_BUTTON_NUM;i++) txt_button[i] = NULL;
+  for(Short_t i=0; i<CHK_BUTTON_NUM;i++) chk_button[i] = NULL;
+
+  event_range_status  = false;
+  load_rootfile_status = false;
 
   CreateFrame(parent,w, h);
 
@@ -40,6 +50,7 @@ QwGUITrackingSystem::QwGUITrackingSystem(const TGWindow *main, TGCompositeFrame 
 
 QwGUITrackingSystem::~QwGUITrackingSystem()
 {
+  delete [] chk_button;
   delete [] txt_button;
   delete [] num_label;
   delete [] num_entry;
@@ -53,74 +64,79 @@ QwGUITrackingSystem::~QwGUITrackingSystem()
 void 
 QwGUITrackingSystem::CreateFrame(TGCompositeFrame *parent, UInt_t w, UInt_t h)
 {
-
-
-
-  Short_t digital_width   = 10;
-  Short_t run_number_min  = 1077;
-
   
+
   hframe [0] = new TGHorizontalFrame(parent);
   parent  -> AddFrame(hframe[0], new TGLayoutHints(kLHintsCenterX|kLHintsExpandX,10,10,10,10));
   hframe [1] = new TGHorizontalFrame(parent);
   parent  -> AddFrame(hframe[1], new TGLayoutHints(kLHintsCenterX|kLHintsExpandX,10,10,10,10));
-
-
+  
+  
+  Short_t digital_width     = 10;
+  Short_t run_number_min    = 1;
+  Short_t event_num_min     = 0;
+  Short_t default_event_num = 0;
+  
+  
   // hframe [0] 
   num_label[0]  = new TGLabel(hframe[0], "Run #");
   num_entry[0]  = new TGNumberEntry(hframe[0], run_number, digital_width, BA_RUN_NUMBER, 
 				    TGNumberFormat::kNESInteger,
 				    TGNumberFormat::kNEAPositive,
 				    TGNumberFormat::kNELLimitMin, run_number_min);
-  txt_button [0] = new TGTextButton(hframe[0], "Load Hit-based ROOT file",   BA_TEXTBUTTONONE);
-  txt_button [0] -> SetToolTipText(" Not yet decided to be here or not.\n Just get a glimpse of what advanced Tracking GUI might be like in the future.\n Stand-alone or inside QwGUIMain? I am not sure....");
-
+  chk_button[0] = new TGCheckButton (hframe[0], "Select Event Range", BA_CHKBUTTON_ONE);
   
-  hframe[0] -> AddFrame(num_label[0],  new TGLayoutHints(kLHintsExpandX,0, 0,4,0));
-  hframe[0] -> AddFrame(num_entry[0],  new TGLayoutHints(kLHintsExpandX,0, 0,4,0));
-  hframe[0] -> AddFrame(txt_button[0], new TGLayoutHints(kLHintsExpandX,20,1,4,1));
+  num_label[1] = new TGLabel(hframe[0], "Evt #");
+  
+  digital_width = 4;
+  
+  num_entry[1] = new TGNumberEntry(hframe[0], default_event_num, digital_width, BA_EVENT_NUMBER_START, 
+				   TGNumberFormat::kNESInteger,
+				   TGNumberFormat::kNEANonNegative,
+				   TGNumberFormat::kNELLimitMin, event_num_min);
+  num_entry[2] = new TGNumberEntry(hframe[0], default_event_num, digital_width, BA_EVENT_NUMBER_END, 
+				   TGNumberFormat::kNESInteger,
+				   TGNumberFormat::kNEANonNegative,
+				   TGNumberFormat::kNELLimitMin, event_num_min);
+  
+  hframe[0] -> AddFrame(num_label[0],   new TGLayoutHints(kLHintsCenterX,0,0,4,0));
+  hframe[0] -> AddFrame(num_entry[0],   new TGLayoutHints(kLHintsCenterX,0,0,0,0));
+  hframe[0] -> AddFrame(chk_button[0],  new TGLayoutHints(kLHintsCenterX,4,0,4,0));
+  hframe[0] -> AddFrame(num_label[1],   new TGLayoutHints(kLHintsCenterX,0,0,4,0));
+  hframe[0] -> AddFrame(num_entry[1],   new TGLayoutHints(kLHintsExpandX,0,0,0,0));
+  hframe[0] -> AddFrame(num_entry[2],   new TGLayoutHints(kLHintsExpandX,0,0,0,0));
 
-  num_entry  [0] -> Associate(this);
-  txt_button [0] -> Associate(this);
-  txt_button [0] -> ChangeBackground(red);
+  num_entry [0] -> Associate(this);
+  chk_button[0] -> Associate(this);
+  num_entry [1] -> Associate(this);
+  num_entry [2] -> Associate(this);
+  num_entry [1] -> SetState(false);
+  num_entry [2] -> SetState(false);
+
+
+  chk_button[0] -> SetState(kButtonUp);
+  chk_button[0] -> SetToolTipText("Turn on/off Select Range");
+
 
   // hframe [1] 
 
 
-  Short_t event_num_min     = 0;
-  Short_t default_event_num = 0;
-    
-  digital_width = 4;
 
-  num_label[1] = new TGLabel(hframe[1], "Physics Event #");
-  num_entry[1] = new TGNumberEntry(hframe[1], default_event_num, digital_width, BA_EVENT_NUMBER_START, 
-				   TGNumberFormat::kNESInteger,
-				   TGNumberFormat::kNEANonNegative,
-				   TGNumberFormat::kNELLimitMin, event_num_min);
-  num_entry[2] = new TGNumberEntry(hframe[1], default_event_num, digital_width, BA_EVENT_NUMBER_END, 
-				   TGNumberFormat::kNESInteger,
-				   TGNumberFormat::kNEANonNegative,
-				   TGNumberFormat::kNELLimitMin, event_num_min);
-//   txt_button[1] = new TGTextButton(hframe[1], "Reset Event #",   BA_TEXTBUTTONTWO);
-  
-  hframe[1] -> AddFrame(num_label[1],  new TGLayoutHints(kLHintsExpandX,0, 0,4,0));
-  hframe[1] -> AddFrame(num_entry[1],  new TGLayoutHints(kLHintsExpandX,0, 0,4,0));
-  hframe[1] -> AddFrame(num_entry[2],  new TGLayoutHints(kLHintsExpandX,0, 0,4,0));
-//   hframe[1] -> AddFrame(txt_button[1], new TGLayoutHints(kLHintsExpandX,10,1,4,1));
-  
-  num_entry  [1] -> Associate(this);
-  num_entry  [1] -> SetState(false);
-  num_entry  [2] -> Associate(this);
-  num_entry  [2] -> SetState(false);
-  //  num_label  [1] -> SetToolTipText("This Event is a Physics Event of CODA buffer");
-//   txt_button [1] -> SetToolTipText("Reset Event number from a beginning..");
-//   //  txt_button [2] -> SetToolTipText("Maybe next version.....");
-//   txt_button [1] -> Associate(this);
-  //  txt_button [2] -> SetEnabled(false);
-  
+  txt_button [0] = new TGTextButton(hframe[1], "Load Hit-based ROOT file",   BA_TEXTBUTTONONE);
+  txt_button [0] -> SetToolTipText(" Not yet decided to be here or not.\n Just get a glimpse of what advanced Tracking GUI might be like in the future.\n Stand-alone or inside QwGUIMain? I am not sure....");
+  txt_button [1] = new TGTextButton(hframe[1], "Prepare historgrams",   BA_TEXTBUTTONTWO);
 
 
+  hframe[1] -> AddFrame(txt_button[0], new TGLayoutHints(kLHintsExpandX,20,1,4,1));
+  hframe[1] -> AddFrame(txt_button[1], new TGLayoutHints(kLHintsExpandX,20,1,4,1));
 
+ 
+  txt_button[0] -> Associate(this);
+  txt_button[0] -> ChangeBackground(red);
+  txt_button[1] -> Associate(this); 
+  txt_button[1] -> SetEnabled(false);
+
+ 
   gframe[0] = new TGGroupFrame(parent, "ReadHits.C prototype GUI version and test TSuperCanvas", kVerticalFrame);
   parent -> AddFrame(gframe[0],  new TGLayoutHints(kLHintsCenterX|kLHintsExpandX,0,0,0,0));
 
@@ -134,7 +150,70 @@ QwGUITrackingSystem::CreateFrame(TGCompositeFrame *parent, UInt_t w, UInt_t h)
 
 
 
+TString 
+QwGUITrackingSystem::GetFileName() 
+{
+  run_number       = (UInt_t) num_entry[0] -> GetNumber();
+  return Form("%s/Qweak_%d.root", getenv("QW_ROOTFILES_DIR"), run_number);
+}
 
+
+
+
+Bool_t
+QwGUITrackingSystem::SetEventRange() 
+{
+//   for(Short_t i=0; i<2; i++)
+//     {
+//       event_range[i] = (UInt_t) num_entry[i+1] -> GetNumber();
+//     }
+
+
+//   if( (event_range[0] ==0) && (event_range[1] ==0) )
+//     {
+//       event_range_status = false;  
+//       range[0] = 0;
+//       range[1] = total_physics_event_number;
+//     }
+//   else
+//     {
+//       event_number_range_status = true;
+//       if (!(event_number_range[0] < event_number_range[1]) ) 
+// 	{
+// 	  event_number_range[1] = event_number_range[0] + 10;
+// 	  num_entry[2]-> SetNumber(event_number_range[1]);
+// 	  // force to set max range 10 larger than min range
+// 	}
+//       range[0] = event_number_range[0]; 
+//       range[1] = event_number_range[1];
+//     }
+
+  return true;
+}
+
+void 
+QwGUITrackingSystem::DefaultEventRange(Bool_t entry_status=true) 
+{
+  for(Short_t i=0; i<2; i++)
+    {
+      num_entry[i+1] -> SetNumber(default_event_range[i]);
+      num_entry[i+1] -> SetState(entry_status);
+    }
+  return;
+}
+
+
+void 
+QwGUITrackingSystem::ResetEventRange(Bool_t entry_status=false) 
+{
+  for(Short_t i=0; i<2; i++)
+    {
+      event_range[i] = 0;
+      num_entry[i+1] -> SetNumber(event_range[i]);
+      num_entry[i+1] -> SetState(entry_status);
+    }
+  return;
+}
 
 
 Bool_t 
@@ -158,6 +237,12 @@ QwGUITrackingSystem::ProcessMessage(Long_t msg, Long_t parm1, Long_t parm2)
 		    if(run_number%2 == 0) txt_button[0] -> ChangeBackground(green);
 		    else                  txt_button[0] -> ChangeBackground(red);
 		    printf("You selected RUN %d ROOT file\n", run_number);
+		    printf("Is it the right one %s ?\n", GetFileName().Data());
+		  };;;
+		  break;
+		case  BA_TEXTBUTTONTWO:
+		  {;;;
+		    printf("you pressed 2nd text button\n");
 		  };;;
 		  break;
 		default:
@@ -201,8 +286,17 @@ QwGUITrackingSystem::ProcessMessage(Long_t msg, Long_t parm1, Long_t parm2)
 	    {;;	  
 	      switch(parm1)  
 		{
-		case  BA_DEBUGONE:
+		case  BA_CHKBUTTON_ONE:
 		  {;;;
+		    event_range_status  = chk_button[0] -> GetState();
+		    if(event_range_status)
+		      {
+			DefaultEventRange();
+		      }
+		    else
+		      {
+			ResetEventRange();
+		      }
 		  };;;
 		  break;
 		default:
