f8002277c4cd1bc89fd05e0bfa6b0ca80259164b
pking <pking@b9700a98-9937-0410-b777-f318d6eb5bff>
2010-02-12 18:15:22 +0000

    Merged the changes in revision range 711:734 of branch pking.f1tdc_decoder_change into the trunk.
    These changes correct an inconsistency in the method expected
    to correct the F1TDC values for the reference time between the
    VDC and HDC.
    Normal usage of the F1TDC reference time subtraction will now
    return (raw time - reference time), and all other time offset
    corrections are the responsibility of the containing class.
    
    TODO:  There is a special mode of the F1TDC reference time
           correction to continue to support the VDC's expectation
           that additional time offset corrections are done there.
           A future revision should correct the VDC subsystem class
           to expect the "normal" reference time subtraction and
           apply other corrections appropriately.
    
    
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@735 b9700a98-9937-0410-b777-f318d6eb5bff

--- Tracking/src/QwDriftChamberVDC.cc
+++ Tracking/src/QwDriftChamberVDC.cc
@@ -24,14 +24,14 @@ This generates identical set of hits to region 2 for region 3.
 const UInt_t QwDriftChamberVDC::kBackPlaneNum=4;
 const UInt_t QwDriftChamberVDC::kLineNum=8;
 
-QwDriftChamberVDC::QwDriftChamberVDC ( TString region_tmp ) :VQwSubsystem ( region_tmp ),
-       QwDriftChamber ( region_tmp,fWireHitsVDC ) {
-	//QwDriftChamber ( region_tmp,fTDCHits ) {
-    std::vector<QwDelayLine> temp;
-    temp.resize ( kLineNum );
-    fDelayLineArray.resize ( kBackPlaneNum,temp );
-    fDelayLinePtrs.resize ( 21 );
-    OK=0;
+QwDriftChamberVDC::QwDriftChamberVDC ( TString region_tmp ): VQwSubsystem ( region_tmp ),
+							     QwDriftChamber ( region_tmp,fWireHitsVDC ) {
+  SetReferenceParameters(-30000., 30000., 64495., 8929., kTRUE);
+  std::vector<QwDelayLine> temp;
+  temp.resize ( kLineNum );
+  fDelayLineArray.resize ( kBackPlaneNum,temp );
+  fDelayLinePtrs.resize ( 21 );
+  OK=0;
 };
 
 Int_t QwDriftChamberVDC::LoadQweakGeometry ( TString mapfile ) {
@@ -256,6 +256,7 @@ void  QwDriftChamberVDC::FillRawTDCWord ( Int_t bank_index, Int_t slot_num, Int_
 
             // hitCount=std::count_if(fTDCHits.begin(),fTDCHits.end(),boost::bind(&QwHit::WireMatches,_1,2,boost::ref(package),boost::ref(plane),boost::ref(wire)) );
             //std::cout<<"At QwDriftChamberVDC::FillRawTDCWord_2"<<endl;
+            hitCount=std::count_if(fTDCHits.begin(),fTDCHits.end(),boost::bind(&QwHit::WireMatches,_1,3,boost::ref(package),boost::ref(plane),boost::ref(wire)) );
             fTDCHits.push_back ( QwHit ( bank_index, slot_num, chan, hitCount, kRegionID3, package, plane,direction, wire, data ) );//in order-> bank index, slot num, chan, hitcount, region=3, package, plane,,direction, wire,wire hit time
 
         }
@@ -268,15 +269,7 @@ void  QwDriftChamberVDC::FillRawTDCWord ( Int_t bank_index, Int_t slot_num, Int_
 
 
 
-Int_t QwDriftChamberVDC::LinkReferenceChannel ( const UInt_t chan, const UInt_t plane, const UInt_t wire ) {
-    fReferenceChannels.at ( fCurrentBankIndex ).first  = fCurrentTDCIndex;
-    fReferenceChannels.at ( fCurrentBankIndex ).second = chan;
-    //  Register a reference channel with the wire equal to the bank index.
-    fTDCPtrs.at ( fCurrentTDCIndex ).at ( chan ).fPackage = 0;
-    fTDCPtrs.at ( fCurrentTDCIndex ).at ( chan ).fPlane   = plane;
-    fTDCPtrs.at ( fCurrentTDCIndex ).at ( chan ).fElement = fCurrentBankIndex;
-    return OK;
-};
+
 
 Int_t QwDriftChamberVDC::BuildWireDataStructure ( const UInt_t chan, const UInt_t package, const UInt_t plane, const Int_t wire ) {
     if ( plane == kReferenceChannelPlaneNumber ) {
