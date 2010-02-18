f3246a8face621c16ed9c30c10464c0ecb8d3e9b
jhlee <jhlee@b9700a98-9937-0410-b777-f318d6eb5bff>
2010-02-18 18:07:30 +0000

    * introduce QwColor.h in order to see color outputs in    terminal
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@745 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Macros/Tracking/TSuperCanvas.h
+++ Extensions/Macros/Tracking/TSuperCanvas.h
@@ -84,20 +84,29 @@
 #include "TMath.h"
 #include "TVirtualX.h"
 
-// funny colors: http://www.faqs.org/docs/abs/HTML/colorizing.html
-//           or: http://linuxgazette.net/issue65/padala.html
+
 #define RED       "\E[31m"
 #define GREEN     "\E[32m"
+#define BROWN     "\E[33m"
 #define BLUE      "\E[34m"
-#define BOLD      "\033[1m"
-#define BOLDRED   "\E[31m\033[1m"
-#define BOLDGREEN "\E[32m\033[1m"
-#define BOLDBLUE  "\E[34m\033[1m"
-#define BACKRED   "\E[31m\033[7m"
-#define BACKGREEN "\E[32m\033[7m"
-#define BACKBLUE  "\E[34m\033[7m"
-#define NORMAL    "\033[0m"
+#define MAGENTA   "\E[35m"
+#define CYAN      "\E[36m"
+#define WHITE     "\E[37m"
 
+#define BOLD        "\033[1m"
+
+#define BOLDRED     "\E[31m\033[1m"
+#define BOLDGREEN   "\E[32m\033[1m"
+#define BOLDBROWN   "\E[33m\033[1m"
+#define BOLDBLUE    "\E[34m\033[1m"
+#define BOLDMAGENTA "\E[35m\033[1m"
+#define BOLDCYAN    "\E[36m\033[1m"
+#define BOLDWHITE    "\E[37m\033[1m"
+
+#define BACKRED     "\E[31m\033[7m"
+#define BACKGREEN   "\E[32m\033[7m"
+#define BACKBLUE    "\E[34m\033[7m"
+#define NORMAL      "\033[0m"
 
 class TSuperCanvas : public TCanvas
 {
