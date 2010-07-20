28ef4e9be27a689383d99257f7cf6cae77951d23
cornejo <cornejo@b9700a98-9937-0410-b777-f318d6eb5bff>
2010-07-20 05:05:10 +0000

    Merge branch 'eventdisplay' of coderepo@paper.paintblack.com:qwanalysis_trunk
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@1505 b9700a98-9937-0410-b777-f318d6eb5bff

---
+++ Extensions/EventDisplay3D/main/QwEventDisplay3D.cc
@@ -0,0 +1,50 @@
+/*------------------------------------------------------------------------*//*!
+
+ \file QwEventDisplayTest.cc
+ \ingroup QwTrackingAnl
+
+ \brief Example code for the event display routines
+
+*//*-------------------------------------------------------------------------*/
+
+// Standard C and C++ headers
+#include <iostream>
+
+// ROOT headers
+#include <TApplication.h>
+
+// Qweak headers
+#include "QwOptionsTracking.h"
+#include "QwLog.h"
+
+// Deprecated Qweak headers
+#include "Det.h"
+#include "Qset.h"
+
+// Qweak subsystem headers
+#include "QwSubsystemArrayTracking.h"
+#include "QwGasElectronMultiplier.h"
+#include "QwDriftChamberHDC.h"
+#include "QwDriftChamberVDC.h"
+#include "QwTriggerScintillator.h"
+#include "QwMainDetector.h"
+#include "QwTreeEventBuffer.h"
+
+// Qweak event display header
+#include "QwEventDisplay3D.h"
+
+int main (int argc, char* argv[])
+{
+  // Start this application
+  TApplication theApp("QwEventDisplayTest3D", &argc, argv);
+
+  // Create a QwEventDisplay application
+  QwEventDisplay3D* display = new QwEventDisplay3D(gClient->GetRoot(),1200,800);
+
+  // Now run this application
+  display->RedrawViews();
+  theApp.Run();
+
+  return 0;
+
+}
