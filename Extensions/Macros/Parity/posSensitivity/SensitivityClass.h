6f4ad0eb0b9f7771e3d6eb055baca6ffbfaad392
pking <pking@b9700a98-9937-0410-b777-f318d6eb5bff>
2010-10-23 02:28:01 +0000

    Commit of what I think are Nur's sensitivity plot macros, whcih had been added, but not committed on the cdaq cluster.
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@2043 b9700a98-9937-0410-b777-f318d6eb5bff

---
+++ Extensions/Macros/Parity/posSensitivity/SensitivityClass.h
@@ -0,0 +1,46 @@
+/********************************************************************/
+/*                      File: NurClass.h                            */
+/*                         Nuruzzaman                               */
+/*                         10/08/2010                               */
+/*                                                                  */
+/*                   Last Edited:10/08/2010                         */
+/*                                                                  */
+/* This includes the different classes needed for the main files.   */
+/* It also defines the different styles used in the plots.          */
+/********************************************************************/
+
+#ifndef __NURCLASS_H 
+#define __NURCLASS_H
+
+gROOT->Reset();
+#include <TROOT.h>
+#include <TFile.h>
+#include <TH2.h>
+#include <TProfile.h>
+#include <TBox.h>
+#include <TGraph.h>
+#include <TCanvas.h>
+#include <TStyle.h>
+#include <TString.h>
+#include <TAxis.h>
+#include <TCut.h>
+#include <TText.h>
+#include <TAttText.h>
+#include <TLine.h>
+#include <time.h>
+#include <stdio.h>
+#include <TBox.h>
+#include <TPaveText.h>
+#include <TObject.h>
+#include <TApplication.h>
+#include <math.h>
+#include <fstream>
+#include <vector>
+#include <new>
+#include <TColor.h>
+//#include <QwColor.h>
+//#include <ConsoleColor.h>
+
+
+
+#endif
