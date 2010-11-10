9bfa87603abda2ade4a8d9172272e68348d9d0ba
sxyang <sxyang@b9700a98-9937-0410-b777-f318d6eb5bff>
2010-11-10 18:08:41 +0000

    -modify the Projection.C so that it can draw the projection map with weights  from main detector.
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@2162 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Macros/Tracking/Projection.C
+++ Extensions/Macros/Tracking/Projection.C
@@ -96,19 +96,26 @@ void project(){
 }
 
 void project_root(string s="", int package=1,int run_number=6327){
-	string file_name= Form ( "%s/Qweak_%d_50000.root",gSystem->Getenv ( "QWSCRATCH" ),run_number );	
+	string file_name= Form ( "%s/Qweak_%d.root",gSystem->Getenv ( "QWSCRATCH" ),run_number );	
         TFile *file = new TFile ( file_name.c_str() );
-	ofstream ifile;
-	ifile.open("output.dat");
-	TH1F* hp=new TH1F("profile","profile",100,250,350);
-	TH2F* h=new TH2F("h","h",1200,-600,600,200,-100,100);
+	TH1F* hp=new TH1F("profile","profile",200,-100,100);
+	TH1F* hp_w=new TH1F("profile_weighted","profile_weighted",200,-100,100);
+	h_r=new TProfile("weight profile","weight profile vs y",200,-100,100);
+	TH2F* h=new TH2F("h","h",120,280,400,200,-100,100);
+	TH2F* h_w=new TH2F("h_w","h_w",120,280,400,200,-100,100);
 
 
         TTree* event_tree= ( TTree* ) file->Get ( "event_tree" );
  	QwEvent* fEvent=NULL;
 	QwPartialTrack* pt=NULL; 
 	QwTrackingTreeLine* tl=NULL;
-	event_tree->SetBranchAddress("events",&fEvent);
+
+	TBranch* branch_event=event_tree->GetBranch("events");
+	TBranch* branch=event_tree->GetBranch("maindet");
+	branch_event->SetAddress(&fEvent);
+	TLeaf* md5p=branch->GetLeaf("md5p_adc");
+	TLeaf* md5m=branch->GetLeaf("md5m_adc");
+
 	Int_t nevents=event_tree->GetEntries();
 	
 
@@ -124,7 +131,9 @@ void project_root(string s="", int package=1,int run_number=6327){
 
 	cout << "number of events: " << nevents << endl;
 	for(int i=0;i<nevents;i++){
-		event_tree->GetEntry(i);
+		
+		branch_event->GetEntry(i);
+		branch->GetEntry(i);
 		double xoffset,yoffset,xslope,yslope,x,y;
 		for(int p=0;p<fEvent->GetNumberOfPartialTracks();p++){
 			pt=fEvent->GetPartialTrack(p);
@@ -135,20 +144,47 @@ void project_root(string s="", int package=1,int run_number=6327){
 			   yslope=pt->fSlopeY;
 			   x=xoffset+xslope*dz;
 			   y=yoffset+yslope*dz;
-			   hp->Fill(y);
+			   hp_w->Fill(x,(md5p->GetValue()+md5m->GetValue()));
+			   hp->Fill(x,1);
+			   h_r->Fill(x,(md5p->GetValue()+md5m->GetValue()),1);
+			   h_w->Fill(y,x,(md5p->GetValue()+md5m->GetValue()));
 			   h->Fill(y,x);
 			}
 		}
 	}
+
+	
 	gStyle->SetPalette(1);
-	hp->GetXaxis()->SetTitle("position x:cm");
-	hp->SetTitle("track projection on trigger");
+	hp->GetXaxis()->SetTitle("position y:cm");
+	hp->SetTitle("track projection on main detector(nonweighted)");
+	hp->SetLineColor(kRed);
+	hp_w->GetXaxis()->SetTitle("position y:cm");
+	hp_w->SetTitle("track projection on main detector(stacked)");
+	hp_w->SetLineColor(kBlue);
+// 	hp->SetMarkerStyle(2);
 //  	hp->Draw();
 	h->GetXaxis()->SetTitle("position x:cm");
 	h->GetYaxis()->SetTitle("position y:cm");
-	h->SetTitle("track projection on main detector");
-	h->Draw("colz");
+	h->SetTitle("track projection on main detector(not weighted)");
+	h_w->GetXaxis()->SetTitle("position x:cm");
+	h_w->GetYaxis()->SetTitle("position y:cm");
+	h_w->SetTitle("track projection on main detector(weighted)");
+	h_r->GetXaxis()->SetTitle("position y:cm");
+	h_r->GetYaxis()->SetTitle("weight (md5m+md5p)");
+	h_r->SetTitle(" weight factor depending on y");
+	h_w->Draw("colz");
+// 	h_r->Draw();
+// 	hp2->Draw();
+	
+// 	TCanvas* c=new TCanvas("c","c",400,400);
+// 	hp_w->Draw();
+// 	hp->Scale(3700);
+// 	hp->Draw("same");
+// 	leg=new TLegend(0.1,0.7,0.48,0.9);
+// 	leg->SetHeader("title");
+// 	leg->AddEntry(hp_w,"weighted");	
+// 	leg->AddEntry(hp,"not weighted");
+// 	leg->Draw();
 	
 	return;
 };
-
