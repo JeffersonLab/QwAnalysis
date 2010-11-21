f45c16e693d6c0aae9621323cca9667a77df5de8
buddhini <buddhini@b9700a98-9937-0410-b777-f318d6eb5bff>
2010-11-21 18:34:28 +0000

    Updated the read_run_range.cc script with a more user friendly way of getting rid of beam trips and current ramps. This method includes fitting the bcm1 yield/signal with a "landaun" fit and using the most probable value (MPV) from that fit to apply an event cut. Events with current between MPV*0.97 and MPV*1.03 will be used as good events to get the means and rms that we want from the data. The +-3% limit for current was obtained by looking at most recent data and doing lot of tests. Also, to avoid loong beam trips messing up the MPV from the fit, I applied a cut for current, current > 2uA. So far, by looking at lot of runs using this script, I was able to see that it can get rid of beam trips and current ramps and fit event ranges that I want it to fit. So I think this script now ready to be used by anyone who wants to look at run range summaries for devices. Let me know if you find any errors.
    Some minor modifications were done in get_mutli_range.sh to reorder things its plotting.
    
    
    
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@2235 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Macros/Parity/read_run_range.cc
+++ Extensions/Macros/Parity/read_run_range.cc
@@ -5,12 +5,18 @@
 // 
 // November 16th Buddhini : Updated to read trees instead of histograms. 
 //                          Use error Device_Error_Code == 0 to identify good events.
-// 
+// November 21st Buddhini : Updated to use a landaun fit to get rid of beam trips and rampings.
+//
 // This macro can access a range of runs specified by "run1" to "run2" to
 // to get the mean value and mean error of the "property" of the "device".
 // The canvas is then saved in to a .gif file with name e.g.5070_5080_qwk_bpm3h09bXP_s_summary.gif.
 //
-// To use it, compile and do,
+// To get rid of beam trips and current ramps, I use a landaun fit on the bcm1 value and then select 
+// events that fall within mean*0.97 and mean*1.03, where mean = most probable value from landaun fit.
+// In the case of mean < 2 (this happens when most of the run there was a beamtrip), those events are 
+// rejected.
+//
+// To use this script, compile and do,
 // ./get_from_runs run1 run2 device property
 //  device - Give the full name of the device as it appears in the mapfiles. e.g. qwk_bpm3h09b
 //  property - There are 4 properties that you can access with this script.
@@ -87,7 +93,7 @@ int main(Int_t argc,Char_t* argv[])
   gDirectory->Delete("*") ;
   gStyle->Reset();
   gStyle->SetOptTitle(1);
-  gStyle->SetOptFit(1111);
+  gStyle->SetOptFit(0);
   gStyle->SetStatColor(10);  
   gStyle->SetStatH(0.2);
   gStyle->SetStatW(0.1);     
@@ -133,9 +139,9 @@ int main(Int_t argc,Char_t* argv[])
   std::vector<TFile*> file_list;//List of TFile  to access histograms
   TString histoname = "";
   TString cut = "";
-  TCanvas *c1 = new TCanvas();
-  c1->Draw();
-  c1->cd();
+  TString counter = "";
+  TCanvas *c1 = new TCanvas("c1","c1",800,300);
+
   // Read in the arguments.
   // The first argument should be the program name itself.
   // The second argument should be the run1
@@ -163,48 +169,17 @@ int main(Int_t argc,Char_t* argv[])
     std::cout<<"Get mean and mean error for "<<property<<" of "<<device<<std::endl;
 
   }
-  std::cout<<"Enter current? enter 1 for yes and 0 for no"<<std::endl;
-  Int_t op = 0;
-  Double_t current = 0;
-  Double_t Ilcut = 0;
-  Double_t Iucut = 0;
-  std::cin>>op;
-  if(op == 1){
-    std::cout<<"current ~ "<<std::endl;
-    std::cin>>current;
-    Ilcut = current * 0.95;
-    Iucut = current*1.05;
-  }
 
-  // Now clear out what we want to get from where.
-  // Q:Which tree to access?
-  // Q:What histogram to get?
 
-  
- // Find the files from given run range.
-  Bool_t found = kFALSE;
-  Int_t k = 0;
-  TVectorD mean(0);
-  TVectorD sigma(0);
-  TVectorD error(0);
-  TVectorD runs(0);
-  TVectorD fakeerror(0);
-  std::vector<TString> labels;
-
-  TString filename = "";
-  file_list.clear();
-  labels.clear();
+  // assign proper tree variables
   TString currentcut;
-
   Char_t  treename[200];
+  TH1F* htemp1 = NULL;
 
-  for(Int_t run = run1; run< run2+1; run++){
- 
-    // first create chains
     if(property.Contains("d")||property.Contains("a")||property.Contains("y")){
-      tree = new TChain("Hel_Tree");
       sprintf(treename,"Hel_Tree");
       currentcut = "yield_qwk_bcm1.hw_sum";
+      counter = "pattern_number";
       if(property.Contains("y")){
 	histoname = Form("yield_%s.hw_sum>>htemp",device.Data());
 	cut = Form("yield_%s.Device_Error_Code == 0 && yield_qwk_bcm1.Device_Error_Code == 0",device.Data());
@@ -219,8 +194,8 @@ int main(Int_t argc,Char_t* argv[])
       }
     }
     else if (property.Contains("s")){
-      tree = new TChain("Mps_Tree");
       currentcut = "qwk_bcm1.hw_sum";
+      counter = "mps_counter";
       sprintf(treename,"Mps_Tree");
       histoname = Form("%s.hw_sum>>htemp",device.Data());
       cut = Form("%s.Device_Error_Code == 0 && qwk_bcm1.Device_Error_Code == 0",device.Data());
@@ -229,106 +204,155 @@ int main(Int_t argc,Char_t* argv[])
       std::cout<<"Requested unknown "<<property<<" from device "<<device<<".\n I don't know this"<<std::endl;
       exit(1);
     }
-    
 
-    // Open the file
+    // Find the files from given run range.
+    Bool_t found = kFALSE;
+    Int_t k = 0;
+    TVectorD mean(0);
+    TVectorD sigma(0);
+    TVectorD error(0);
+    TVectorD runs(0);
+    TVectorD fakeerror(0);
+    std::vector<TString> labels;
+    
+    TString filename = "";
     file_list.clear();
-    filename = "";
-    filename = Form("QwPass*_%i.*root", run);
-    found = FindFiles(filename, file_list, tree);
-    if(!found){
-      filename = Form("first*_%i.root", run);      
+    labels.clear();
+
+
+    for(Int_t run = run1; run< run2+1; run++){
+ 
+      //create chains
+      if(property.Contains("d")||property.Contains("a")||property.Contains("y")){
+	tree = new TChain("Hel_Tree");
+      }
+      else if (property.Contains("s")){
+	tree = new TChain("Mps_Tree");
+      }
+    
+      // Open the file
+      file_list.clear();
+      filename = "";
+      filename = Form("QwPass*_%i.*root", run);
       found = FindFiles(filename, file_list, tree);
       if(!found){
-	filename = Form("Qweak*_%i.*root", run);
+	filename = Form("first*_%i.root", run);      
 	found = FindFiles(filename, file_list, tree);
 	if(!found){
-	  std::cerr<<"Unable to find root file(s) for run "<<run<<std::endl;
-	  file_list.clear();
-	} 
+	  filename = Form("Qweak*_%i.*root", run);
+	  found = FindFiles(filename, file_list, tree);
+	  if(!found){
+	    std::cerr<<"Unable to find root file(s) for run "<<run<<std::endl;
+	    file_list.clear();
+	  } 
+	}
       }
-    }
-    // If there are no files in this range, nothing to plot. Move to next run.
-    if(file_list.size() == 0){
-      //do nithing.
-    }
-    else{
-      
+      // If there are no files in this range, nothing to plot. Move to next run.
+      if(file_list.size() == 0){
+	//do nithing.
+      }
+      else{
+	
       //Found rootfiles for this run, get the histogram info from them.
       TH1F* htemp = NULL;
-      TH1F* htemp1 = NULL;
+      TF1* fit = NULL;
+
       Double_t meanl = 0;
       Double_t errorl = 0;
       Double_t widthl = 0.0;
+      Bool_t ok = kTRUE;
+      Double_t cutmean = 0;     
+      Double_t lcut = 0;
+      Double_t ucut = 0;
 
       TString tcut;
 
       for(size_t i = 0;i<file_list.size();i++){
 	TFile* f = new TFile(file_list[i]->GetName()); 
+	std::cout << "\nFound file "<<f->GetName()<<std::endl; 
+
 	TTree* nt = (TTree*)(f->Get(treename));
 	if(!nt) exit(1);
-	nt->Draw(Form("%s>>htemp1",currentcut.Data()), cut);
-	htemp1 = (TH1F*)gDirectory->Get("htemp1");
 
+	// apply cuts for average current and current fluctuations.
+	// I fit bcm1 data with landaun fit to get the most probable current
+	// and then select events with currents ranging from mean+=3%.
+
+	nt->Draw(Form("%s>>htemp1",currentcut.Data()),cut);
+	htemp1 = (TH1F*)gDirectory->Get("htemp1");
 	if (!htemp1){
-	  std::cerr<<"Cannot apply current cuts of +- 10%"<<std::endl;
-	  tcut = cut;
-	} else{
-	  if(op == 1)
-	    tcut = Form("%s && %s>%f && %s < %f",cut.Data(),currentcut.Data(),
-			Ilcut, currentcut.Data(), Iucut);
+	  std::cerr<<"There are no bcm1 data in this segment"<<std::endl;
+	  ok = kFALSE;
+	}
+	else{
+
+	  htemp1->Fit("landaun","W I Q E M");
+	  fit = htemp1->GetFunction("landaun");
+	  cutmean = fit->GetParameter(1); //most probable value
+	  if(cutmean < 2)
+	    ok = kFALSE;
 	  else{
-	    Ilcut = htemp1->GetMean()*0.95;
-	    Iucut = htemp1->GetMean()*1.05;
-	    tcut = Form("%s && %s>%f && %s < %f",cut.Data(),currentcut.Data(),
-			Ilcut, currentcut.Data(), Iucut);
+	    lcut = cutmean*0.97; // 3% down
+	    ucut = cutmean*1.03; // 3% up
+	    tcut = Form("%s && %s > %f && %s < %f",
+			cut.Data(),currentcut.Data(),lcut,
+			currentcut.Data(),ucut);
+	    std::cout<<"###############\n";
+	    std::cout<<" Most probable current(uA) from landaun fit = "<<cutmean<<std::endl;
+	    std::cout<<" Select currents(uA) >"<<lcut<<" and <"<<ucut<<std::endl;
+	    std::cout<<"###############\n";
 	  }
 	}
-	//	std::cout<<tcut<<std::endl;
-	nt->Draw(histoname,tcut);
-	htemp = (TH1F*)gDirectory->Get("htemp");
-	if (!htemp){
-	  std::cerr<<"Histogram name does not exist"<<std::endl;
-	} else {
-	  meanl  =  htemp->GetMean();
-	  errorl =  htemp->GetMeanError();
-	  widthl =  htemp->GetRMS();
-	  if((meanl == 0) &&  (errorl == 0)){
-	    std::cout<<"histogram has zero mean and szero width!"<<std::endl;
-	  } 
-	  else{
-	    mean.ResizeTo(k+1);
-	    sigma.ResizeTo(k+1);
-	    error.ResizeTo(k+1);
-	    runs.ResizeTo(k+1);
-	    fakeerror.ResizeTo(k+1);
-	    
-	    if(property.Contains("a") || property.Contains("d")){
-	      if(device.Contains("md"))
-		mean.operator()(k) = meanl*1e+6 + 80; //put asymmetry in ppm and add a blinder to md asyms of 60ppm
-	      else
+	
+
+	if(ok){
+	  nt->Draw(histoname,tcut);
+	  htemp = (TH1F*)gDirectory->Get("htemp");
+	  if (!htemp){
+	    std::cerr<<"Histogram name does not exist"<<std::endl;
+	  } else {
+	    meanl  =  htemp->GetMean();
+	    errorl =  htemp->GetMeanError();
+	    widthl =  htemp->GetRMS();
+	    if((meanl == 0) &&  (errorl == 0)){
+	      std::cout<<"histogram has zero mean and szero width!"<<std::endl;
+	    } 
+	    else{
+	      mean.ResizeTo(k+1);
+	      sigma.ResizeTo(k+1);
+	      error.ResizeTo(k+1);
+	      runs.ResizeTo(k+1);
+	      fakeerror.ResizeTo(k+1);
+	      
+	      if(property.Contains("a") || property.Contains("d")){
+		if(device.Contains("md"))
+		  mean.operator()(k) = meanl*1e+6 + 80; //put asymmetry in ppm and add a blinder to md asyms of 60ppm
 		mean.operator()(k) = meanl*1e+6; //put ather asyms and diffs in ppm/nm. 
-	      error.operator()(k) =  errorl*1e+6;
-	      sigma.operator()(k)  = widthl*1e+6; // the width in root is called the RMS.
-	    
-	    }else{
-	      mean.operator()(k)  = meanl;
-	      error.operator()(k) = errorl;
-	      sigma.operator()(k) = widthl; // the width in root is called the RMS.
+		error.operator()(k) =  errorl*1e+6;
+		sigma.operator()(k)  = widthl*1e+6; // the width in root is called the RMS.
+	      }else{
+		mean.operator()(k)  = meanl;
+		error.operator()(k) = errorl;
+		sigma.operator()(k) = widthl; // the width in root is called the RMS.
+	      }
+	      runs.operator()(k)      = run+(i*0.1);
+	      labels.push_back(Form("%4.1f",runs[i]));
+	      fakeerror.operator()(k) = 0.0;
+	      htemp1->Fill(sigma[i]);
+	      
+	      k++;
 	    }
-	    runs.operator()(k)      = run+(i*0.1);
-	    labels.push_back(Form("%4.1f",runs[i]));
-	    fakeerror.operator()(k) = 0.0;
-	    k++;
 	  }
+	}
+	else {
+	  std::cout<<"Failed bcm1 RMS cut. Probebly a beam ramp/trip."<<std::endl;
 	}
       }
+
       delete tree;
     }
     
   }
-
-  c1->Close();
 
 
   // If all the available histograms have 0 netries, nothing to plot. exit.
@@ -336,7 +360,8 @@ int main(Int_t argc,Char_t* argv[])
     std::cerr<<"There are no histograms called "<<histoname<<" in this range ("<<run1<<"-"<<run2<<")!."<<std::endl;
     exit(1);
   }
-    
+  c1->Close();
+
   // Create a graph and a histogram using these information.
   TGraphErrors* g  = new TGraphErrors(runs, mean, fakeerror, error);
   TGraph* g1       = new TGraph(runs, sigma);
@@ -359,12 +384,6 @@ int main(Int_t argc,Char_t* argv[])
   g->GetYaxis()->SetTitle(Form("%s %s",device.Data(), prop.Data()));
   g->GetXaxis()->SetTitle("run number");
 
-
-//   Int_t nbins = g  // draw labels along X
-// h->GetXaxis()->GetNbins();
-//   std::cout<<nbins<<std::endl;
-//   for(size_t i=0;i<labels.size();i++)
-//     gh -> GetXaxis()->SetBinLabel(i+1,labels[i]);
 
   g->SetMarkerColor(kBlue);
   g->SetMarkerStyle(21);
@@ -394,35 +413,14 @@ int main(Int_t argc,Char_t* argv[])
   pad2->cd(1);
   g->Draw("APE1");
 
-//   // draw labels along X
-//   TH1F* gh = g->GetHistogram();
-//   gh->GetXaxis()->SetLabelOffset(99);
-//   gh->GetYaxis()->SetLabelOffset(99);
-//   std::cout<<"here\n";
-//   std::cout<<gPad->GetUymin();
-//   Float_t xaxis = 0, yaxis = 0;
-//   yaxis = gPad->GetUymin()- 0.2*(gh->GetYaxis()->GetBinWidth(1));
-
-//   TText tl;
-//   tl.SetTextAngle(60);
-//   tl.SetTextSize(0.02);
-//   tl.SetTextAlign(33);
-//   for(size_t i=0;i<labels.size();i++) {
-//     xaxis = gh->GetXaxis()->GetBinCenter(i+1);
-//     tl.DrawText(xaxis,yaxis,labels[i]);
-//   }
-  
-//   g->Draw("APE1");
-
   pad2->cd(2);
-  //  gPad->SetLogy();
   g1->Draw("ABP");
   g1->GetYaxis()->SetTitle(Form("%s %s widths",device.Data(), prop.Data()));
   canvas->Update();
+  c1->Update();
   canvas->SetBorderMode(0);
-  // Save the canvas on to a .gif file
-  canvas->SaveAs(Form("$QWANALYSIS/Extensions/Macros/Parity/Plots/%i_%i_%s_%s_summary.gif",run1, run2, device.Data(),property.Data())); 
-  //   theApp.Run(); 
+ 
+  //  theApp.Run(); 
   Int_t l = 0;
   std::cin>>l;
 };
@@ -449,8 +447,7 @@ Bool_t FindFiles(TString filename, std::vector<TFile*> &atfile_list, TChain * ch
     while (( chain_element=(TChainElement*)next() )){
       TFile *f=new TFile(chain_element->GetTitle());
       atfile_list.push_back(f);
-      std::cout << "Found file:  ";
-      f->Print(); 
+      // std::cout << "Found file "<<f->GetName()<<std::endl; 
     }
   }
 
