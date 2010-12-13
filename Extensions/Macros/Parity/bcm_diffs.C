501ee111df58bb6777d7825bf2a853dfcf24bf2e
pking <pking@b9700a98-9937-0410-b777-f318d6eb5bff>
2010-12-13 18:05:35 +0000

    Added additional double differences with the BPM effective charges to the bcm_diffs script.
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@2324 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Macros/Parity/bcm_diffs.C
+++ Extensions/Macros/Parity/bcm_diffs.C
@@ -6,7 +6,7 @@
 bcm_diffs(const int run = 8100, 
 	  const int first = -1, const int last = -1) {
 
- const char* file = "QwPass1_%i.*.root";
+ const char* file = "$QW_ROOTFILES/QwPass1_%i.*.root";
 
  TChain chain("Hel_Tree");
  if (0 == chain.Add(Form(file, run)) ) {
@@ -22,6 +22,8 @@ bcm_diffs(const int run = 8100,
  const char* tc2 = "BCM yield vs. pattern counter, run %d";
  const char* tc3 = "BCM asymmetries, run %d";
  const char* tc4 = "BCM 1 vs. BCM 2, run %d";
+ const char* tc5 = "BCM & BPM asymmetry differences, run %d";
+ const char* tc6 = "BCM & BPM correlations, run %d";
 
  TCanvas *c1 = new TCanvas("c1",Form(tc1,run),0,0,1024,768);
 c1->Divide(2,3);
@@ -38,9 +40,30 @@ chain . Draw("(asym_qwk_bcm2-asym_qwk_bcm6)*1e6",cut);
 c1->cd(6);
 chain . Draw("(asym_qwk_bcm5-asym_qwk_bcm6)*1e6",cut);
 
+ TCanvas *c5 = new TCanvas("c5",Form(tc5,run),0,0,1024,768);
+c5->Divide(2,5);
+c5->cd(1);
+chain . Draw("(asym_qwk_bcm1-asym_qwk_bpm3h09b_EffectiveCharge)*1e6",cut);
+c5->cd(2);
+chain . Draw("(asym_qwk_bcm1-asym_qwk_bpm3c12_EffectiveCharge)*1e6",cut);
+c5->cd(3);
+chain . Draw("(asym_qwk_bcm2-asym_qwk_bpm3h09b_EffectiveCharge)*1e6",cut);
+c5->cd(4);
+chain . Draw("(asym_qwk_bcm2-asym_qwk_bpm3c12_EffectiveCharge)*1e6",cut);
+c5->cd(5);
+chain . Draw("(asym_qwk_bcm5-asym_qwk_bpm3h09b_EffectiveCharge)*1e6",cut);
+c5->cd(6);
+chain . Draw("(asym_qwk_bcm5-asym_qwk_bpm3c12_EffectiveCharge)*1e6",cut);
+c5->cd(7);
+chain . Draw("(asym_qwk_bcm6-asym_qwk_bpm3h09b_EffectiveCharge)*1e6",cut);
+c5->cd(8);
+chain . Draw("(asym_qwk_bcm6-asym_qwk_bpm3c12_EffectiveCharge)*1e6",cut);
+c5->cd(9);
+chain . Draw("(asym_qwk_bpm3h09b_EffectiveCharge-asym_qwk_bpm3c12_EffectiveCharge)*1e6",cut);
+
 
  TCanvas *c2 = new TCanvas("c2",Form(tc2,run),0,0,1024,768);
-c2->Divide(2,2);
+c2->Divide(2,3);
 c2->cd(1);
 chain . Draw("yield_qwk_bcm1:pat_counter",cut);
 c2->cd(2);
@@ -49,9 +72,13 @@ c2->cd(3);
 chain . Draw("yield_qwk_bcm5:pat_counter",cut);
 c2->cd(4);
 chain . Draw("yield_qwk_bcm6:pat_counter",cut);
+c2->cd(5);
+chain . Draw("yield_qwk_bpm3h09b_EffectiveCharge:pat_counter",cut);
+c2->cd(6);
+chain . Draw("yield_qwk_bpm3c12_EffectiveCharge:pat_counter",cut);
 
  TCanvas *c3 = new TCanvas("c3",Form(tc3,run),0,0,1024,768);
-c3->Divide(2,2);
+c3->Divide(2,3);
 c3->cd(1);
 chain . Draw("asym_qwk_bcm1*1e6",cut);
 c3->cd(2);
@@ -60,6 +87,10 @@ c3->cd(3);
 chain . Draw("asym_qwk_bcm5*1e6",cut);
 c3->cd(4);
 chain . Draw("asym_qwk_bcm6*1e6",cut);
+c3->cd(5);
+chain . Draw("asym_qwk_bpm3h09b_EffectiveCharge*1e6",cut);
+c3->cd(6);
+chain . Draw("asym_qwk_bpm3c12_EffectiveCharge*1e6",cut);
 
  TCanvas *c4 = new TCanvas("c4",Form(tc4,run),0,0,1024,768);
 gStyle->SetOptFit(1);
@@ -67,13 +98,57 @@ c4->Divide(1,2);
 c4->cd(1);
 chain . Draw("asym_qwk_bcm1:asym_qwk_bcm2",cut,"");
 c4->cd(2);
-
 chain . Draw("asym_qwk_bcm1:asym_qwk_bcm2>>h1",cut,"prof");
 //h1->Fit("pol1","","",-0.02e-3,0.02e-3);
  TProfile *h1 = gDirectory->FindObject("h1");
- double fitlo = h1->GetMean() - h1->GetRMS();
- double fithi = h1->GetMean() + h1->GetRMS();
+ double fitlo = h1->GetMean() - 2*h1->GetRMS();
+ double fithi = h1->GetMean() + 2*h1->GetRMS();
  h1->Fit("pol1","","", fitlo, fithi);
+
+ TCanvas *c6 = new TCanvas("c6",Form(tc6,run),0,0,1024,768);
+ TProfile *myprof;
+ c6->Divide(2,5);
+ c6->cd(1);
+ chain . Draw("asym_qwk_bcm1:asym_qwk_bpm3h09b_EffectiveCharge",cut);
+ c6->cd(2);
+ chain . Draw("asym_qwk_bcm1:asym_qwk_bpm3h09b_EffectiveCharge>>prof1",cut,"prof");
+ myprof = (TProfile*)gDirectory->FindObject("prof1");
+ double fitlo = myprof->GetMean() - 2*myprof->GetRMS();
+ double fithi = myprof->GetMean() + 2*myprof->GetRMS();
+ myprof->Fit("pol1","","", fitlo, fithi);
+ c6->cd(3);
+ chain . Draw("asym_qwk_bcm2:asym_qwk_bpm3h09b_EffectiveCharge",cut);
+ c6->cd(4);
+ chain . Draw("asym_qwk_bcm2:asym_qwk_bpm3h09b_EffectiveCharge>>prof2",cut,"prof");
+ myprof = (TProfile*)gDirectory->FindObject("prof2");
+ double fitlo = myprof->GetMean() - 2*myprof->GetRMS();
+ double fithi = myprof->GetMean() + 2*myprof->GetRMS();
+ myprof->Fit("pol1","","", fitlo, fithi);
+ c6->cd(5);
+ chain . Draw("asym_qwk_bcm5:asym_qwk_bpm3h09b_EffectiveCharge",cut);
+ c6->cd(6);
+ chain . Draw("asym_qwk_bcm5:asym_qwk_bpm3h09b_EffectiveCharge>>prof5",cut,"prof");
+ myprof = (TProfile*)gDirectory->FindObject("prof5");
+ double fitlo = myprof->GetMean() - 2*myprof->GetRMS();
+ double fithi = myprof->GetMean() + 2*myprof->GetRMS();
+ myprof->Fit("pol1","","", fitlo, fithi);
+ c6->cd(7);
+ chain . Draw("asym_qwk_bcm6:asym_qwk_bpm3h09b_EffectiveCharge",cut);
+ c6->cd(8);
+ chain . Draw("asym_qwk_bcm6:asym_qwk_bpm3h09b_EffectiveCharge>>prof6",cut,"prof");
+ myprof = (TProfile*)gDirectory->FindObject("prof6");
+ double fitlo = myprof->GetMean() - 2*myprof->GetRMS();
+ double fithi = myprof->GetMean() + 2*myprof->GetRMS();
+ myprof->Fit("pol1","","", fitlo, fithi);
+ c6->cd(9);
+ chain . Draw("asym_qwk_bpm3c12_EffectiveCharge:asym_qwk_bpm3h09b_EffectiveCharge",cut);
+ c6->cd(10);
+ chain . Draw("asym_qwk_bpm3c12_EffectiveCharge:asym_qwk_bpm3h09b_EffectiveCharge>>profbpm",cut,"prof");
+ myprof = (TProfile*)gDirectory->FindObject("profbpm");
+ double fitlo = myprof->GetMean() - 2*myprof->GetRMS();
+ double fithi = myprof->GetMean() + 2*myprof->GetRMS();
+ myprof->Fit("pol1","","", fitlo, fithi);
+
 
  // c1 -> SaveAs("c1.png"); c2 -> SaveAs("c2.png"); c3 -> SaveAs("c3.png"); c4 -> SaveAs("c4.png"); 
 }
