95e180a30e2e10a1a6d280254356874bcfcc3e90
jhlee <jhlee@b9700a98-9937-0410-b777-f318d6eb5bff>
2011-01-05 14:01:07 +0000

    - added "tree-based" plots (seperated plane and package)
    Wednesday, January  5 09:00:54 EST 2011, jhlee
    
    
    
    
    
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@2356 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Macros/Tracking/all_plots_hdc.C
+++ Extensions/Macros/Tracking/all_plots_hdc.C
@@ -1,4 +1,4 @@
-
+//
 // Author : Jeong Han Lee / John leacock
 // Date   : Sunday, December  5 21:31:38 EST 2010
 // 
@@ -7,29 +7,22 @@
 //                 - copied John script to modify them
 //                   to fit plot_HDC
 //
-
+//          0.0.2 " Tuesday, January  4 16:08:42 EST 2011, jhlee
+//                 - extended to check the different planes
+//        
 #include "TSystem.h"
 #include "TString.h"
+#include "TTree.h"
+#include "TCut.h"
+
+TTree *event_tree;
 
 void 
-all_plots_hdc(TString name="")
+plot_histo(TString name)
 {
-  TString file_dir;
-  file_dir = gSystem->Getenv("QWSCRATCH");
-  file_dir += "/rootfiles/";
- 
-  if(name.IsNull()) {
-    printf("There is no root file\n");
-    exit(-1);
-  }
-  else {
-    TFile *file =  new TFile(Form("%s%s", file_dir.Data(), name.Data()));
-  }
-
-  gStyle->SetPalette(1); 
   tracking_histo->cd();
 
-  TCanvas *c1 = new TCanvas(name.Data(), name.Data(), 1100, 700);
+  TCanvas *c1 = new TCanvas(Form("histograms_%s", name.Data()), Form("R2 HitOnEachWire in %s", name.Data()), 1100, 700);
   c1->Divide(4,3);
   c1->cd(1);
   R2HitsOnEachWirePlane1->Draw();
@@ -56,5 +49,61 @@ all_plots_hdc(TString name="")
   c1->cd(12);
   R2HitsOnEachWirePlane12->Draw();
 
-}
+  return;
+};
+  
+
+void 
+plot_hits(TString name)
+{
+  
+  TCut region2 = "fRegion==2";
+
+  TCanvas *c2 = new TCanvas(Form("hits_%s", name.Data()), Form("R2 Hits in %s", name.Data()), 1100, 700);
+
+  c2->Divide(6,4);
+
+  Int_t idx = 0;
+  Int_t package = 0;
+  for (Int_t i=0;i<24;i++)
+    {
+      idx = i+1;
+      c2->cd(idx);
+      if(i < 12) { package = 1; }
+      else       { package = 2; idx -= 12;}
+
+      TCut cut = Form("fPackage==%d&&fPlane==%d", package, idx);
+      cut+=region2;
+      event_tree->Draw("fElement",  cut);
+    }
+
+  return;
+};
+  
+
+
+void 
+all_plots_hdc(TString name="")
+{
+  TString file_dir;
+  file_dir = gSystem->Getenv("QWSCRATCH");
+  file_dir += "/rootfiles/";
+ 
+  if(name.IsNull()) {
+    printf("There is no root file\n");
+    exit(-1);
+  }
+  else {
+    TFile *file =  new TFile(Form("%s%s", file_dir.Data(), name.Data()));
+  }
+
+  gStyle->SetPalette(1); 
+
+  event_tree = (TTree*) file->Get("event_tree");
+
+  plot_histo(name);
+  plot_hits(name);
+
+  return;
+};
 
