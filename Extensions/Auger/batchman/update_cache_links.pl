7a54fb92cff4256464f36a963bd87173006ca4e9
wdconinc <wdconinc@b9700a98-9937-0410-b777-f318d6eb5bff>
2011-02-25 02:12:04 +0000

    Added two command line arguments to the update_cache_links perl script.  It now accepts -s <stem> to specify the stem for the link files -r <regex> to specify the regex for the cached data files
    Both of those changes were motivated by the Compton: we use Compton_<run>.dat.<segment>,
    and the Moller data files are cached in the same directory as the Compton data files, so
    we want to avoid conflicts by using a regex.  The shell script can be used to reuse those
    arguments repeatedly.
    
    
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@2571 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Auger/batchman/update_cache_links.pl
+++ Extensions/Auger/batchman/update_cache_links.pl
@@ -12,7 +12,7 @@ use File::Basename;
 use Getopt::Std;
 
 ###  Get the option flags.
-getopts('hS:');
+getopts('hr:s:S:');
 
 if ($opt_h){
     displayusage();
@@ -23,6 +23,18 @@ if($opt_S ne ""){
     $source_directory = $opt_S;
 } else {
     $source_directory = "/cache/mss/hallc/qweak/raw/";
+}
+
+if($opt_s ne ""){
+    $link_stem = $opt_s;
+} else {
+    $link_stem = "QwRun_";
+}
+
+if($opt_r ne ""){
+    $regex = $opt_r;
+} else {
+    $regex = ".*";
 }
 
 ###  Get the link file directory for the analyzer.
@@ -54,7 +66,7 @@ foreach $linkfile (`find $link_directory -type l`){
 
 
 ###  Make links for the files in the source directory.
-foreach $sourcefile (`find $source_directory -maxdepth 1`){
+foreach $sourcefile (`find $source_directory -maxdepth 1 -regex $regex`){
     chomp $sourcefile;
     next if (! -f $sourcefile);
     #  Extract the run number and sgment number.
@@ -63,9 +75,9 @@ foreach $sourcefile (`find $source_directory -maxdepth 1`){
     ($prename,$segment) = split /\.dat\.?/, $base_name, 2;
     $run_number = substr $prename, (rindex $prename, "_")+1;
     if ($segment ne ""){
-	$link_path = "$link_directory/QwRun_$run_number.log.$segment";
+	$link_path = "$link_directory/$link_stem$run_number.log.$segment";
     } else {
-	$link_path = "$link_directory/QwRun_$run_number.log";
+	$link_path = "$link_directory/$link_stem$run_number.log";
     }
     if (-l $link_path){
 	#  The link file already exists.
@@ -85,14 +97,18 @@ sub displayusage {
 	"update_cache_links.pl is a tool to create the Qweak analysis data links\n",
 	"given a list of raw data files.\n\n",
 	"Usage:\n\tupdate_cache_links.pl -h\n",
-	"\tupdate_cache_links.pl [-S <source directory>]\n\n",
+	"\tupdate_cache_links.pl [-S <source directory>] [-s <stem>] [-r <regex>]\n\n",
 	"Options:\n",
 	"\t-h\n",
 	"\t\tPrint usage information\n",
 	"\t-S <source directory>\n",
-	"\t\tThis flag  specifies the  directory which  contains\n",
-	"\t\tthe raw data files.   The default is to  use the Qweak\n",
-	"\t\traw cache directory.\n";
+	"\t\tThis flag  specifies the directory which contains\n",
+	"\t\tthe raw data files.  The default is to use the Qweak\n",
+	"\t\traw cache directory.\n",
+	"\t-s <stem>\n",
+	"\t\tThis flag specifies the stem that the data links will get\n",
+	"\t-r <regex>\n",
+	"\t\tThis flag specifies the regex that the data file has to conform to\n";
 }
 
 ################################################
