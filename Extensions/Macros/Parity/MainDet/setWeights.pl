b49fe9a59e8319333da3c90472f310db696ae684
smacewan <smacewan@b9700a98-9937-0410-b777-f318d6eb5bff>
2011-02-28 20:27:35 +0000

    Added in some mapfiles with weightings appropriate for some more runs from 2010. I decided that insertion/removal of the octant 7 plug, as well as any high voltages changes, warrented a new set of weights. I also fixed up the setWeights script a bit with some helpful suggestions from Rob (thanks!)
    For those following the weighting saga. Note that the trunk version of the analyzer DOES have weights from the "new" scheme wherein we weight solely by yields and not asymmetry widths as well.
    
    The over-attentive collaborator might notice a drastic change in the magnitude of the weights. On this point I would remind him/her that the analyser normalizes by the sum of the weights and that it is the relative size of weights between tubes that matters and not the absolute scale.
    
    
    
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@2583 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Macros/Parity/MainDet/setWeights.pl
+++ Extensions/Macros/Parity/MainDet/setWeights.pl
@@ -1,14 +1,18 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
+use strict;
+use warnings;
 
 # setWeights.pl -- smacewan
 # 
 # setWeights takes in as a single argument a string containing the complete filename of
-# a file containing main detector weights formatted as:
+# a file containing main detector yields formatted as:
 #
 # Run   9888
-# MD1POS  3.447e-05
-# MD1NEG  3.111e-05
+# MD1POS  0.0343
+# MD1NEG  0.0354
 # ...
+#
+# It is important to NOT HAVE A NEWLINE at the bottom of the file. The last line should be the entry for a tube.
 #
 # It will create a file in $QWANALYSIS/Parity/prminput/ called qweak_maindet.RUNNUM-.map where RUNNUM
 # is the first run from which those weightings are appropriate.
@@ -17,10 +21,11 @@
 # 	perl setWeights.pl "~/users/smacewan/weights.txt" 
 #
 
-$QWANALYSIS = $ENV{QWANALYSIS};
-$inFile = "$QWANALYSIS/Parity/prminput/qweak_maindet.map";
+my $QWANALYSIS = $ENV{QWANALYSIS};
+my $inFile = "$QWANALYSIS/Parity/prminput/qweak_maindet.map";
 
-%weight;
+my %weight;
+my $runNum;
 
 # read in parameter file that holds the new weights
 open(WEIGHTFILE,$ARGV[0]) or die "Cannot open weight file.";
@@ -31,21 +36,21 @@ while(my $line = <WEIGHTFILE>){
 	}
 	else{
 		my $w =  1 / $splitLine[1];
-		$weight{"$splitLine[0]"} = $w;
+		$weight{$splitLine[0]} = $w;
 	}
 }
-close PARFILE;
+close WEIGHTFILE;
 
 #open default and new file
-$outFile = "$QWANALYSIS/Parity/prminput/qweak_maindet.$runNum-.map";
+my $outFile = "$QWANALYSIS/Parity/prminput/qweak_maindet.$runNum-.map";
 
-open(INFILE,$inFile) or die "Cannot open default file $infile";
+open(INFILE,$inFile) or die "Cannot open default file $inFile";
 open(OUTFILE,">$outFile") or die "Cannot make new file";
 
 while(my $line = <INFILE>){
 	if($line =~ /VPMT/){
 		#this is a CombinationPMT definition line
-		my @splitLine = ($class,$chan,$n,$type,$name,@data) = split(/\s*,\s*/,$line);
+		my @splitLine = (my $class,my $chan,my $n,my $type,my $name,my @data) = split(/\s*,\s*/,$line);
 		
 		#print out pmt definition headers
 		print OUTFILE "$class, $chan, $n, $type, $name, ";
