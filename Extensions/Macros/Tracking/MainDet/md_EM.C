44c12fad7fc8a72cfdd70870e983fa7195b90743
leacock <leacock@b9700a98-9937-0410-b777-f318d6eb5bff>
2011-03-11 23:03:49 +0000

    Minor update for the main detector event mode script.  RC asked to have two canvases, one with suppressed zeros and one without.
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@2640 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Macros/Tracking/MainDet/md_EM.C
+++ Extensions/Macros/Tracking/MainDet/md_EM.C
@@ -10,14 +10,14 @@ const string md_dets[num_dets]={
 "maindet.md6p_adc",
 "maindet.md7p_adc",
 "maindet.md8p_adc",
-"maindet.md1n_adc",
-"maindet.md2n_adc",
-"maindet.md3n_adc",
-"maindet.md4n_adc",
-"maindet.md5n_adc",
-"maindet.md6n_adc",
-"maindet.md7n_adc",
-"maindet.md8n_adc"
+"maindet.md1m_adc",
+"maindet.md2m_adc",
+"maindet.md3m_adc",
+"maindet.md4m_adc",
+"maindet.md5m_adc",
+"maindet.md6m_adc",
+"maindet.md7m_adc",
+"maindet.md8m_adc"
 };
 
 
@@ -35,15 +35,30 @@ for (int i=0;i<num_dets;i++)
 	h[i] = new TH1F(Form("h_%i",i),"",100,0,0);
 	h[i]->SetDirectory(0);
 	gPad->SetLogy(1);
-//	event_tree->Draw(Form("%s>>h_%i(100,0,4096)",md_dets[i],i),Form("%s!=0",md_dets[i]));
 	event_tree->Draw(Form("%s>>h_%i(100,0,4096)",md_dets[i],i),Form("%s!=0",md_dets[i]));
 //	event_tree->Draw(Form("%s>>h_%i",md_dets[i],i));
 	TH1F *h_temp = (TH1F*)gPad->GetPrimitive(Form("h_%i",i));
-//	h_temp->SetRange(0,4096);
-//	h_temp->Draw();
 	c1->Update();
 };
 
+
+TCanvas *c2 = new TCanvas("c2","",1100,900);
+
+TH1F *h2[num_dets];
+
+c2->Divide(4,4);
+
+for (int i=0;i<num_dets;i++)
+{  
+	c2->cd(i+1);
+	h2[i] = new TH1F(Form("h2_%i",i),"",100,0,0);
+	h2[i]->SetDirectory(0);
+	gPad->SetLogy(1);
+//	event_tree->Draw(Form("%s>>h2_%i(100,0,4096)",md_dets[i],i),Form("%s!=0",md_dets[i]));
+	event_tree->Draw(Form("%s>>h2_%i(100,0,4096)",md_dets[i],i));
+	TH1F *h2_temp = (TH1F*)gPad->GetPrimitive(Form("h2_%i",i));
+	c2->Update();
+};
 
 
 
