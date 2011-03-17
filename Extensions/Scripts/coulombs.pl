e87e6ac9418913767c21ef8639ee3b613ab4eaa4
rob <rob@b9700a98-9937-0410-b777-f318d6eb5bff>
2011-03-17 17:03:09 +0000

    A Coulomb counter.  Accesses the Hall C EPICS archiver and downloads a "spreadsheet" of ibcm1 (beam current) and g0rate14 (DAQ trigger rate, after mid-Feburary).  Integrates beam current while DAQ rate is near 960 Hz.
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@2675 b9700a98-9937-0410-b777-f318d6eb5bff

---
+++ Extensions/Scripts/coulombs.pl
@@ -0,0 +1,81 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Date::Manip;
+use Scalar::Util qw[looks_like_number];
+
+my %coulombs;
+my @shift = qw[owl day swing];
+
+my @wget = qw[wget --no-check-certificate -q -O-];
+my $baseurl = "https://hallcweb.jlab.org/~cvxwrks/cgi/CGIExport.cgi";
+my $interval = 10; # interpolation time in seconds
+my @channels = qw[ibcm1 g0rate14];
+
+my $then = ParseDate(shift @ARGV);
+my $now  = ParseDate(shift @ARGV);
+unless (defined $then) { $then = ParseDate("yesterday midnight"); }
+unless (defined $now ) { $now  = DateCalc($then, "+48 hours"); }
+
+# don't interpolate a few seconds into the next day
+$now = DateCalc $now, "-$interval seconds";
+
+my @fmt = qw[%Y %m %d %H %M %S];
+my @then = UnixDate($then, @fmt);
+my @now  = UnixDate($now , @fmt);
+
+#print "@then, @now\n";
+
+my %args = (
+	DIRECTORY	=> "..%2FArchives%2FArchiveData%2Ffreq_directory",
+	PATTERN		=> "",
+	NAMES		=> "@channels",
+	STARTMONTH	=> $then[1],
+	STARTDAY	=> $then[2],
+	STARTYEAR	=> $then[0],
+	STARTHOUR	=> $then[3],
+	STARTMINUTE	=> $then[4],
+	STARTSECOND	=> $then[5],
+	ENDMONTH	=> $now[1],
+	ENDDAY		=> $now[2],
+	ENDYEAR		=> $now[0],
+	ENDHOUR		=> $now[3],
+	ENDMINUTE	=> $now[4],
+	ENDSECOND	=> $now[5],
+	COMMAND		=> "GET",
+	FORMAT		=> "SPREADSHEET",
+	Y0		=> "0",
+	Y1		=> "0",
+	INTERPOL	=> $interval,
+	   );
+
+my $url = $baseurl . "?" . join "&", map { "$_=$args{$_}" } sort keys %args;
+
+open DATA, "-|", @wget, $url
+  or die "couldn't open wget: $!\n";
+while (<DATA>) {
+  my($day, $time, $ibcm1, $daqrate) = split ' ';
+
+  # skip ugly lines
+  next if /^[;T]/ or m[N/A];
+  next unless looks_like_number $ibcm1 and looks_like_number $daqrate;
+
+  my($hour,$minute,$second) = split /:/, $time;
+  my $shift = int($hour/8);
+
+  if ( $ibcm1>0 and abs($daqrate - 960) < 10 ) {
+    $coulombs{$day}[$shift] += $ibcm1 * 1e-6 * $interval;
+  }
+
+}
+
+unless ($.) { warn "warning: fetched no data.  url was\n\t$url\n" }
+
+### Parse this block after reaching the end of the input file
+# print "Read $. lines from archiver\n";
+foreach my $day (sort keys %coulombs) {
+  foreach my $s (grep { ($coulombs{$day}[$_] += 0) > 0.01} 0..2) {
+    printf "$day %-5s: %0.2f C\n", $shift[$s], $coulombs{$day}[$s];
+  }
+}
