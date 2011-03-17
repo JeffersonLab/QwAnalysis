f40ad9c308037a3e2a7d9b5b03720953c57727fc
rob <rob@b9700a98-9937-0410-b777-f318d6eb5bff>
2011-03-17 17:03:22 +0000

    Add an option requested by Jean-Francois.  Add documentation.
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@2676 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Scripts/coulombs.pl
+++ Extensions/Scripts/coulombs.pl
@@ -4,6 +4,14 @@ use warnings;
 
 use Date::Manip;
 use Scalar::Util qw[looks_like_number];
+use Getopt::Long;
+
+################################################################
+## A quick-n-dirty, but not useless, Coulomb counter.
+## See definition of $helpstring below for documentation.
+##
+## Rob Mahurin rob@jlab.org and Josh Magee magee@jlab.org
+## 2011-03-17
 
 my %coulombs;
 my @shift = qw[owl day swing];
@@ -12,6 +20,12 @@ my @wget = qw[wget --no-check-certificate -q -O-];
 my $baseurl = "https://hallcweb.jlab.org/~cvxwrks/cgi/CGIExport.cgi";
 my $interval = 10; # interpolation time in seconds
 my @channels = qw[ibcm1 g0rate14];
+
+my ($help,$nodaqcut);
+my $optstatus = GetOptions
+  "help|h|?"	=> \$help,
+  "no-daq-cut"	=> \$nodaqcut,
+;
 
 my $then = ParseDate(shift @ARGV);
 my $now  = ParseDate(shift @ARGV);
@@ -26,6 +40,23 @@ my @then = UnixDate($then, @fmt);
 my @now  = UnixDate($now , @fmt);
 
 #print "@then, @now\n";
+
+my $helpstring = <<EOF;
+A Coulomb counter.  Accesses the Hall C EPICS archiver and downloads
+an interpolated "spreadsheet" of ibcm1 (beam current) and g0rate14
+(DAQ trigger rate, after mid-Feburary).  Integrates beam current while
+DAQ rate is near 960 Hz.
+Calling syntax:
+	coulombs.pl [options] ['start date' ['end date']]
+For example:
+	coulombs.pl			# integrate yesterday and today
+	coulombs.pl 'march 1'		# integrate March 1 and March 2
+	coulombs.pl 'last sunday' 'today' # does what you'd like
+Options:
+	--help		print this text
+	--no-daq-cut	integrate /all/ the time, ignore g0rate14
+EOF
+die $helpstring if $help;
 
 my %args = (
 	DIRECTORY	=> "..%2FArchives%2FArchiveData%2Ffreq_directory",
@@ -56,6 +87,8 @@ open DATA, "-|", @wget, $url
   or die "couldn't open wget: $!\n";
 while (<DATA>) {
   my($day, $time, $ibcm1, $daqrate) = split ' ';
+
+  $daqrate = 960 if $nodaqcut;
 
   # skip ugly lines
   next if /^[;T]/ or m[N/A];
