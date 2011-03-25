0f79c4d5073a1415f0a18e98c1bae009f80b1c95
smacewan <smacewan@b9700a98-9937-0410-b777-f318d6eb5bff>
2011-03-25 15:01:27 +0000

    Bug fixes for md macros to make them more 64-bit friendly so they can be run on the ifarm. Replaced all strings with TStrings and referenced them with .Data() where required.
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@2717 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Macros/Parity/MainDet/md_asyms.C
+++ Extensions/Macros/Parity/MainDet/md_asyms.C
@@ -4,16 +4,16 @@
 #include <stdlib.h>
 
 
-void md_asyms(int runNum){
+void md_asyms(Int_t runNum){
 gROOT->Reset();
 gROOT->SetStyle("Plain");
 
 Bool_t save_file = kTRUE;
 
-const string cut1 = "mps_counter>1e3";
-const string cut2 = "mps_counter<500e3";
+//const string cut1 = "mps_counter>1e3";
+//const string cut2 = "mps_counter<500e3";
 
-const string md[16] ={
+const TString md[16] ={
 "qwk_md1neg","qwk_md2neg","qwk_md3neg","qwk_md4neg",
 "qwk_md5neg","qwk_md6neg","qwk_md7neg","qwk_md8neg",
 "qwk_md1pos","qwk_md2pos","qwk_md3pos","qwk_md4pos",
@@ -27,7 +27,7 @@ const string md[8] ={
 
 Char_t filename[100];
 
-sprintf(filename,"$QW_ROOTFILES/first100k_%i.root",runNum);
+sprintf(filename,"$QW_ROOTFILES/first100k_stdWeights_%i.root",runNum);
 f = new TFile(filename);
 if(!f->IsOpen()){
 	std::cerr<<"Error opening ROOTFile "<<filename<<".\n"<<endl;
@@ -47,12 +47,12 @@ mc_md->Divide(4,4);
 
   for (Int_t p=0; p < 16; p++)
     {           
-      mdHist[p] = new TH1F(Form("%i_h_%s",runNum,md[p]),"",100,0,0);
+      mdHist[p] = new TH1F(Form("%i_h_%s",runNum,md[p].Data()),"",100,0,0);
       mc_md->cd(p+1);
       mdHist[p]->SetDirectory(0);     
-      hel_tree->Draw(Form("asym_%s>>run%i_h_%s",md[p],runNum,md[p]),Form("%s && %s && %s.Device_Error_Code==0",cut1,cut2, md[p]));
-      TH1F *htemp = (TH1F*)gPad->GetPrimitive(Form("run%i_h_%s",runNum,md[p]));
-      std::cout<<htemp->GetMean()<<" "<<htemp->GetRMS()<<std::endl;
+      hel_tree->Draw(Form("1e6*asym_%s>>run%i_h_%s",md[p].Data(),runNum,md[p].Data()),Form("asym_%s.Device_Error_Code==0",md[p].Data()));
+      TH1F *htemp = (TH1F*)gPad->GetPrimitive(Form("run%i_h_%s",runNum,md[p].Data()));
+      std::cout<<md[p].Data()<<" "<<htemp->GetMean()<<" "<<htemp->GetRMS()<<std::endl;
 
      }
 
--- Extensions/Macros/Parity/MainDet/md_yields.C
+++ Extensions/Macros/Parity/MainDet/md_yields.C
@@ -16,7 +16,7 @@ const string cut2 = "mps_counter>1000";
 //const string cut3 = "qwk_md1pos>0.005 && qwk_md1neg>0.005";
 double mean[16],rms[16];
 
- const string md[16] ={
+ const TString md[16] ={
    "qwk_md1neg","qwk_md2neg","qwk_md3neg","qwk_md4neg",
    "qwk_md5neg","qwk_md6neg","qwk_md7neg","qwk_md8neg",
    "qwk_md1pos","qwk_md2pos","qwk_md3pos","qwk_md4pos",
@@ -43,13 +43,13 @@ double mean[16],rms[16];
  
  for (Int_t p=0; p < 16; p++)
    {           
-      mdHist[p] = new TH1F(Form("%i_h_%s",runNum,md[p]),"",100,0,0);
+      mdHist[p] = new TH1F(Form("%i_h_%s",runNum,md[p].Data()),"",100,0,0);
       mc_md->cd(p+1);
       mdHist[p]->SetDirectory(0);     
-      mps_tree->Draw(Form("%s.hw_sum>>run%i_h_%s",md[p],runNum,md[p]),Form("%s && %s && %s.Device_Error_Code==0",cut1,cut2,md[p]));
-      TH1F *htemp = (TH1F*)gPad->GetPrimitive(Form("run%i_h_%s",runNum,md[p]));
+      mps_tree->Draw(Form("%s.hw_sum>>run%i_h_%s",md[p].Data(),runNum,md[p].Data()),Form("%s && %s && %s.Device_Error_Code==0",cut1,cut2,md[p].Data()));
+      TH1F *htemp = (TH1F*)gPad->GetPrimitive(Form("run%i_h_%s",runNum,md[p].Data()));
       mean[p]=htemp->GetMean(); rms[p]=htemp->GetRMS();
-      std::cout<<md[p]<<": "<<htemp->GetMean()<<" "<<htemp->GetRMS()<<std::endl;
+      std::cout<<md[p].Data()<<": "<<htemp->GetMean()<<" "<<htemp->GetRMS()<<std::endl;
    }
   
 } // end of macro
