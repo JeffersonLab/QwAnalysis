92f8879a82c42e0933fb6f0ee61fb7bd711247ef
spayde <spayde@b9700a98-9937-0410-b777-f318d6eb5bff>
2011-03-29 16:56:39 +0000

    This commit updates Parity/include/QwSSQLS.h to compatability with a version 01.02.0000 database.  The valid database version information in Analysis/src/QwDatabase.cc has been updated so that at least a version 01.02.0000 database is required to enable any kind of access.  This breaks compatability with existing production databases.  If you need access to 'qw_fall2010_20101204' then you should not update your analyzer to this commit number.  (Hope to commit further changes that will decouple read access to a database from validating against this version number and thereby restoring access to older databases.)
    Updated Parity/prminput/qweak_mysql.conf so that access defaults to a new test database on cdaql6 (qw_test_20110329).  Please feel free to use this database to test database-related QwAnalysis updates.
    
    Changes to database schema incorporated in this version:
    - Added 'injector_slug' to 'run' table.
    - Added 'analyzer_version' (a TEXT field) to 'analysis' table.
    - Removed 'root_based_regression' from 'analysis' table.
    
    
    
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@2737 b9700a98-9937-0410-b777-f318d6eb5bff

--- Analysis/src/QwDatabase.cc
+++ Analysis/src/QwDatabase.cc
@@ -34,7 +34,7 @@ std::map<string, unsigned int> QwDatabase::fSlowControlDetectorIDs;// for epics
  * mysqlpp::Connection() object that has exception throwing disabled.
  */
 //QwDatabase::QwDatabase() : Connection(false)
-QwDatabase::QwDatabase() : Connection(), kValidVersionMajor("01"), kValidVersionMinor("01"), kValidVersionPoint("0000")
+QwDatabase::QwDatabase() : Connection(), kValidVersionMajor("01"), kValidVersionMinor("02"), kValidVersionPoint("0000")
 {
   // Initialize member fields
   fDatabase=fDBServer=fDBUsername=fDBPassword="";
@@ -55,7 +55,7 @@ QwDatabase::QwDatabase() : Connection(), kValidVersionMajor("01"), kValidVersion
  *  the QwOptions object.
  * @param options  The QwOptions object.
  */
-QwDatabase::QwDatabase(QwOptions &options) : Connection(), kValidVersionMajor("01"), kValidVersionMinor("01"), kValidVersionPoint("0000")
+QwDatabase::QwDatabase(QwOptions &options) : Connection(), kValidVersionMajor("01"), kValidVersionMinor("02"), kValidVersionPoint("0000")
 {
   // Initialize member fields
   fDatabase=fDBServer=fDBUsername=fDBPassword="";
--- Parity/include/QwSSQLS.h
+++ Parity/include/QwSSQLS.h
@@ -1,3 +1,4 @@
+
 #ifndef QWSSQLS_HH
 #define QWSSQLS_HH
 
@@ -45,10 +46,11 @@ namespace QwParityDB {
 			, mysqlpp::Null<mysqlpp::sql_text> , type
 		) 
   
-    sql_create_9(run, 1, 2 
+    sql_create_10(run, 1, 2 
 			, mysqlpp::sql_int_unsigned , run_id
 			, mysqlpp::sql_int_unsigned , run_number 	
       , mysqlpp::sql_int_unsigned , slug
+      , mysqlpp::sql_int_unsigned , injector_slug
 			, mysqlpp::Null<mysqlpp::sql_text> , run_type
 //			, mysqlpp::Null<mysqlpp::sql_set> , good_for_id
 //			, mysqlpp::Null<mysqlpp::sql_set> , run_quality_id
@@ -75,6 +77,7 @@ namespace QwParityDB {
 			, mysqlpp::sql_int_unsigned , analysis_id
 			, mysqlpp::sql_int_unsigned , runlet_id
 			, mysqlpp::sql_int_unsigned , seed_id
+			, mysqlpp::Null<mysqlpp::sql_text> , analyzer_version
 			, mysqlpp::sql_int_unsigned , monitor_calibration_id
 			, mysqlpp::sql_int_unsigned , cut_id
 			, mysqlpp::Null<mysqlpp::sql_datetime> , time
@@ -87,25 +90,28 @@ namespace QwParityDB {
 			, mysqlpp::Null<mysqlpp::sql_int> , segment
 			, mysqlpp::Null<mysqlpp::sql_enum> , slope_calculation
 			, mysqlpp::Null<mysqlpp::sql_enum> , slope_correction
-      , mysqlpp::Null<mysqlpp::sql_enum> , root_based_regression
 		) 
   
-    sql_create_7(md_slope, 1, 2 
+    sql_create_9(md_slope, 1, 2 
 			, mysqlpp::sql_int_unsigned , md_slope_id
 			, mysqlpp::sql_int_unsigned , analysis_id
 			, mysqlpp::sql_int_unsigned , slope_type_id
 			, mysqlpp::sql_char , measurement_type_id
 			, mysqlpp::sql_int_unsigned , main_detector_id
+      , mysqlpp::sql_tinyint_unsigned , subblock
+			, mysqlpp::sql_int_unsigned , n
 			, mysqlpp::sql_float , value
 			, mysqlpp::sql_float , error
 		) 
   
-    sql_create_7(lumi_slope, 1, 2 
+    sql_create_9(lumi_slope, 1, 2 
 			, mysqlpp::sql_int_unsigned , lumi_slope_id
 			, mysqlpp::sql_int_unsigned , analysis_id
 			, mysqlpp::sql_int_unsigned , slope_type_id
 			, mysqlpp::sql_char , measurement_type_id
 			, mysqlpp::sql_int_unsigned , lumi_detector_id
+      , mysqlpp::sql_tinyint_unsigned , subblock
+			, mysqlpp::sql_int_unsigned , n
 			, mysqlpp::sql_float , value
 			, mysqlpp::sql_float , error
 		) 
@@ -309,7 +315,6 @@ namespace QwParityDB {
 		) 
   
     }
-
 
 #if (__GNUC__ == 4) && (__GNUC_MINOR__ > 5)
 #  pragma GCC diagnostic pop
