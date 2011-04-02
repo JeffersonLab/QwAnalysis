b4b7bbf721c5b82f1e7bfd19b8b9f38eb543a480
cornejo <cornejo@b9700a98-9937-0410-b777-f318d6eb5bff>
2011-04-02 00:15:20 +0000

    Added the include directories to QwROOT's default paths for Parity, Tracking and Analysis.
    One may now easily compile code that includes one of these header files :)
    
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@2755 b9700a98-9937-0410-b777-f318d6eb5bff

--- Analysis/main/QwRoot.cc
+++ Analysis/main/QwRoot.cc
@@ -10,9 +10,17 @@
 
 // Qweak headers
 #include "QwRint.h"
+#include <TSystem.h>
+#include <TROOT.h>
+#include <TString.h>
+#include <QwOptions.h>
 
 int main(int argc, char** argv)
 {
+  TString path = getenv_safe_TString("QWANALYSIS");
+  gROOT->ProcessLine(".include " + path + "/Analysis/include");
+  gROOT->ProcessLine(".include " + path + "/Parity/include");
+  gROOT->ProcessLine(".include " + path + "/Parity/include");
   // Start Qw-Root command prompt
   QwRint* qwrint = new QwRint("Qweak-Root Analyzer", &argc, argv);
   qwrint->Run();
