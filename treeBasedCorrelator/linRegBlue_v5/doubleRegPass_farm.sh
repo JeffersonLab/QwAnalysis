9584fca23846aa2b7765a8612309d1387b3327e2
pking <pking@b9700a98-9937-0410-b777-f318d6eb5bff>
2011-07-29 16:07:27 +0000

    NOTE:  This is in linRegBlue_v5.
    Added the original version of the double regression pass
    script to be used by the farm jobs.
    
    
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/branches/balewski.correlator@3089 b9700a98-9937-0410-b777-f318d6eb5bff

---
+++ treeBasedCorrelator/linRegBlue_v5/doubleRegPass_farm.sh
@@ -0,0 +1,107 @@
+#!/bin/sh
+run=$1
+seg=$2
+outPath=$3
+workPath=$4
+rootFileStem=$5
+dbName=$6
+
+if [ $# -ne 6 ] ; then
+   echo provide run +segemnt + outPath + workDir + rootFileStem + dbName
+   exit
+fi
+
+scriptPath=`dirname $0`
+
+if [ -d $workPath ] ; then
+    echo '$1=="inpPath"{$2="'$workPath\/$rootFileStem'" ; print}; $1!="inpPath" {print}' > $workPath/tmp.awk
+    awk -f $workPath/tmp.awk $scriptPath/blueReg.conf > $workPath/blueReg.conf
+else
+    echo The directory $workPath does not exist.
+    exit
+fi
+
+cd $workPath
+
+logPath='./out/'
+mxEve=2223344
+
+echo doubleRegPass  run.seg=$run.$seg
+#destDir=tree/regR$run.$seg
+destDir=$outPath/regR$run.$seg
+
+
+
+if [ -d out ] ; then
+  mv -f out outOld
+fi
+
+
+if [ -d $destDir ] ; then
+    newDest=`dirname $destDir`/old`basename $destDir`
+    mv -f $destDir $newDest
+    if [ $? -ne 0 ] ; then exit; fi  
+fi
+
+
+
+mkdir out
+
+#.......................................
+echo regPass1 started ...
+time $scriptPath/linRegBlue  $run $seg $mxEve >& $logPath/logS1
+
+if [ $? -ne 0 ] ; then 
+   echo failed reg-pass1 for run $run.$seg
+   cat $logPath/logS1
+   mv out $outPath/out-regAbort1-$run.$seg
+   echo abandon this run
+   exit
+fi
+
+#.......................................
+echo regPass1 successful 
+slopeFile=blueR$run.$seg.slope.root
+mv  out/blueR$run.$seg\new.slope.root out/$slopeFile
+
+if [ $? -ne 0 ] ; then 
+   echo failed to find slope matrix  for run $run.$seg
+   mv out $outPath/out-regAbort2-$run.$seg
+   echo abandon this run
+   exit
+fi
+
+
+#.......................................
+echo regPass2 started 
+echo $scriptPath/linRegBlue  $run $seg $mxEve  blueReg.conf $slopeFile
+time $scriptPath/linRegBlue  $run $seg $mxEve  blueReg.conf $slopeFile >& $logPath/logS2
+if [ $? -ne 0 ] ; then 
+   echo failed reg-pass2 for run $run.$seg
+   mv out $outPath/out-regAbort3-$run.$seg
+   echo abandon this run
+   exit
+fi
+
+#.......................................
+echo Prepare data for DB upload
+
+root -b -q $scriptPath/prCsvRecordTwo.C'(2,'$run.$seg',"bhla","out/")'
+
+if [ $? -eq 0 ] ; then 
+    echo Upload data to DB
+    if [[ -n "$PERL5LIB" ]]; then
+	export PERL5LIB=${scriptPath}:${PERL5LIB}
+    else
+	export PERL5LIB=${scriptPath}
+    fi
+    $scriptPath/upload_linreg_data.pl -u qwreplay -n qweakdb -d $dbName -prf  $scriptPath out/blueR${run}.${seg}_DBrecord.txt
+fi
+
+
+#.......................................
+echo regPass2 successful destDir=$destDir
+
+
+mv out $destDir
+du -hs $destDir
