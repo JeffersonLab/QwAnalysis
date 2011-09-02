d796d6d3478476b4999a28960ffd6463ccfa10c2
pking <pking@b9700a98-9937-0410-b777-f318d6eb5bff>
2011-09-02 20:13:55 +0000

    Added a new boolean option "helicity.toggle-mode" to the QwHelicity class. This basically combines a pattern-size of two with a helcity delay of zero, and disables the predictor, to directly use the reported helicity as the "true" helicity.
    This should make it simpler to analyze toggle-mode data.
    
    Note that this disables some of the data robustness checking,
    as it does not expect toggled pairs to always start with
    the same helcity state.  That could be developed more fully
    later.
    
    It does verify that each pattern has equal numbers of
    positive and negative helicity states though.
    
    
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@3177 b9700a98-9937-0410-b777-f318d6eb5bff

--- Parity/src/QwHelicity.cc
+++ Parity/src/QwHelicity.cc
@@ -48,6 +48,9 @@ void QwHelicity::DefineOptions(QwOptions &options)
   options.AddOptions("Helicity options")
       ("helicity.delay", po::value<int>(),
           "Default delay is 2 patterns, set at the helicity map file.");
+  options.AddOptions("Helicity options")
+      ("helicity.toggle-mode", po::value<bool>()->default_bool_value(false),
+          "Activates helicity toggle-mode, overriding the 'delay', 'patternphase', 'bitpattern', and 'seed' options.");
 }
 
 //**************************************************//
@@ -93,6 +96,13 @@ void QwHelicity::ProcessOptions(QwOptions &options)
     SetHelicityBitPattern(bits);
   } else {
     BuildHelicityBitPattern(fMaxPatternPhase);
+  }
+
+  if (options.GetValue<bool>("helicity.toggle-mode")) {
+    fHelicityDelay   = 0;
+    fUsePredictor    = kFALSE;
+    fMaxPatternPhase = 2;
+    fHelicityBitPattern = kDefaultHelicityBitPattern;
   }
 
   //  If we have the default Helicity Bit Pattern & a large fMaxPatternPhase,
