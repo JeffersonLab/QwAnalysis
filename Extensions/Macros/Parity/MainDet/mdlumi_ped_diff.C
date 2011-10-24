bd4c289182ce99cf34c18157548d76febbcc9195
leacock <leacock@b9700a98-9937-0410-b777-f318d6eb5bff>
2011-10-24 21:09:57 +0000

    updated pedestal extraction macro to conform to tighter restrictions on string and character manipulation.
    added pedestal difference macro based on the pedestal script.
    
    to calculate the pedestal differences the following works with the trunk:
    
    qwparity -r11503 -e:5000 --rootfile-stem diff_ --enable-differences 1 --disable-histos 1
    
    There is a problem with the histogram handling with differences enabled so disable them or get a seg fault.
    
    The md and bg pedestal differences leaves are empty...  Paul said they may be zeroed out in the analyzer to hide the blinding factor.
    
    
    
    
    
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@3355 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Macros/Parity/MainDet/mdlumi_ped_diff.C
+++ Extensions/Macros/Parity/MainDet/mdlumi_ped_diff.C
@@ -1,9 +1,9 @@
-void mdlumi_ped(int run_num)
+void mdlumi_ped_diff(int run_num)
 {
 
   const int mps_start = 0;
-  const int mps_stop = 1e9;
-//  const int mps_stop = 180e3;
+//  const int mps_stop = 1e9;
+  const int mps_stop = 180e3;
   const bool get_md = kTRUE;
   const bool get_lumi = kTRUE;
   const bool save_file = kTRUE;
@@ -14,26 +14,27 @@ void mdlumi_ped(int run_num)
   gStyle->SetStatH(0.4);
   gStyle->SetOptStat("neMr");
 
-  TChain chain("Mps_Tree");
-  chain.Add(Form("$QW_ROOTFILES/qwick_mdlumiped_%i.root",run_num));
+  TChain chain("Hel_Tree");
+//  chain.Add(Form("$QW_ROOTFILES/qwick_mdlumiped_%i.root",run_num));
 //  chain.Add(Form("$QW_ROOTFILES/first100k_%i.root",run_num));
 //  chain.Add(Form("$QW_ROOTFILES/Qweak_%i.000.trees.root",run_num));
+  chain.Add(Form("$QW_ROOTFILES/diff_%i.000.trees.root",run_num));
 
   const char *lumi[16] = {
-  "qwk_dslumi1","qwk_dslumi2","qwk_dslumi3","qwk_dslumi4",
-  "qwk_dslumi5","qwk_dslumi6","qwk_dslumi7","qwk_dslumi8",
-  "qwk_uslumi1neg","qwk_uslumi1pos","qwk_uslumi3neg","qwk_uslumi3pos",
-  "qwk_uslumi5neg","qwk_uslumi5pos","qwk_uslumi7neg","qwk_uslumi7pos"};
+  "diff_qwk_dslumi1","diff_qwk_dslumi2","diff_qwk_dslumi3","diff_qwk_dslumi4",
+  "diff_qwk_dslumi5","diff_qwk_dslumi6","diff_qwk_dslumi7","diff_qwk_dslumi8",
+  "diff_qwk_uslumi1neg","diff_qwk_uslumi1pos","diff_qwk_uslumi3neg","diff_qwk_uslumi3pos",
+  "diff_qwk_uslumi5neg","diff_qwk_uslumi5pos","diff_qwk_uslumi7neg","diff_qwk_uslumi7pos"};
 
   const char *md[16] = {
-  "qwk_md1neg","qwk_md2neg","qwk_md3neg","qwk_md4neg",
-  "qwk_md5neg","qwk_md6neg","qwk_md7neg","qwk_md8neg",
-  "qwk_md1pos","qwk_md2pos","qwk_md3pos","qwk_md4pos",
-  "qwk_md5pos","qwk_md6pos","qwk_md7pos","qwk_md8pos"};
+  "diff_qwk_md1neg","diff_qwk_md2neg","diff_qwk_md3neg","diff_qwk_md4neg",
+  "diff_qwk_md5neg","diff_qwk_md6neg","diff_qwk_md7neg","diff_qwk_md8neg",
+  "diff_qwk_md1pos","diff_qwk_md2pos","diff_qwk_md3pos","diff_qwk_md4pos",
+  "diff_qwk_md5pos","diff_qwk_md6pos","diff_qwk_md7pos","diff_qwk_md8pos"};
   
   const char *bg[8] = {
-  "qwk_pmtonl","qwk_pmtltg","qwk_md9neg","qwk_md9pos",
-  "qwk_pmtled","qwk_isourc","qwk_preamp","qwk_cagesr"};
+  "diff_qwk_pmtonl","diff_qwk_pmtltg","diff_qwk_md9neg","diff_qwk_md9pos",
+  "diff_qwk_pmtled","diff_qwk_isourc","diff_qwk_preamp","diff_qwk_cagesr"};
   
 
   char * pPath = "";
@@ -41,12 +42,12 @@ void mdlumi_ped(int run_num)
 
   if (get_md == kTRUE)
     {      
-      ofstream md_pedestal_file;
+/*      ofstream md_pedestal_file;
       md_pedestal_file.open(Form("%s/calib/qweak_maindet_pedestal.%i-.map",pPath,run_num));
       md_pedestal_file<<endl;
       md_pedestal_file<<Form("!The following pedestals were recorded from RUN %i",run_num)<<endl;
       md_pedestal_file<<"!channel name , Mps channelname.hw_sum_raw/num_samples , gain"<<endl;
-
+*/
       
       TCanvas *c_md = new TCanvas("md","md",1500,1100);
       c_md->Divide(4,4);
@@ -56,9 +57,9 @@ void mdlumi_ped(int run_num)
           c_md->cd(i+1);      
           mdhst[i] = new TH1F(Form("%h_%s",md[i]),"",100,0,0);
           mdhst[i]->SetDirectory(0);   
-          chain.Draw( Form("%s.hw_sum_raw/%s.num_samples*%f*1000>>h_%s",md[i],md[i],scale,md[i]),Form("qwk_charge<1 && %s.Device_Error_Code==0 && mps_counter>%i && mps_counter<%i",md[i],mps_start,mps_stop),"");
+          chain.Draw( Form("%s>>h_%s",md[i],md[i]),Form("%s.Device_Error_Code==0 && mps_counter>%i && mps_counter<%i",md[i],mps_start,mps_stop),"");
           TH1F *htemp = (TH1F*)gPad->GetPrimitive(Form("h_%s",md[i]));
-          md_pedestal_file<<Form("%s  ,  ",md[i])<<htemp->GetMean()/(scale*1000)<<Form("  ,  %10.8f",scale)<<endl;  
+//          md_pedestal_file<<Form("%s  ,  ",md[i])<<htemp->GetMean()/(scale*1000)<<Form("  ,  %10.8f",scale)<<endl;  
           cout<<md[i]<<"  "<<htemp->GetMean()<<endl;
           c_md->Update();
         }
@@ -70,29 +71,31 @@ void mdlumi_ped(int run_num)
           c_bg->cd(i+1);      
           bghst[i] = new TH1F(Form("%h_%s",bg[i]),"",100,0,0);
           bghst[i]->SetDirectory(0);   
-          chain.Draw( Form("%s.hw_sum_raw/%s.num_samples*%f*1000>>h_%s",bg[i],bg[i],scale,bg[i]),Form("qwk_charge<1 && %s.Device_Error_Code==0 && mps_counter>%i && mps_counter<%i",bg[i],mps_start,mps_stop),"");
+          chain.Draw( Form("%s>>h_%s",bg[i],bg[i]),Form("%s.Device_Error_Code==0 && mps_counter>%i && mps_counter<%i",bg[i],mps_start,mps_stop),"");
           TH1F *htemp = (TH1F*)gPad->GetPrimitive(Form("h_%s",bg[i]));
           (if i<4){
-		md_pedestal_file<<Form("%s  ,  ",bg[i])<<htemp->GetMean()/(scale*1000)<<Form("  ,  %10.8f",scale)<<endl;
+//		md_pedestal_file<<Form("%s  ,  ",bg[i])<<htemp->GetMean()/(scale*1000)<<Form("  ,  %10.8f",scale)<<endl;
            }
           cout<<bg[i]<<"  "<<htemp->GetMean()<<endl;
           c_bg->Update();
         }
-      md_pedestal_file<<"qwk_pmtled  ,  0  ,  0.00007629"<<endl;
+/*      md_pedestal_file<<"qwk_pmtled  ,  0  ,  0.00007629"<<endl;
       md_pedestal_file<<"qwk_isourc  ,  0  ,  0.00007629"<<endl;
       md_pedestal_file<<"qwk_preamp  ,  0  ,  0.00007629"<<endl;
       md_pedestal_file<<"qwk_cagesr  ,  0  ,  0.00007629"<<endl;
      
       md_pedestal_file.close();
+*/
      }; //end get md
 
   if (get_lumi == kTRUE)
     { 
-      ofstream lumi_pedestal_file;
+/*      ofstream lumi_pedestal_file;
       lumi_pedestal_file.open(Form("%s/calib/qweak_lumi_pedestal.%i-.map",pPath,run_num));
       lumi_pedestal_file<<endl;
       lumi_pedestal_file<<Form("!The following pedestals were recorded from RUN %i",run_num)<<endl;
       lumi_pedestal_file<<"!channel name , Mps channelname.hw_sum_raw/num_samples , gain"<<endl;
+*/
       TCanvas *c_lumi = new TCanvas("lumi","lumi",1500,1100);
       c_lumi->Divide(4,4);
       TH1F *lumihst[16];
@@ -101,20 +104,20 @@ void mdlumi_ped(int run_num)
           c_lumi->cd(i+1);      
           lumihst[i] = new TH1F(Form("%h_%s",lumi[i]),"",100,0,0);
           lumihst[i]->SetDirectory(0);   
-          chain.Draw( Form("%s.hw_sum_raw/%s.num_samples*%f*1000>>h_%s",lumi[i],lumi[i],scale,lumi[i]),Form("qwk_charge<1 && %s.Device_Error_Code==0 && mps_counter>%i && mps_counter<%i",lumi[i],mps_start,mps_stop),"");
+          chain.Draw( Form("%s>>h_%s",lumi[i],lumi[i]),Form("%s.Device_Error_Code==0 && mps_counter>%i && mps_counter<%i",lumi[i],mps_start,mps_stop),"");
           TH1F *htemp = (TH1F*)gPad->GetPrimitive(Form("h_%s",lumi[i]));
-          lumi_pedestal_file<<Form("%s  ,  ",lumi[i])<<htemp->GetMean()/(scale*1000)<<Form("  ,  %10.8f",scale)<<endl;  
+//          lumi_pedestal_file<<Form("%s  ,  ",lumi[i])<<htemp->GetMean()/(scale*1000)<<Form("  ,  %10.8f",scale)<<endl;  
           cout<<lumi[i]<<"  "<<htemp->GetMean()<<endl;
           c_lumi->Update();
         }
-     lumi_pedestal_file.close();
+//     lumi_pedestal_file.close();
      }; // end get lumi  
      
-
+/*
 c_md->SaveAs(Form("%s/calib/pedPlots/%i_md.png",pPath,run_num));
 c_bg->SaveAs(Form("%s/calib/pedPlots/%i_bg.png",pPath,run_num));
 c_lumi->SaveAs(Form("%s/calib/pedPlots/%i_lumi.png",pPath,run_num));
-
+*/
 
 }
 
