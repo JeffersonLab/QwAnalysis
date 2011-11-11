7d11cb9dd0f34aac9b4bbaf110de2fc00980d598
rakithab <rakithab@b9700a98-9937-0410-b777-f318d6eb5bff>
2011-11-11 18:03:07 +0000

    Update the scaler plot with the CodaEventType==1 cut. Removed one duplicate canvas in the adc spectra plots
    Rakitha
    
    
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@3446 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Macros/Tracking/MainDet/md_adc.C
+++ Extensions/Macros/Tracking/MainDet/md_adc.C
@@ -21,12 +21,12 @@ for (int i=0;i<num_dets;i++)
 	h[i] = new TH1F(Form("h_%i",i),"",100,0,0);
 	h[i]->SetDirectory(0);
 	gPad->SetLogy(1);
-	event_tree->Draw(Form("%s>>h_%i(100,0,4096)",md_dets[i].Data(),i),Form("%s!=0",md_dets[i].Data()));
+	event_tree->Draw(Form("%s>>h_%i(100,0,4096)",md_dets[i].Data(),i),Form("%s>0",md_dets[i].Data()));
 	TH1F *h_temp = (TH1F*)gPad->GetPrimitive(Form("h_%i",i));
 	c1->Update();
 };
 
-
+/*
 TCanvas *c2 = new TCanvas("c2","",1100,900);
 TH1F *h2[num_dets];
 c2->Divide(4,4);
@@ -34,15 +34,15 @@ c2->Divide(4,4);
 for (int i=0;i<num_dets;i++)
 {  
 	c2->cd(i+1);
-	h2[i] = new TH1F(Form("h2_%i",i),"",100,0,0);
-	h2[i]->SetDirectory(0);
-	gPad->SetLogy(1);
-	event_tree->Draw(Form("%s>>h2_%i(100,0,4096)",md_dets[i].Data(),i));
+	//h2[i] = new TH1F(Form("h2_%i",i),"",100,0,0);
+	//h2[i]->SetDirectory(0);
+	gPad->SetLogy(0);
+	event_tree->Draw(Form("%s:CodaEventNumber>>h2_%i",md_dets[i].Data(),i),"");
 	TH1F *h2_temp = (TH1F*)gPad->GetPrimitive(Form("h2_%i",i));
 	c2->Update();
 };
 
-
+*/
 
 
 
