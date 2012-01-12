0bc8eae147d3707f0145c793119b3a4f5848bc08
sxyang <sxyang@b9700a98-9937-0410-b777-f318d6eb5bff>
2012-01-12 17:11:19 +0000

    Edit and update the r2_projection.C script. Make its usage more easily as well as providing the capability of projecting the R2 tracks to arbitrary plane. Please refer to the begnning of the file for more details on how to use it.
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@3793 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Macros/Tracking/r2_projection.C
+++ Extensions/Macros/Tracking/r2_projection.C
@@ -1,67 +1,39 @@
+// Author:Siyuan Yang
+// Email:sxyang@email.wm.edu
+// Date: Jan 12th 2012
+// 
+// Description: This file is used project the r2 track back to certain planes
+// Two basic cuts are set as default here: MD cut and one-track-per-event cut.
+// 
+// Example
+// root[1] projection("ds",1,0,-1,13563,1)
+// this will draw the projection of qualified R2 tracks back to the downstream target plane for package 1 over all events
+// the number behind the run number 13563 specifies which octant the pkg2 of R2 covers in this run.
+// Notice that the first string can be either of the following listed choices:
+// "ds" downstream solid target
+// "us" upstream solid target
+// "coll1" downstream face of collimator 1
+// "coll2" downstream face of primary collimator
+// "-500" arbitrary number in z location
+// Another example
+// root[2] projection("-330",1,0,-1,13654,7)
+// this will draw the projection of qualified R2 tracks back to z=-330 plane, where the pkg2 of R2 covers octant 7 this time.
+
+
+
 #include <cstring>
 #include <cstdlib>
 #include <sstream>
+//#include <string>
 
-const double us_target=-667;
-const double target=-650;
-const double ds_target=-633;
-const double coll1=-568.17;
-const double coll2=-370.719;
+const int multitracks=18;
 
-void sim(Int_t run=8658,std::string element="fQ2"){
-
-  TDirectory * where=gDirectory;
-  TFile sim_file(Form("%s/qweak_sim.root",gSystem->Getenv("QW_ROOTFILES")));
-  TFile data_file(Form("%s/Qweak_%d_base_cut_v1.root",gSystem->Getenv("QW_ROOTFILES"),run));
-  TFile data_file_1(Form("%s/Qweak_%d_base_cut_v2.root",gSystem->Getenv("QW_ROOTFILES"),run));
-
-  TTree* sim_tree=(TTree*)sim_file.Get("h8013");
-  TTree* data_tree=(TTree*)data_file.Get("event_tree");
-  TTree* data_tree_1=(TTree*)data_file_1.Get("event_tree");
-  where->cd();
-  TH1F* sim_h=new TH1F("sim","sim",100,4,14);
-  TH1F* data_h=new TH1F("data","data",100,4,14);
-  TH1F* data_h_1=new TH1F("data_1","data",100,4,14);
-  string sim_string;
-  if(element=="fQ2")
-    sim_string="q__2";
-  else if(element=="fScatteringAngle")
-    sim_string="theta_o";
-
-  sim_tree->Project("sim",Form("%s",sim_string.c_str()));
-  
-  data_tree->Project("data",Form("events.fQwTracks.%s",element.c_str()),"events.fQwTracks.fScatteringAngle>4 && events.fQwTracks.fScatteringAngle<12");
-  data_tree_1->Project("data_1",Form("events.fQwTracks.%s",element.c_str()),"events.fQwTracks.fScatteringAngle>4 && events.fQwTracks.fScatteringAngle<12");
-  sim_h->SetLineColor(kRed);
-  sim_h->Draw();
-  sim_h->SetTitle("sim(Red) vs real data: method1(Blue) vs method2(Cyan)");
-  sim_h->GetXaxis()->SetTitle(Form("x axis:%s",element.c_str()));
-  data_h->SetLineColor(kBlue);
-  data_h->Scale(sim_h->GetEntries()/data_h->GetEntries());
-  data_h->Draw("same");
-  //data_h_1->SetLineColor(6);
-  //data_h_1->Scale(sim_h->GetEntries()/data_h_1->GetEntries());
-  //data_h_1->Draw("same");
-
-  return;
-
-}
-
-/*
- * option=1, draw the projection to collimator1 as well as the target vertex distribution
- * option=4, draw the projection to target position
- * option=5, draw the projection to the upstream target plane
- *
- */
+void projection(string target,int pkg=1,int event_start=0,int event_end=-1,int run=8658,int oct=1,string suffix=""){
 
 
-
-void projection(int option=0,int pkg=1,int event_start=0,int event_end=-1,int run=8658,string suffix=""){
-
-
-   bool fDebug=false;
+   //bool fDebug=false;
    //string folder= "/scratch/sxyang";
-   string folder="/work/hallc/qweak/sxyang/rootfiles";
+   string folder=gSystem->Getenv("QW_ROOTFILES");
    ostringstream ss;
    ss << folder << "/Qweak_";
    ss << run << suffix;
@@ -69,16 +41,38 @@ void projection(int option=0,int pkg=1,int event_start=0,int event_end=-1,int ru
    string file_name=ss.str();
    cout <<  file_name << endl;
    TFile* file=new TFile(file_name.c_str());
+  
+   double z;
+   if(target=="coll1")
+     z=-568.17;
+   else if(target=="coll2")
+     z=-370.719;
+   else if(target=="ds")
+     z=-633;
+   else if(target=="us")
+     z=-667;
+   else{
+     istringstream random(target);
+     random >> z;
+   }
 
-   
-    TH1F* vertex=new TH1F("vertex in Z","vertex in Z",300,-1000,-400);
-    // TH2F* projection=new TH2F("projection","projection",160,-20,20,100,5,30);
-    TH2F* projection=new TH2F("projection","projection",320,-40,40,320,-40,40);
-    TH2F* projection_target=new TH2F("projection to target plane","projection to target plane",120,-15,15,120,-15,15);
+   TH1F* vertex=new TH1F("vertex in Z","vertex in Z",200,-850,-450);
+   TH2F* projection;
+   if(z<-500 && z>-600)
+     projection=new TH2F("projection","projection",240,-30,30,240,-30,30);
+   else if(z<-600)
+     projection=new TH2F("projection","projection",120,-15,15,120,-15,15);
+   else if(z<-350)
+     projection=new TH2F("projection","projection",480,-60,60,480,-60,60);
+   else
+     projection=new TH2F("projection","projection",720,-90,90,720,-90,90);
+     
+    //TH2F* projection=new TH2F("projection","projection",240,-30,30,240,-30,30);
+    //TH2F* projection=new TH2F("projection","projection",240,-30,30,140,25,60);
+    // TH2F* projection_target=new TH2F("projection to target plane","projection to target plane",120,-15,15,120,-15,15);
     TH2F* Directionphioff=new TH2F("DirectionPhioff vs r","DirectionPhioff vs r",1000,-0.5,0.5,200,0,20);
     TH2F* Directionthetaoff=new TH2F("DirectionThetaoff vs r","DirectionThetaoff vs r",1000,-0.05,0.05,200,0,20);
     
-    //TH2F* projection=new TH2F("projection","projection",120,-15,15,80,-10,10);
     TProfile2D* profile_target=new TProfile2D("1","1",120,-15,15,80,-10,10);
    
     TH1F* Xtreeline_histo=new TH1F("ar in treeline x","ar in treeline x",100,0,0.3);
@@ -86,7 +80,7 @@ void projection(int option=0,int pkg=1,int event_start=0,int event_end=-1,int ru
     TH1F* Vtreeline_histo=new TH1F("ar in treeline x","ar in treeline v",100,0,0.3);
     TH1F* Chi_histo=new TH1F("chi distribution of partial track","chi distribution in r2",1000,0,100);
     TH1F* x_histo=new TH1F("x coll1 distribution","x coll1 distribution",200,-10,10);
-    TH1F* y_histo=new TH1F("y coll1 distribution","y coll1 distribution",200,0,20);
+    TH1F* y_histo=new TH1F("y coll1 distriution","y coll1 distribution",200,0,20);
     TH1F* Plane_residual[12];
     for(int i=0;i<12;++i)
       Plane_residual[i]=new TH1F(Form("residual in plane%d",i+1),Form("residual in plane%d",i+1),100,-0.5,0.5);
@@ -107,8 +101,8 @@ void projection(int option=0,int pkg=1,int event_start=0,int event_end=-1,int ru
     TBranch* maindet_branch=event_tree->GetBranch("maindet");
     event_branch->SetAddress(&fEvent);
   
-    int md_1=5;
-    int md_2=1;
+    int md_1=(oct+4)%8;
+    int md_2=oct;
     
     TLeaf* tsp_1=maindet_branch->GetLeaf(Form("md%dp_f1",md_1));
     TLeaf* tsm_1=maindet_branch->GetLeaf(Form("md%dm_f1",md_1));
@@ -141,7 +135,6 @@ void projection(int option=0,int pkg=1,int event_start=0,int event_end=-1,int ru
       double pkg2_thetaoff_lower=mean_thetaoff_pkg2-width*rms_thetaoff_pkg2;
       double pkg2_thetaoff_upper=mean_thetaoff_pkg2+width*rms_thetaoff_pkg2;
 
-    int good=0;
     int oct=0;
     for(int i=start;i<end;++i){
 
@@ -176,13 +169,12 @@ void projection(int option=0,int pkg=1,int event_start=0,int event_end=-1,int ru
 	}
 	
 	
-	//if(valid_hits>=18) continue;
+      if(valid_hits>=multitracks) continue;
       double ntracks=fEvent->GetNumberOfTracks();
       double npts=fEvent->GetNumberOfPartialTracks();
 
       double chi=0;
       // call build function
-      //if(ntracks!=0){
       for(int nts=0;nts<ntracks;++nts){
       track=fEvent->GetTrack(nts);
       if(track->GetPackage()!=pkg){
@@ -201,24 +193,24 @@ void projection(int option=0,int pkg=1,int event_start=0,int event_end=-1,int ru
 	  oct=pt->GetOctantNumber();
 	chi=pt->fChi;
         double vertex_z=-(pt->fSlopeX*pt->fOffsetX + pt->fSlopeY*pt->fOffsetY)/(pt->fSlopeX*pt->fSlopeX+pt->fSlopeY*pt->fSlopeY);
-	double x=pt->fOffsetX+coll1*pt->fSlopeX;
-	double y=pt->fOffsetY+coll1*pt->fSlopeY;
-	double x_tar=pt->fOffsetX+ds_target*pt->fSlopeX;
-	double y_tar=pt->fOffsetY+ds_target*pt->fSlopeY;
+	double x=pt->fOffsetX+z*pt->fSlopeX;
+	double y=pt->fOffsetY+z*pt->fSlopeY;
+	//double x_tar=pt->fOffsetX+ds_target*pt->fSlopeX;
+	//double y_tar=pt->fOffsetY+ds_target*pt->fSlopeY;
 	x*=-1;
-	x_tar*=-1;
-	double r=sqrt(x_tar*x_tar+y_tar*y_tar);
+	//x_tar*=-1;
+	//double r=sqrt(x_tar*x_tar+y_tar*y_tar);
+	double r=sqrt(x*x+y*y);
 	Directionphioff->Fill(track->fDirectionPhioff,r);
 	Directionthetaoff->Fill(track->fDirectionThetaoff,r);
-
 
 	vertex->Fill(vertex_z);
 	projection->Fill(x,y);
 
 	x_histo->Fill(x);
 	y_histo->Fill(y);
-	projection_target->Fill(x_tar,y_tar);
-	profile_target->Fill(x,y,chi);
+	//projection_target->Fill(x_tar,y_tar);
+	//profile_target->Fill(x,y,chi);
 	Xtreeline_histo->Fill(pt->TResidual[1]);
 	Utreeline_histo->Fill(pt->TResidual[3]);
 	Vtreeline_histo->Fill(pt->TResidual[4]);
@@ -232,9 +224,107 @@ void projection(int option=0,int pkg=1,int event_start=0,int event_end=-1,int ru
    
     //cout << "good: " << good << endl;
     cout << "oct: " << oct << endl;
-    TCanvas* c=new TCanvas("c","c",800,800);
-    if(option==1){
-      c->Divide(1,3);
+    
+    TCanvas* c1=new TCanvas("c","c",1000,500);
+    c1->Divide(2,1);
+    c1->cd(1);
+    gStyle->SetPalette(1);
+    projection->GetXaxis()->SetTitle("x axis:cm");
+    projection->GetYaxis()->SetTitle("y axis:cm");
+    projection->Draw("colz");
+    
+    projection->SetTitle(Form("run %d: pkg%d projection to Z=%f",run,pkg,z));
+    double PI=3.1415926;
+    double px[6];
+    double py[6];
+    if(target=="coll1"){
+     
+    double angle=oct==8? -135:-(3-oct)*45;
+    double Sin=sin(angle*PI/180);
+    double Cos=cos(angle*PI/180);
+    double highy=18.46,middley=12.63,lowy=7.03;
+    double highx=3.65,middlex=3.65,lowx=1.91;
+    
+    px[0]=-Cos*highx+Sin*highy;
+    py[0]=Sin*highx+Cos*highy;
+    px[1]=Cos*highx+Sin*highy;
+    py[1]=-Sin*highx+Cos*highy;
+    px[2]=-Cos*lowx+Sin*lowy;
+    py[2]=Sin*lowx+Cos*lowy;
+    px[3]=Cos*lowx+Sin*lowy;
+    py[3]=-Sin*lowx+Cos*lowy;
+    px[4]=-Cos*middlex+Sin*middley;
+    py[4]=Sin*middlex+Cos*middley;
+    px[5]=Cos*middlex+Sin*middley;
+    py[5]=-Sin*middlex+Cos*middley;
+    
+    TLine* t1=new TLine(px[0],py[0],px[1],py[1]);
+    TLine* t2=new TLine(px[2],py[2],px[3],py[3]);
+    TLine* t3=new TLine(px[0],py[0],px[4],py[4]);
+    TLine* t4=new TLine(px[1],py[1],px[5],py[5]);
+    TLine* t5=new TLine(px[4],py[4],px[2],py[2]);
+    TLine* t6=new TLine(px[5],py[5],px[3],py[3]);
+    t1->SetLineWidth(2);
+    t2->SetLineWidth(2);
+    t3->SetLineWidth(2);
+    t4->SetLineWidth(2);
+    t5->SetLineWidth(2);
+    t6->SetLineWidth(2);
+    t1->Draw("same");
+    t2->Draw("same");
+    t3->Draw("same");
+    t4->Draw("same");
+    t5->Draw("same");
+    t6->Draw("same");
+    }
+    else if(target=="coll2"){
+       double angle=oct==8? -135:-(3-oct)*45;
+    double Sin=sin(angle*PI/180);
+    double Cos=cos(angle*PI/180);
+    double highy=53.7,middley=37.1,lowy=30.57;
+    double highx=9.2,middlex=9.2,lowx=6.5;
+    
+    px[0]=-Cos*highx+Sin*highy;
+    py[0]=Sin*highx+Cos*highy;
+    px[1]=Cos*highx+Sin*highy;
+    py[1]=-Sin*highx+Cos*highy;
+    px[2]=-Cos*lowx+Sin*lowy;
+    py[2]=Sin*lowx+Cos*lowy;
+    px[3]=Cos*lowx+Sin*lowy;
+    py[3]=-Sin*lowx+Cos*lowy;
+    px[4]=-Cos*middlex+Sin*middley;
+    py[4]=Sin*middlex+Cos*middley;
+    px[5]=Cos*middlex+Sin*middley;
+    py[5]=-Sin*middlex+Cos*middley;
+    
+    TLine* t1=new TLine(px[0],py[0],px[1],py[1]);
+    TLine* t2=new TLine(px[2],py[2],px[3],py[3]);
+    TLine* t3=new TLine(px[0],py[0],px[4],py[4]);
+    TLine* t4=new TLine(px[1],py[1],px[5],py[5]);
+    TLine* t5=new TLine(px[4],py[4],px[2],py[2]);
+    TLine* t6=new TLine(px[5],py[5],px[3],py[3]);
+    t1->SetLineWidth(2);
+    t2->SetLineWidth(2);
+    t3->SetLineWidth(2);
+    t4->SetLineWidth(2);
+    t5->SetLineWidth(2);
+    t6->SetLineWidth(2);
+    t1->Draw("same");
+    t2->Draw("same");
+    t3->Draw("same");
+    t4->Draw("same");
+    t5->Draw("same");
+    t6->Draw("same");
+    }
+
+    c1->cd(2);
+    vertex->Draw();
+
+    // TCanvas* c2=new TCanvas("c","c",800,800);
+    // vertex->Draw();
+    /*if(option==1){
+      //c->Divide(1,3);
+      c->Divide(1,2);
       c->cd(1);
     gStyle->SetPalette(1);
     projection->GetXaxis()->SetTitle("x axis:cm");
@@ -244,16 +334,17 @@ void projection(int option=0,int pkg=1,int event_start=0,int event_end=-1,int ru
     
     // ds coll1
     double PI=3.1415926;
+    oct=3;
     double angle=oct==8? -135:-(3-oct)*45;
     double Sin=sin(angle*PI/180);
     double Cos=cos(angle*PI/180);
     
     double px[6];
     double py[6];
-    double highy=18.46,middley=12.63,lowy=7.03;
-    double highx=3.65,middlex=3.65,lowx=1.91;
-    //double highy=53.7,middley=37.1,lowy=30.57;
-    //double highx=9.2,middlex=9.2,lowx=6.5;
+    //double highy=18.46,middley=12.63,lowy=7.03;
+    //double highx=3.65,middlex=3.65,lowx=1.91;
+    double highy=53.7,middley=37.1,lowy=30.57;
+    double highx=9.2,middlex=9.2,lowx=6.5;
     //double highy=77.5,middley=51,lowy=39.95;
     //double highx=14.5,middlex=14.5,lowx=10.1;
     px[0]=-Cos*highx+Sin*highy;
@@ -298,8 +389,8 @@ void projection(int option=0,int pkg=1,int event_start=0,int event_end=-1,int ru
     vertex->Draw();
     vertex->GetXaxis()->SetTitle("z axis:cm");
     vertex->SetTitle(Form("run %d: pkg%d vertex distribution",run,pkg));
-    c->cd(3);
-    Chi_histo->Draw();
+    //c->cd(3);
+    //Chi_histo->Draw();
     }
     else if(option==2){
     c->Divide(2,2);
@@ -360,7 +451,7 @@ void projection(int option=0,int pkg=1,int event_start=0,int event_end=-1,int ru
     Directionthetaoff->Draw("colz");
     
     }
-    
+    */
     
     return;
 
