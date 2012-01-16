5fba5874e7a192983f8c8289eae5888fac1d9677
rsubedi <rsubedi@b9700a98-9937-0410-b777-f318d6eb5bff>
2012-01-16 22:30:04 +0000

    It looks like Han forgot to comment the header file QwGasElectronMultiplier.h from the codes:
    M       Tracking/main/QwSimRayTracer.cc
    M       Tracking/main/QwEventDisplayTest.cc
    M       Tracking/main/QwSimTracking.cc
    
    It's commented out now.
    
    
    
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@3803 b9700a98-9937-0410-b777-f318d6eb5bff

--- Tracking/main/QwSimTracking.cc
+++ Tracking/main/QwSimTracking.cc
@@ -34,7 +34,7 @@
 
 // Qweak tracking subsystem headers
 #include "QwSubsystemArrayTracking.h"
-#include "QwGasElectronMultiplier.h"
+//#include "QwGasElectronMultiplier.h"
 #include "QwDriftChamberHDC.h"
 #include "QwDriftChamberVDC.h"
 #include "QwTriggerScintillator.h"
