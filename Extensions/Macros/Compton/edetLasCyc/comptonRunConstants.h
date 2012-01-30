3a0393bc84b941e222385339e2fe0c938ca43672
narayan <narayan@b9700a98-9937-0410-b777-f318d6eb5bff>
2012-01-30 22:07:48 +0000

    added a config file to call the function creating histograms for autoanalyzer; added a file which calculates theoretical asymmetry for the given distance of a particular strip; added some constants in the comptonRunConstants.h file that are relevant for theoretical asymmetry evaluation; edetasym checked to work for all 4 planes on run#23351,added list of further modifications; added a directory which will hold runlet based analysis scripts, currently it holds a script to analyze either first100k or the first runlet(based on a flag) and give out the histograms for all 4 electron detector planes.
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@3886 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Macros/Compton/edetLasCyc/comptonRunConstants.h
+++ Extensions/Macros/Compton/edetLasCyc/comptonRunConstants.h
@@ -1,21 +1,27 @@
 //Asymmetry constants 
-const Double_t PLANCK = 4.135667e-21;//Planck's constant (MeV*s)
-const Double_t A0 = 2.817940289;//classical electron radius (m)
-const Double_t E0 = 1161;//electron beam energy (MeV)
-const Double_t C = 2.997e8;//speed of light (m/s)
-const Double_t LAMDA = 532e-9;//wavelength of laser (m)
-const Double_t K0 = PLANCK * C/LAMDA;//incident photon energy in lab frame 
-const Double_t PI_ = 3.14159265;//value of pi
-const Double_t THETA0 = 1.33 * PI_ / 180.0;//incident photon angle
-const Double_t ME = 0.5109989;//electron mass (MeV)
-const Double_t KMAX = K0 * (E0 + sqrt(E0*E0 - ME*ME)*cos(THETA0)) / //max backscattered
-                     (E0+K0-sqrt(E0*E0 - ME*ME)+K0*cos(THETA0));// photon energy (MeV)
-const Double_t Z_CROSS = 9;//strip of Compton asymmetry zero crossing
-const Double_t C_EDGE = 51;//strip of Compton edge
+///attempt-1
 
+///attempt-2
+const Float_t pi=3.141592;
+
+const Float_t hbarc=0.197E-15;// GeV.m
+//const Float_t hbarc=0.197;//old
+
+const Float_t alpha=0.00729927; ///=1.0/137.0;
+const Float_t me=0.000511;
+const Float_t xmuB=5.788381749E-14; ///  Bohr magnetron GeV/T
+const Float_t B_dipole = 0.675;
+const Float_t E = 1.159; //Beam energy (GeV)
+const Float_t lambda = 532E-9; //photon wavelength (nm)      
+//const Float_t lambda = 532.0; //old photon wavelength (nm)      
+const Float_t xgap = 0.005; //!min distance (m)
 //Hardware constants
 const Int_t nStrips = 96;
 const Int_t nPlanes = 4;
+const Float_t zdrift = 2.275; //drift distance(m) from middle of 2nd dipole to front of 3rd dipole
+// const Float_t thetabend = 10*pi/180; //bend angle in Compton chicane (radians)
+// const Float_t thetaprime = 10.39*pi/180; //bend angle for the max deflected electrons
+
 
 //Run constants
 const Double_t beamFrac = 0.6;//90% of maximum beam to be considered as beamOn
