bc759cd1930baadb72349f809ca2428546d0471d
narayan <narayan@b9700a98-9937-0410-b777-f318d6eb5bff>
2012-01-31 15:29:57 +0000

    minor modifications to the theoryAsym.C in order to work with the new asymFit.Cj; minor modifications to comptonRunConstants.h; trivial modifications to edetasym.C added new file asymFit.C which will take theoretical asymmetry and experimental asymmetry as an input from theoryAsym.C and edetasym.C and plot it; currently this new file does not yet receive input from edetasym.C; but only plots the theoretical asymmetry.
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@3889 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Macros/Compton/edetLasCyc/theoryAsym.C
+++ Extensions/Macros/Compton/edetLasCyc/theoryAsym.C
@@ -4,14 +4,14 @@
  */
 #include <rootClass.h>
 #include "comptonRunConstants.h"
-Float_t theoryAsym() 
+Float_t theoryAsym(std::vector<Float_t> &asymStrip) 
 {
   Bool_t debug=0,debug1=0,debug2=1;
-  Int_t stripNum=50;
+  //  Int_t stripNum=50;
   Float_t gamma,k,a,kprimemax;
   Float_t dxprimemax,asymmax,rho0,k0prime,dx0prime;
   Float_t kStripprime,rhoStrip;
-  Float_t asymStrip1,asymStrip;
+  Float_t asymStrip1;
   Float_t re,R_bend;
   Float_t dsdrho1,dsdrho;
   
@@ -54,17 +54,22 @@ Float_t theoryAsym()
   dx0prime=(k0prime/E)*zdrift*thetabend; //  displacement at asym 0 (m)
   if(debug1) cout<<"\tAsym Zero e-displacement: "<<dx0prime*1000<<" mm"<<endl;
 
-  if(debug2) cout<<"\nStrip#\tdist(mm)\trho\tk-photon\tasymStrip"<<endl;
+  if(debug2) cout<<"\nstrip#\tdis(mm)\trho\tk-photon\tasymStrip"<<endl;
   
-for(Int_t s =0; s <=stripNum; s++) {
-    kStripprime=((xStrip+s*2E-4)/zdrift)*E/thetabend; //   scattered photon energy (GeV)
+  //for(Int_t p =0; p <=nPlanes; p++) {
+  for(Int_t s =startStrip; s <=endStrip; s++) {
+    kStripprime=((xgap+s*2E-4)/zdrift)*E/thetabend; //   scattered photon energy (GeV)
     rhoStrip=kStripprime/kprimemax; //  rho (dimensionless)
     dsdrho1 = (1-rhoStrip*(1+a))/(1-rhoStrip*(1-a)); // eqn 22
     dsdrho = 2*pi*re*re/100*a*((rhoStrip*rhoStrip*(1-a)*(1-a)/(1-rhoStrip*(1-a)))+1+dsdrho1*dsdrho1);//eqn. 22
     asymStrip1 = 1-rhoStrip*(1-a);
-    asymStrip = (2*pi*re*re/100*a/dsdrho)*(1-rhoStrip*(1+a))*(1-1/(asymStrip1*asymStrip1)); //
-  
-    cout<<s<<"\t"<<(xStrip+s*1E-4)*1000<<"\t"<<rhoStrip<<"\t"<<kStripprime<<"\t"<<asymStrip<<endl;
+    asymStrip.push_back((2*pi*re*re/100*a/dsdrho)*(1-rhoStrip*(1+a))*(1-1/(asymStrip1*asymStrip1)));
+    
+    //    cout<<s<<"\t"<<(xgap+s*2E-4)*1E3<<"\t"<<rhoStrip<<"\t"<<kStripprime<<"\t"<<asymStrip.at(s)<<endl;
   }
-  return asymStrip;
+  return asymStrip1;
 }
+/****** Comments:
+ *Currently returned value is just for the sake of it 
+ *I should return the array of theoretical asymStrip so that the main program can use it
+ ******/
