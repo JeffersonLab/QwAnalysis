fad9733ca554623b786b084bd08312508e7c83fc
narayan <narayan@b9700a98-9937-0410-b777-f318d6eb5bff>
2012-02-02 08:03:17 +0000

    added polFit.C to pull in the ratio of theoretical asymmetry and experimental asym for each plane and plot it; this is in a very naive state currently; though the combination compiles well and produces a plot, which though is not polarization certainly
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@3909 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Macros/Compton/edetLasCyc/asymFit.C
+++ Extensions/Macros/Compton/edetLasCyc/asymFit.C
@@ -5,21 +5,22 @@
 #include "edetExpAsym.C"
 //#include "comptonRunConstants.h"///don't reinclude
 
-void asymFit() {
-  Int_t runnum=23351;
+Float_t asymFit(Float_t theoExpRatio[nPlanes][nStrips], Float_t stripAsymRMS[nPlanes][nStrips])
+{
   Float_t calcAsym[nStrips],zero[nStrips],stripPlot[nStrips];
-  Float_t stripAsym[nPlanes][nStrips],stripAsymEr[nPlanes][nStrips],stripAsymRMS[nPlanes][nStrips];
+  Float_t stripAsym[nPlanes][nStrips],stripAsymEr[nPlanes][nStrips];//,stripAsymRMS[nPlanes][nStrips];
+  //  Float_t theoExpRatio[nPlanes][nStrips];
 
   theoryAsym(calcAsym);
   edetExpAsym(runnum,runnum,stripAsym,stripAsymEr,stripAsymRMS);
   
-  TGraphErrors *grTheoryAsym;
-  TGraphErrors *grAsymPlane[nPlanes];
+//   TGraphErrors *grTheoryAsym;
+//   TGraphErrors *grAsymPlane[nPlanes];
 
-  for (Int_t s=0; s <endStrip; s++) {
-    stripPlot[s]=s+1;
-    zero[s]=0;
-  }
+//   for (Int_t s=0; s <endStrip; s++) {
+//     stripPlot[s]=s+1;
+//     zero[s]=0;
+//   }
   
   if(IHWP_in) {
     for (Int_t s=0; s <endStrip; s++) {
@@ -27,24 +28,32 @@ void asymFit() {
     }
   }
 
-  TCanvas *cAsym = new TCanvas("cAsym","Asymmetry Vs Strip number",10,10,800,800);
-  cAsym->Divide(2,2);
+//   TCanvas *cAsym = new TCanvas("cAsym","Asymmetry Vs Strip number",10,10,800,800);
+//   cAsym->Divide(2,2);
   
-  TLine *myline = new TLine(0,0,60,0);
-  myline->SetLineStyle(1);
+//   TLine *myline = new TLine(0,0,60,0);
+//   myline->SetLineStyle(1);
   
-  for (Int_t p =startPlane; p <endPlane; p++) {
-    cAsym->cd(p+1);
-    grAsymPlane[p] = new TGraphErrors(endStrip,stripPlot,stripAsym[p],zero,stripAsymRMS[p]);
-    grAsymPlane[p]->GetXaxis()->SetTitle("strip number");
-    grAsymPlane[p]->GetYaxis()->SetTitle("asymmetry");
-    grAsymPlane[p]->SetTitle(Form("Plane %d",p+1));
-    grAsymPlane[p]->Draw("A*");
-    grTheoryAsym = new TGraphErrors(endStrip,stripPlot,calcAsym,zero,zero);
-    grTheoryAsym->Draw("L");    
-    myline->Draw();
-    cAsym->Update();
-  }  
+//   for (Int_t p =startPlane; p <endPlane; p++) {
+//     cAsym->cd(p+1);
+//     grAsymPlane[p] = new TGraphErrors(endStrip,stripPlot,stripAsym[p],zero,stripAsymRMS[p]);
+//     grAsymPlane[p]->GetXaxis()->SetTitle("strip number");
+//     grAsymPlane[p]->GetYaxis()->SetTitle("asymmetry");
+//     grAsymPlane[p]->SetTitle(Form("Plane %d",p+1));
+//     grAsymPlane[p]->Draw("A*");
+//     myline->Draw();
+//     grTheoryAsym = new TGraphErrors(endStrip,stripPlot,calcAsym,zero,zero);
+//     grTheoryAsym->SetLineColor(4);
+//     grTheoryAsym->Draw("L");    
+//     cAsym->Update();
+//   } 
+
+  for (Int_t p =startPlane; p <endPlane; p++) {  
+    for (Int_t s =startStrip; s <endStrip; s++) {  
+      if (stripAsym[p][s]!=0.0 && calcAsym[s]!=0.0) theoExpRatio[p][s]= calcAsym[s]/stripAsym[p][s];
+    }    
+  }
+  return 1.0; ///! just for now, since compiler won't let me pass a variable as an argument in a 'void' type function
 }
 
 /*Comments
