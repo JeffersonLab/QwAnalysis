dc399aa2f5642dbcd8aad6ef4a03c01a86e16789
cornejo <cornejo@b9700a98-9937-0410-b777-f318d6eb5bff>
2012-02-06 22:21:41 +0000

    Copied over the comptom infrastructure to setup the tracking one
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@3929 b9700a98-9937-0410-b777-f318d6eb5bff

---
+++ Extensions/Tracking/macros/utils/format_front_page.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+################################################################################
+## filename:      format_front_page.sh
+## Summary:       This script formats the front Tracking Summary webpage. It
+##                reads (not now, but will) from a database details about
+##                the compton and pushes them to the website. To prevent a
+##                strain on the database only a text file is updated and the
+##                website rebuilds itself from that.
+##
+################################################################################
+## Simple idealistic way to get parameters
+SQLITE=$1
+DBFILE=$2
+RUNNUM=$3
+WEBPAGE=$4
+FIRST100K=$5
+DETECTOR=$6
+################################################################################
+## Configurations
+BASEDIR=${QWANALYSIS}/Extensions/Tracking/macros
+RUNLIST=${WEBPAGE}/_runlist_frontpage
+RUNLISTTEMP=${WEBPAGE}/_runlist_frontpage_temp
+ISINFILE=`grep ${RUNNUM} ${RUNLISTTEMP}`
+################################################################################
+## Check if an entry is already there
+cp ${RUNLIST} ${RUNLISTTEMP}
+if [ "${ISINFILE}x" != "x" ]; then
+   # Just remove the kTRUE label if it's there
+   sed -i "s|${RUNNUM}.kTRUE|${RUNNUM}|" -- ${RUNLISTTEMP}
+   cp ${RUNLISTTEMP} ${RUNLIST}
+else
+   ## Enter this runlist
+   if [ ${FIRST100K} == kTRUE ]; then
+      echo "${RUNNUM}.kTRUE" >>  ${RUNLISTTEMP}
+   else
+      echo "${RUNNUM}" >> ${RUNLISTTEMP}
+   fi
+
+   ## Sort the list (because it loves to get unsorted)
+   sort -nru ${RUNLISTTEMP} > ${RUNLIST}
+fi
+
+## Prepare the website
+echo "<!-- Runs -->" > ${WEBPAGE}/_front_page_runs.html
+for line in `cat ${RUNLIST}`
+do
+   run=`echo ${line} | awk -F. '/[0-9]/ {print $1}'`
+   first100=`echo ${line} | awk -F. '{print $2}'`
+   if [ "x${first100}" == "x" ] ; then
+      first100="no"
+   fi
+   #echo "Run: ${run}   First100K?: ${first100}"
+   ${BASEDIR}/utils/format_run_link.sh ${SQLITE} ${DBFILE} ${run} ${first100} ${DETECTOR} >> ${WEBPAGE}/_front_page_runs.html
+done
+
+# Create the index.html file
+cat ${BASEDIR}/templates/header > ${WEBPAGE}/index.html
+echo "<table class=\"runlist_table\">" >> ${WEBPAGE}/index.html
+
+## Copy over the front page runs to the front page
+cat ${WEBPAGE}/_front_page_runs.html >> ${WEBPAGE}/index.html
+echo "</table>" >> ${WEBPAGE}/index.html
+
+## Copy the footer
+cat ${BASEDIR}/templates/footer >> ${WEBPAGE}/index.html
+###############################################################################
+## 
+
+exit -1;
+
+# Delete from the first_100k list
+${WEBPAGE}/_runlist_first100k_frontpage
+
+## Store this run into the list of runs.
+echo $
+## Update the front page with this run
+## 
+## Update the website adding this run
+## Copy over the headers
+cat templates/header > ${WEBPAGE}/index.html
+echo "<table class=\"runlist_table\">" >> ${WEBPAGE}/index.html
+${BASEDIR}/utils/format_run_link.sh $SQLITE $DBFILE $RUNNUM ${DETECTOR} > ${WEBPAGE}/_front_page_runs_new.html
+
+## Copy over the old front page runs to this new page
+cat ${WEBPAGE}/_front_page_runs.html >> ${WEBPAGE}/_front_page_runs_new.html
+
+## Now make it the new front page runs
+mv ${WEBPAGE}/_front_page_runs_new.html ${WEBPAGE}/_front_page_runs.html
+
+## Copy over the front page runs to the front page
+cat ${WEBPAGE}/_front_page_runs.html >> ${WEBPAGE}/index.html
+echo "</table>" >> ${WEBPAGE}/index.html
+
+## Copy the footer
+cat templates/footer >> ${WEBPAGE}/index.html
+
---
+++ Extensions/Tracking/macros/utils/run_macro.C
@@ -0,0 +1,43 @@
+///////////////////////////////////////////////////////////////////////////////
+// FileName:   run_macro.C
+// Author(s):  Juan Carlos Cornejo <cornejo@jlab.org>
+// Summary:    This is a generic script to run the auto-analyzed macros. It is
+//             called by the autoanalyzer automatically and needs not be
+//             called outside of that script.
+///////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+
+#include <TSystem.h>
+#include <TROOT.h>
+#include <TString.h>
+
+void run_macro(const char* macroName, const char* functionName,
+      const char* localIncludes, Int_t runNumber, Bool_t isFirst100k, Bool_t compile)
+{
+   // Base directory for macros
+   TString macroDir = TString(getenv("QWANALYSIS")) + "/Extensions/Macros/Tracking";
+
+   // First include the default directory
+   gROOT->ProcessLine(".include " + macroDir + "/shared");
+
+   // Then include the local directory
+   gROOT->ProcessLine(".include " + macroDir + "/" + localIncludes);
+
+   // Load the macro
+   TString macro = macroDir + Form("/%s",macroName);
+   std::cout << "Loading macro " << macro << std::endl;
+   if (compile)
+      gROOT->LoadMacro(macro + "++g");
+   else
+      gROOT->LoadMacro(macro);
+
+   // Finally run the script
+   TString first100;
+   if (isFirst100k)
+      first100.Append("kTRUE");
+   else
+      first100.Append("kFALSE");
+   TString command = Form("%s(%d,%s)",functionName,runNumber, first100.Data());
+   gROOT->ProcessLine(command);
+}
