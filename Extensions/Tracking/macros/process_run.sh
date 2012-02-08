2ef9a6123ce53c8336eef4af0ee0631f37ae4698
wsduvall <wsduvall@b9700a98-9937-0410-b777-f318d6eb5bff>
2012-02-08 13:17:21 +0000

    Copied over the comptom infrastructure to setup the tracking one
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/branches/wsduvall@3995 b9700a98-9937-0410-b777-f318d6eb5bff

---
+++ Extensions/Tracking/macros/process_run.sh
@@ -0,0 +1,159 @@
+#!/bin/bash
+################################################################################
+## filename:      process_run.sh
+## Summary:       Creates the PhotonDetector summary and pushes it to the
+##                website.
+##
+################################################################################
+## Make sure group write permission is granted
+umask 002
+
+## First define a few globals, such as the location of necessary programs
+
+## SQLITE3 for database management (assume in path)
+SQLITE=sqlite3
+
+## SQLITE3 database
+DBFILE=${QWSCRATCH}/www/tracking_summaryruns.db
+
+## Path to the directory for log files
+LOGDIR=${QWSCRATCH}/tracking/log
+
+## Path to website directory
+WEBDIR=${QWSCRATCH}/tracking/www
+
+## Path to macros directory
+MACROSDIR=${QWANALYSIS}/Extensions/Macros/Tracking
+
+## Path to base directory
+BASEDIR=${QWANALYSIS}/Extensions/Tracking/macros
+
+## Path to configuration files for the macros
+CONFIGDIR=${BASEDIR}/config.d
+
+## Path to templates
+TEMPLATES=${BASEDIR}/templates
+
+## The macro that runs other macros
+RUNMACRO=${BASEDIR}/utils/run_macro.C
+
+## The default should be analyze the full run and not the first 100K
+FIRST100K=kFALSE
+
+## The default should be to NOT compile the script unless the config file
+## specifically asks for it
+COMPILE=kFALSE
+
+## Local Include path (placeholder)
+INCLUDESDIR=""
+
+## Placeholder for notice on the website
+FIRST100KMESSAGE=""
+
+################################################################################
+## Check for parameters
+DOMACROS=1
+DOINDEXING=1
+for i in $*
+do
+  case $i in
+      --run=*)
+	  RUNNUM=`echo $i | sed 's/--run=//'`
+	  ;;
+      --first100k)
+	  FIRST100K=kTRUE
+	  FIRST100KMESSAGE="(First 100k analysis)"
+	  ;;
+         ## This flag skips running the macros and regenerating the index page 
+         ## if all you want to is to regenerate the run webpage
+      --quick)
+	  DOMACROS=0
+	  DOINDEXING=0
+	  ;;
+      *)
+         echo "Error: option $i is unkown"
+         exit -1;
+         ;;
+   esac
+done
+
+# Run number is a REQUIRED parameter, complain!
+if [ x${RUNNUM} ==  "x" ]
+then
+   echo "Did not provide a run number! Provide it with --run=run_number"
+   exit -1;
+fi
+
+################################################################################
+################################################################################
+################################################################################
+## Process Defines
+DB="${SQLITE} ${DBFILE}"
+################################################################################
+## CD into the right directory
+cd ${BASEDIR}
+
+## Create the log directory
+mkdir -p ${LOGDIR}
+
+## Create the web directory
+mkdir -p ${WEBDIR}/run_$RUNNUM
+
+ROOTFILE=`ls ${QW_ROOTFILES}/Qweak_$RUNNUM.*root`
+if [  -f $ROOTFILE ]
+then
+    ROOTDATE=$(stat -c %y $ROOTFILE | cut -f1 -d".")
+fi
+
+ANALYSISDATE=$(date +"%Y-%m-%d %H:%M")
+## First create the web page 
+RUNPAGE="${WEBDIR}/run_$RUNNUM/run_$RUNNUM.html"
+# The body of the page
+cat ${TEMPLATES}/run_page > $RUNPAGE
+# End this run's page
+cat ${TEMPLATES}/footer_run >> $RUNPAGE
+## Then make the substitutions to make it relevant
+sed -i -e "s|%%RUNNUM%%|$RUNNUM|" $RUNPAGE
+sed -i -e "s|%%ROOTDATE%%|$ROOTDATE|"  $RUNPAGE
+sed -i -e "s|%%ANALYSISDATE%%|$ANALYSISDATE|"  $RUNPAGE
+sed -i -e "s|%%FIRST100KMESSAGE%%|${FIRST100KMESSAGE}|" $RUNPAGE
+
+## Find all macros that need to run and run them
+if [ ${DOMACROS} ==  1 ]
+then
+    echo "Processing config files"
+    for config in `ls ${CONFIGDIR}/*.conf`
+    do
+        if [ -e "${config}.disable" ]
+        then
+            echo "${config} file disabled. Skipping!"
+        else
+            echo "Processing config: ${config}"
+
+            ## Parse the configuration file contents
+            MACRO=`awk -F= ' /macro/ {print $2}' ${config}`
+            FUNCTION=`awk -F= ' /function/ {print $2}' ${config}`
+            if [ -e ${MACROSDIR}/${MACRO} ]
+            then
+                INCLUDESDIR=`awk -F= ' /includesdir/ {print $2}' ${config}`
+                DIRECTORY==`awk -F= ' /directory/ {print $2}' ${config}`
+                COMPILE=`awk -F= ' /compile/ {print $2}' ${config}`
+
+                ## Now after the configuration file has been read, and the script is enabled, process the script
+                echo "Running ${MACRO}"
+                echo "qwroot -l -b -q ${RUNMACRO}\(\"${MACRO}\",\"${FUNCTION}\",\"${INCLUDESDIR}\",${RUNNUM},${FIRST100K},${COMPILE}\) 2>&1 | tee -a ${LOGDIR}/${FUNCTION}_${RUNNUM}.log"
+                nice  qwroot -l -b -q ${RUNMACRO}\(\"${MACRO}\",\"${FUNCTION}\",\"${INCLUDESDIR}\",${RUNNUM},${FIRST100K},${COMPILE}\) 2>&1 | tee -a ${LOGDIR}/${FUNCTION}_${RUNNUM}.log
+            else
+                echo "Macro ${MACROSDIR}/${MACRO} not found"
+            fi
+        fi
+    done
+fi
+
+
+## Format the index page
+if [ ${DOINDEXING} ==  1 ]
+then
+    echo "Formatting the index page"
+    ${BASEDIR}/utils/format_front_page.sh ${SQLITE} ${DBFILE} ${RUNNUM} ${WEBDIR} ${FIRST100K}
+fi
---
+++ Extensions/Tracking/macros/utils/format_run_link.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+###############################################################################
+## A common script to format a link to a specific runfile's summary page
+SQLITE=$1
+DBFILE=$2
+RUN=$3
+FIRST100K=$4
+MESSAGE=""
+DETECTOR=$5
+if [ "x${FIRST100K}" == "xkTRUE" ]; then
+   MESSAGE=" (First 100K analysis) "
+fi
+
+DB="${SQLITE} ${DBFILE}"
+## Extract more info about this file
+#DATETIME=`$DB "select strftime(\"%m/%d/%Y %H:%M:%S\",datetime,\"unixepoch\") from runs where runnum= $RUN"`
+#echo "<tr class=\"runlink_row\"><td><a href=\"run_$RUN/run_$RUN.html\">Run $RUN</td><td>${DATETIME}</td>"
+if [ "x$DETECTOR" == "xelectron" ]; then
+   echo "<tr class=\"runlink_row\"><td><a href=\"Run_$RUN/\">Run $RUN ${MESSAGE}</td></tr>"
+else
+   echo "<tr class=\"runlink_row\"><td><a href=\"run_$RUN/run_$RUN.html\">Run $RUN ${MESSAGE}</td></tr>"
+fi
