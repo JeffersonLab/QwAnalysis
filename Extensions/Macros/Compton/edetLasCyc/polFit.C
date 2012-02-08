5f6faebfbb8e349bfb399fb6c10dd9bb8c26ddb8
wsduvall <wsduvall@b9700a98-9937-0410-b777-f318d6eb5bff>
2012-02-08 13:16:01 +0000

    added polFit.C to pull in the ratio of theoretical asymmetry and experimental asym for each plane and plot it; this is in a very naive state currently; though the combination compiles well and produces a plot, which though is not polarization certainly
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/branches/wsduvall@3983 b9700a98-9937-0410-b777-f318d6eb5bff

---
+++ Extensions/Macros/Compton/edetLasCyc/polFit.C
@@ -0,0 +1,33 @@
+#include <rootClass.h>
+#include "asymFit.C"
+#include "comptonRunConstants.h"
+
+Float_t polFit()
+{
+  Float_t theoExpRatio[nPlanes][nStrips], stripAsymRMS[nPlanes][nStrips];
+  Float_t zero[nStrips],stripPlot[nStrips];
+
+  asymFit(theoExpRatio,stripAsymRMS);
+
+  TGraphErrors *grPolPlane[nPlanes];
+             
+  TCanvas *cPol = new TCanvas("cPol","Pol.measured by each Strip",10,10,700,700); 
+  cPol->Divide(2,2);                                                              
+
+  for (Int_t s=0; s <endStrip; s++) {                                              
+    stripPlot[s]=s+1;
+    zero[s]=0;                                                              
+  }                                                                     
+
+  for (Int_t p =startPlane; p <endPlane; p++) {                                    
+    cPol->cd(p+1);
+    grPolPlane[p] = new TGraphErrors(endStrip,stripPlot,theoExpRatio[p],zero,stripAsymRMS[p]);
+    grPolPlane[p]->GetXaxis()->SetTitle("strip number");
+    grPolPlane[p]->GetYaxis()->SetTitle("Polarization(%)");           
+    grPolPlane[p]->SetTitle(Form("Plane %d",p+1));                                
+    grPolPlane[p]->Draw("A*");                      
+    cPol->Update();
+  }
+  return 1.0;
+}
+  
