bc7b1f5224490e08075073ca071bd79df0980dea
magee <magee@b9700a98-9937-0410-b777-f318d6eb5bff>
2012-04-24 06:48:08 +0000

    This function utilitizes fQwHits to pair the TS meantime with the MD meantime for hits in a given event. Initial findings indicate only a small fraction (perhaps <1%) of these are not paired correctly. Future updates should include a proper marriage between the two based on hit times, similar to the logic found in the fQwHits tree.
    To use, use qwroot and call the function TwoHistos( run number). It will loop through the run's events and produce some correlation plots at the moment. Expanded functionality will be implemented over time.
    
    
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@4354 b9700a98-9937-0410-b777-f318d6eb5bff

---
+++ Extensions/Macros/Dilution/coincidenceMT.cpp
@@ -0,0 +1,189 @@
+//Script to plot the relevant adc spectra for a given set of dilution runs
+//Written by Josh Magee, thanks to Josh Hoskins. 2012-02-27
+
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <TROOT.h>
+//#include <TFile.h>
+//#include <TTree.h>
+//#include <TCanvas.h>
+//#include <TH1F.h>
+//#include <TH2F.h>
+//#include <TStyle.h>
+//#include <THStack.h>
+//#include "QwEvent.h"
+//#include "QwHit.h"
+//#include <vector>
+
+void twoHistoPlot( Int_t runNum ) {
+  gROOT -> Reset();
+  gROOT -> SetStyle("Plain");
+  /*
+    TString cuts  = "fQwHits.fElement==3 && fQwHits.fTimeNs!=0";
+    TString mdcut = "fQwHits.fRegion==5 && fQwHits.fPlane==1";
+    TString tscut = "fQwHits.fRegion==4 && fQwHits.fPlane==1";
+  */
+  TString filename = Form("Qweak_%i.root",runNum);
+
+  TFile *file = new TFile(filename);
+  if ( !file->IsOpen() ) {
+    std::cout <<"Error opening ROOTFILE " << file->GetName() <<endl;
+    return;
+  }
+  else {
+    std::cout <<"Successfully opened ROOTFILE " <<file->GetName() <<endl;
+  }
+
+  TH2F* correlateHisto[7];
+  for (Int_t n=0; n<7; n++) {
+    correlateHisto[n] = new TH2F(Form("correlateHisto[%i]",n), Form("Hit #%i",n), 1600,-300,1300,1600,-300,1300);
+  }
+
+  TH1F* tsMinusMdHisto[7];
+  for (Int_t n=0; n<7; n++) {
+    tsMinusMdHisto[n] = new TH1F(Form("tsMinusMdHisto[%i]",n), Form("Hit #%i",n), 1600,-300,1300);
+  }
+
+
+  TTree*   event_tree  = (TTree*) file ->Get("event_tree");
+  TBranch* ev_branch   = event_tree->GetBranch("events");
+  QwEvent* qwevent     = 0;
+  QwHit*   qwhit       = 0;
+
+  Long64_t num_entries = event_tree->GetEntries();
+  Long64_t localEntry  = 0;
+  Int_t    nhit        = 0;
+
+  Int_t fregion = 0;
+  Int_t felement = 0;
+  Int_t fplane = 0;
+
+  ev_branch -> SetAddress(&qwevent);
+ 
+
+  for (Long64_t i=0; i<num_entries; i++)
+    {//;
+      localEntry = event_tree->LoadTree(i);
+      event_tree->GetEntry(localEntry);
+      
+      if(i % 1000 == 0) {
+	printf(" Total events %d events process so far : %d\n", num_entries, i);
+      }
+      
+      nhit = qwevent->GetNumberOfHits();
+      //    printf("\n");
+      Int_t md_mt = -2000;
+      Int_t ts_mt = -2000;
+
+      Int_t mdarray[7];
+      Int_t tsarray[7];
+
+      Double_t tsMinusMd[7];
+
+
+      Int_t ts_hitnum = 0;
+      Int_t md_hitnum = 0;
+      Int_t hitnumber = 0;	//haven't figured out how to use yet...
+
+      for (Int_t j=0; j<7; j++) {
+        mdarray[j]=0;
+        tsarray[j]=0;
+        tsMinusMd[j]=0;
+      }
+
+      for (Int_t hit=0; hit<nhit; hit++) 
+	{//;;;
+	  qwhit = qwevent->GetHit(hit);
+	  fregion = qwhit -> GetRegion();
+	  felement = qwhit -> GetElement();
+	  fplane = qwhit -> GetPlane();
+    hitnumber = qwhit -> GetHitNumber();
+
+	  if (fregion==4 && fplane==2 && felement==3)  {
+//   tsarray[ts_hitnum] = qwhit -> GetTimeNs();
+     tsarray[hitnumber] = qwhit -> GetTimeNs();
+            ts_hitnum++;
+//	    std::cout <<"Event: " <<i <<"\tTS hit#: " <<ts_hitnum <<endl;
+	  }
+	  if (fregion==5 && fplane==1 && felement==3) {
+//	    mdarray[md_hitnum] = qwhit -> GetTimeNs();
+	    mdarray[hitnumber] = qwhit -> GetTimeNs();
+ 	    md_hitnum++;
+//	    std::cout <<"Event: " <<i <<"\tMD hit#: " <<md_hitnum <<endl;
+	  }
+	} //;;; //end loop through hits
+
+//	if (num_entries % 100000 ) { std::cout <<"ts_mt: " <<ts_mt <<"\tmd_mt " <<md_mt <<"\ttshit: " <<tshit <<"\tmdhit: " <<mdhit<<endl; }
+
+    for (Int_t k=0; k<7; k++) {
+      if(tsarray[k]!=0 && mdarray[k]!=0) correlateHisto[k]->Fill(tsarray[k],mdarray[k]);
+      if(tsarray[k]!=0 && mdarray[k]!=0) tsMinusMdHisto[k]->Fill(tsarray[k]-mdarray[k]);
+    }
+
+    }//; end loop through num_entries
+
+  TCanvas* canvases[3];
+  for (Int_t n=1; n<3; n++) {
+    canvases[n] = new TCanvas(Form("c%i",n),"title");
+    correlateHisto[n] -> Draw("colz");
+  }
+
+  TCanvas* canvases[2];
+  for (Int_t n=1; n<2; n++) {
+    canvases[n] = new TCanvas(Form("d%i",n),"title");
+    tsMinusMdHisto[n] -> Draw();
+  }
+
+
+  for (Int_t n=4; n<=7; n++) {
+    if (tsarray[n]!=0) std::cout <<"The TS has " << n <<"hits: make sure to plot them!\n";
+    if (mdarray[n]!=0) std::cout <<"The MD has " << n <<"hits: make sure to plot them!\n";
+  }
+
+  TCanvas *c4 = new TCanvas("c4","TS - MD MT");
+  tsMinusMdHisto[0] -> Draw();
+
+//  TCanvas *c1 = new TCanvas("c1","title");
+//  histo1->Draw("prof");
+//  TCanvas *c2 = new TCanvas("c2","title2");
+//  histo2->Draw("prof");
+} //end function
+
+
+/*
+  {
+  TTree *etree = (TTree*) file->Get("event_tree");
+  TBranch* ev_branch   = etree->GetBranch("events");
+  QwHit*   qwhit       = 0;
+  
+  QwEvent* event = 0;
+
+  TH2F* histo = new TH2F("histo","title",1600,-300,1300,1600,-300,1300);
+
+  ev_branch->SetAddress(&event);
+
+  Long64_t localEntry  = 0;
+  for (Int_t entry =  0; entry < etree->GetEntries(); entry++) {
+  //printf("-1\n")	;
+  localEntry = event_tree->LoadTree(entry);
+  etree->GetEntry(localEntry);
+  //printf("0\n");
+  std::vector<QwHit*> hitlist = event->GetListOfHits();
+  printf("1\n");
+  Int_t mdhit = -1;
+  Int_t tshit = -1;
+  for (Int_t hit = 0; hit < hitlist.size(); hit++) {
+  if (hitlist[hit].fRegion == 4 && hitlist[hit].fPlane == 1 && histlist[hit].fElement == 3)
+  tshit = hit;
+  if (hitlist[hit].fRegion == 5 && hitlist[hit].fPlane == 1 && histlist[hit].fElement == 3)/
+  mdhit = hit;
+  }
+
+  if (tshit >= 0 && mdhit >= 0) {
+  histo->Fill(hitlist[mdhit].fTimeNs,hitlist[tshit].fTimeNs);
+  }
+  }
+  }
+*/
