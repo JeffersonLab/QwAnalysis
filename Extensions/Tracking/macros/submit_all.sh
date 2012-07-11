f644f34d12bb2951df2ade5e3dd824a6a066cdb0
wdconinc <wdconinc@b9700a98-9937-0410-b777-f318d6eb5bff>
2012-07-11 04:31:33 +0000

    Note: this is branch release_3.01 for event mode pass3 replay.
    Changes to tracking website scripts to allow specifying pass through options,
    and to list HCLog entry in run list.
    
    
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/branches/release_3.01@4629 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Tracking/macros/submit_all.sh
+++ Extensions/Tracking/macros/submit_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in /mss/hallc/qweak/rootfiles/pass1/tracking/Qweak_* ; do
+for i in /mss/hallc/qweak/misc/tracking_rootfiles/pass3/Qweak_* ; do
 	run=`basename $i | cut -d_ -f2 | cut -d. -f1`
-	${QWANALYSIS}/Extensions/Tracking/macros/submit_run.sh --run=${run}
+	${QWANALYSIS}/Extensions/Tracking/macros/submit_run.sh --run=${run} $*
 done
