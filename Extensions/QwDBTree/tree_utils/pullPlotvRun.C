01bc733101701cd3a17dcd2fe5b4397acced20ae
smacewan <smacewan@b9700a98-9937-0410-b777-f318d6eb5bff>
2012-07-24 17:36:17 +0000

    Cleaned up the code a lot and added in titles and axis labels. Also separated the IHWP IN and IHWP OUT data as they should be done (and as they are done in the QwDBGUI. A future version could map the "run numbers" onto the x-axis instead of having this slightly abstract runlet_id axis which DOES NOT CORRESPOND TO RUN NUMBER.
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@4681 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/QwDBTree/tree_utils/pullPlotvRun.C
+++ Extensions/QwDBTree/tree_utils/pullPlotvRun.C
@@ -1,22 +1,68 @@
-void pullPlotvRun(TString var="asym_cagesr", Int_t start = 9418, Int_t stop = 9812){
+/*
+pullPlotvRun v1.1 --smacewan (July 2012)
+pullPlotvRun should be run while in a ROOT session with a QWeak dbROOTfile loaded into memory.
+*/
 
+
+void pullPlotvRun(TString var="asym_mdallbars", Int_t start = 9418, Int_t stop = 9812){
 gStyle->SetOptStat("eMR");
-gStyle->SetOptLogy(1);
 
+Double_t mean_in, mean_out;
 
-Double_t mean;
-tree->Draw(Form("%s>>hh",var.Data()),Form("%s.n>0",var.Data()),"goff");
+TString cut = Form("%s.n>0 && run>=%d && run<=%d",var.Data(),start,stop);
 
-mean = hh->GetMean();
+//Get means for pull calculation
+tree->Draw(Form("%s",var.Data()),Form("ihwp_setting==0 && %s",cut.Data()),"goff");
+mean_out = htemp->GetMean();
+tree->Draw(Form("%s",var.Data()),Form("ihwp_setting==1 && %s",cut.Data()),"goff");
+mean_in = htemp->GetMean();
 
-TCanvas *c1 = new TCanvas();
-c1->Divide(1,2);
+TCanvas *c1 = new TCanvas("c1","Pull Plots",1200,750);
+c1->Divide(2,2);
 
+//Plots
 c1->cd(1);
-tree->Draw(Form("(%s-%f)/%s.err>>h1",var.Data(),mean,var.Data()),Form("%s.n>0 && run>%d && run<%d",var.Data(), start, stop));
+tree->Draw(Form("(%s-%f)/%s.err>>hin",var.Data(),mean_in,var.Data()),Form("ihwp_setting==1 && %s",cut.Data()));
+c1->cd(3);
+tree->Draw(Form("((%s-%f)/%s.err):runlet_id>>hhin",var.Data(),mean_in,var.Data()),Form("ihwp_setting==1 && %s",cut.Data()));
 
 c1->cd(2);
+tree->Draw(Form("(%s-%f)/%s.err>>hout",var.Data(),mean_out,var.Data()),Form("ihwp_setting==0 && %s",cut.Data()));
+c1->cd(4);
+tree->Draw(Form("((%s-%f)/%s.err):runlet_id>>hhout",var.Data(),mean_out,var.Data()),Form("ihwp_setting==0 && %s",cut.Data()));
 
-tree->Draw(Form("((%s-%f)/%s.err):run>>h2",var.Data(),mean,var.Data()),Form("%s.n>0 && run>%d && run<%d",var.Data(), start, stop));
 
+//Set Titles
+hin->SetTitle(Form("Pull Plot for %s [IHWP IN]",var.Data()));
+hhin->SetTitle(Form("Pull versus Runlet_id for %s [IHWP IN]",var.Data()));
+hout->SetTitle(Form("Pull Plot for %s [IHWP OUT]",var.Data()));
+hhout->SetTitle(Form("Pull versus Runlet_id for %s [IHWP OUT]",var.Data()));
+
+hin->GetXaxis()->SetTitle("Pull");
+hhin->GetXaxis()->SetTitle("Runlet_id");
+hout->GetXaxis()->SetTitle("Pull");
+hhout->GetXaxis()->SetTitle("Runlet_id");
+
+hin->GetYaxis()->SetTitle("Entries");
+hhin->GetYaxis()->SetTitle("Pull");
+hout->GetYaxis()->SetTitle("Entries");
+hhout->GetYaxis()->SetTitle("Pull");
+
+
+//Set Style
+hin->SetLineColor(kBlue);
+hhin->SetMarkerStyle(21);
+hhin->SetMarkerColor(kBlue);
+hhin->SetMarkerSize(0.3);
+
+hout->SetLineColor(kRed);
+hhout->SetMarkerStyle(21);
+hhout->SetMarkerColor(kRed);
+hhout->SetMarkerSize(0.3);
+
+//Redraw plots to get formatting right...didn't want to define histograms before drawing
+c1->cd(1);hin->Draw();
+c1->cd(2);hout->Draw();
+c1->cd(3);hhin->Draw();
+c1->cd(4);hhout->Draw();
 }
