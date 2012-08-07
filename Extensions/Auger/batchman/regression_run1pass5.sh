fd7b3371a3b825c36e59e2bf7036d526ec2b4423
pking <pking@b9700a98-9937-0410-b777-f318d6eb5bff>
2012-08-07 15:33:16 +0000

    NOTE:  this is on branch release_3.02.
    Small changes in the job processing scripts for run1pass5.
    
    
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/branches/release_3.02@4718 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Auger/batchman/regression_run1pass5.sh
+++ Extensions/Auger/batchman/regression_run1pass5.sh
@@ -15,14 +15,14 @@ fi
 linRegBluePath=/group/qweak/QwAnalysis/Linux_CentOS5.3-x86_64/linRegBlue_v6/run1Config
 outputPath=/work/hallc/qweak/QwAnalysis/run1/pass5regression
 rootFileStem=QwPass5_
-dbName=qw_test_20120720
+dbName=qw_run1_pass5
 
 ln -s $QW_ROOTFILES/$rootFileStem$run.$seg.trees.root $QW_ROOTFILES/${rootFileStem}$run.$seg.root 
 
 for setName in "std" "5+1" "set3" "set4" "set5" "set6" "set7" "set8" "set9" "set10"
 do
-  echo $linRegBluePath/doubleRegPass_run1pass4.sh $run $seg $outputPath/$setName $WORKDIR ${rootFileStem} $dbName $setName
-  $linRegBluePath/doubleRegPass_run1pass4.sh $run $seg $outputPath/$setName $WORKDIR ${rootFileStem} $dbName $setName
+  echo $linRegBluePath/doubleRegPass_run1pass5.sh $run $seg $outputPath/$setName $WORKDIR ${rootFileStem} $dbName $setName
+  $linRegBluePath/doubleRegPass_run1pass5.sh $run $seg $outputPath/$setName $WORKDIR ${rootFileStem} $dbName $setName
 done
 
 if [[ -L $QW_ROOTFILES/${rootFileStem}$run.$seg.root ]] ; then
