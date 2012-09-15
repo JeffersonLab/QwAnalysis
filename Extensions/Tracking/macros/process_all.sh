a13a1ec5282c72e1c412389e7ab09434397d92a7
vmgray <vmgray@b9700a98-9937-0410-b777-f318d6eb5bff>
2012-09-15 00:54:33 +0000

    Changed paths in macro scripst to pass3b.
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/branches/release_3.01@4757 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Tracking/macros/process_all.sh
+++ Extensions/Tracking/macros/process_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in /mss/hallc/qweak/misc/tracking_rootfiles/pass3/Qweak_* ; do
+for i in /mss/hallc/qweak/rootfiles/tracking_pass3b/Qweak_* ; do
 	run=`basename $i | cut -d_ -f2 | cut -d. -f1`
 	${QWANALYSIS}/Extensions/Tracking/macros/process_run.sh --run=${run} $*
 done
--- Extensions/Tracking/macros/submit_all.sh
+++ Extensions/Tracking/macros/submit_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export QWSCRATCH=/group/qweak/QwAnalysis/common/QwScratch
-for i in /mss/hallc/qweak/misc/tracking_rootfiles/pass3/Qweak_* ; do
+for i in /mss/hallc/qweak/rootfiles/tracking_pass3b/Qweak_* ; do
 	run=`basename $i | cut -d_ -f2 | cut -d. -f1`
 	${QWANALYSIS}/Extensions/Tracking/macros/submit_run.sh --run=${run} $*
 done
