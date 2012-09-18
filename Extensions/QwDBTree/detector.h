5364ee80d8d0d0b62a4333c013ffc92ba6c1f740
wsduvall <wsduvall@b9700a98-9937-0410-b777-f318d6eb5bff>
2012-09-18 19:44:08 +0000

    Cleaned up organization of code to make future work simpler
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@4777 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/QwDBTree/detector.h
+++ Extensions/QwDBTree/detector.h
@@ -78,34 +78,4 @@ class QwBeamDet : public QwDetector
         TString query(void);
 };
 
-// forward declare
-class QwParse;
-
-class QwRunlet
-{
-    // pointer to database
-    TSQLServer* db;
-
-    // runlet data
-    vector<Int_t> runlet_id;
-    vector<Int_t> run;
-    vector<Int_t> slug;
-    vector<Int_t> ihwp_setting;
-    vector<Int_t> phwp_setting;
-    vector<Int_t> wien_reversal;
-
-    // methods
-    TString runlet_query(void);
-    TString runlet_temp_table_create(TString reg_type);
-    TString runlet_temp_table_unreg_create(TString reg_type);
-
-    public:
-    // fuction to set the detector name
-    QwRunlet(TSQLServer* db_pointer) {db = db_pointer;};
-    void fill(QwParse &reg_type);
-    void branch(TTree* tree, vector<Int_t> &values);
-    vector<Int_t> get_runlets(void);
-    void get_data_for_runlet(Int_t j, vector<Int_t> &values);
-};
-
 #endif
