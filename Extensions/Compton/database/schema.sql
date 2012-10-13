08f506a570362cde542e4759dbf376a8f9e41fb1
cornejo <cornejo@b9700a98-9937-0410-b777-f318d6eb5bff>
2012-10-13 01:47:25 +0000

    Schema for Compton Pass2 Database
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@4824 b9700a98-9937-0410-b777-f318d6eb5bff

---
+++ Extensions/Compton/database/schema.sql
@@ -0,0 +1,105 @@
+create table if not exists good_for (
+  good_for_id INTEGER PRIMARY KEY ASC,
+  type TEXT
+);
+
+create table if not exists run_quality (
+  run_quality_id INTEGER PRIMARY KEY ASC,
+  type TEXT
+);
+
+create table if not exists run (
+  run_id INTEGER PRIMARY KEY ASC,
+  run_number INT UNSIGNED,
+  slug INT UNSIGNED,
+  run_type TEXT,
+  good_for_id INT,
+  run_quality_id INT,
+  start_time DATETIME,
+  end_time DATETIME,
+  n_mps INT UNSIGNED,
+  n_qrt INT UNSIGNED,
+  comment TEXT
+);
+
+create table if not exists laser_cycle (
+  laser_cycle_id INTEGER PRIMARY KEY ASC,
+  run_id INT UNSIGNED,
+  cycle_number INT UNSIGNED
+);
+
+create table if not exists slow_control_detector (
+  slow_control_detector_id INTEGER PRIMARY KEY ASC,
+  name TEXT,
+  units TEXT,
+  comment TEXT
+);
+
+create table if not exists slow_controls_settings (
+  slow_controls_settings_id INTEGER PRIMARY KEY ASC,
+  run_id INT UNSIGNED,
+  ihwp_1 char(3),
+  ihwp_2 char(3),
+  laser_pol_direction char(5)
+);
+
+create table if not exists slow_controls_data (
+  slow_controls_data_id INTEGER PRIMARY KEY ASC,
+  slow_controls_detector_id INT UNSIGNED,
+  laser_cycle_id INT UNSIGNED,
+  value float,
+  error float
+);
+
+create table if not exists analysis (
+  analysis_id INTEGER PRIMARY KEY ASC,
+  laser_cycle_id INT UNSIGNED,
+  analyzer_version TEXT,
+  laser_state char(3),
+  n_mps INT UNSIGNED,
+  n_qrt INT UNSIGNED,
+  first_event INT UNSIGNED,
+  last_event INT UNSIGNED
+);
+
+create table if not exists measurement_type (
+  measurement_type_id char(4),
+  units TEXT,
+  title TEXT
+);
+
+create table if not exists electron_data (
+  electron_data_id INTEGER PRIMARY KEY ASC,
+  analysis_id INT UNSIGNED,
+  measurement_type_id char(4),
+  plane UNSIGNED INT,
+  strip UNSIGNED INT,
+  n UNSIGNED INT,
+  value float,
+  error float
+);
+
+create table if not exists photon_data (
+  photon_data_id INTEGER PRIMARY KEY ASC,
+  analysis_id INT UNSIGNED,
+  measurement_type_id char(4),
+  n INT UNSIGNED,
+  value float,
+  error float
+);
+
+create table if not exists beam (
+  beam_id INTEGER PRIMARY KEY ASC,
+  analysis_id INT UNSIGNED,
+  monitor_id INT UNSIGNED,
+  measurement_type_id char(4),
+  n INT UNSIGNED,
+  value float,
+  error float
+);
+
+create table if not exists monitor (
+  monitor_id INTEGER PRIMARY KEY ASC,
+  quantity TEXT,
+  title TEXT
+);
