9e834082fe3ace2a7437c3c88ebdd04728db2c75
wsduvall <wsduvall@b9700a98-9937-0410-b777-f318d6eb5bff>
2012-10-31 18:25:57 +0000

    Added mdNpmtavg to all regressions
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/branches/balewski.correlator@4882 b9700a98-9937-0410-b777-f318d6eb5bff

--- treeBasedCorrelator/linRegBlue_v7/run2pass5Config/regression_run2pass5.sh
+++ treeBasedCorrelator/linRegBlue_v7/run2pass5Config/regression_run2pass5.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 run=$1
 seg=$2
-WORKDIR=$QWROOTFILES
 
 if [ $# -ne 2 ] ; then
    echo Regression wrapper script needs run_number and segment_number
@@ -13,20 +12,17 @@ if [[ ! -n "$WORKDIR" || ! -d $WORKDIR ]]; then
     exit
 fi
 
-#linRegBluePath=/group/qweak/QwAnalysis/Linux_CentOS5.3-x86_64/linRegBlue_v6/run1Config
-linRegBluePath=/home/wsduvall/linRegBlue_v6
-#outputPath=/work/hallc/qweak/QwAnalysis/run1/pass4bregression
-outputPath=$QWROOTFILES/reg
-rootFileStem=QwPass5beta_
+linRegBluePath=/group/qweak/QwAnalysis/Linux_CentOS5.3-x86_64/linRegBlue_v6/run1Config
+outputPath=/work/hallc/qweak/QwAnalysis/run1/pass5regression
+rootFileStem=QwPass5_
 dbName=qw_run2_pass5
 
-#ln -s $QWROOTFILES/$rootFileStem$run.$seg.trees.root $QWROOTFILES/${rootFileStem}$run.$seg.root 
+ln -s $QW_ROOTFILES/$rootFileStem$run.$seg.trees.root $QW_ROOTFILES/${rootFileStem}$run.$seg.root 
 
-#for setName in "std" "5+1" "set3" "set4" "set5" "set6" "set7" "set8" "set9" "set10"
-for setName in "set7"
+for setName in "std" "5+1" "set3" "set4" "set5" "set6" "set7" "set8" "set9" "set10"
 do
   echo $linRegBluePath/doubleRegPass_run2pass5.sh $run $seg $outputPath/$setName $WORKDIR ${rootFileStem} $dbName $setName
-  $linRegBluePath/doubleRegPass_run2pass5.sh $run $seg $outputPath/$setName $WORKDIR ${rootFileStem} $dbName $setName
+  $linRegBluePath/run2pass5Config/doubleRegPass_run2pass5.sh $run $seg $outputPath/$setName $WORKDIR ${rootFileStem} $dbName $setName
 done
 
 if [[ -L $QW_ROOTFILES/${rootFileStem}$run.$seg.root ]] ; then
