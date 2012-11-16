1554b7d43c4398683f0bf00b2ced650f262ac380
jhoskins <jhoskins@b9700a98-9937-0410-b777-f318d6eb5bff>
2012-11-16 16:18:44 +0000

    Turn log directories back on for bmod linRegBlue.
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/branches/balewski.correlator@4954 b9700a98-9937-0410-b777-f318d6eb5bff

--- treeBasedCorrelator/linRegBlue_v7/run1pass5ModConfig/doubleRegPass.sh
+++ treeBasedCorrelator/linRegBlue_v7/run1pass5ModConfig/doubleRegPass.sh
@@ -2,28 +2,29 @@
 
 run=$1
 iseg=$2
+outputPath=$3
 
 scriptPath=`dirname $0`
-src_dir=${scriptPath}/..
+src_dir=/group/qweak/QwAnalysis/Linux_CentOS5.3-x86_64/linRegBlue_v7
 
 #logPath='./out/'
 logPath='/work/hallc/qweak/QwAnalysis/run1/pass5_bmod_regression/lrb_bmod/out'
 mxEve=2223344
 
-seg=00$iseg
-if [ $iseg -ge 10 ]; then seg=0$iseg; fi
+seg=$iseg
+#if [ $iseg -ge 10 ]; then seg=0$iseg; fi
 
 echo doubleRegPass  run.seg=$run.$seg
 destDir=treeX/regR$run.$seg
 
-if [ $# -ne 2 ] ; then
-   echo provide run +segemnt 
+if [ $# -ne 3 ] ; then
+   echo provide run +segemnt output directory
    exit
 fi
 
 
 if [ -d out ] ; then
-  mv -f out outOld
+    mv -vf out $outputPath/outOld
 fi
 
 
@@ -36,15 +37,19 @@ fi
 
 mkdir out
 
+if [ -d $outputPath/out ]; then
+    mkdir $outputPath/out
+fi
+
 #.......................................
 echo -n regPass1 started ...
 date
 time 
-$src_dir/linRegBlue  $run $seg $mxEve blueReg_bmod.conf >& $logPath/logS1
+$src_dir/linRegBlue  $run $seg $mxEve $scriptPath/blueReg_bmod.conf >& $logPath/logS1_${run}_${seg}
 
 if [ $? -ne 0 ] ; then 
    echo failed reg-pass1 for run $run.$seg
-   mv out out-regAbort1-$run.$seg
+   mv -vf out $outputPath/out-regAbort1-$run.$seg
    echo abandon this run
    exit
 fi
@@ -52,11 +57,11 @@ fi
 #.......................................
 echo regPass1 successful 
 slopeFile=blueR$run.$seg.slope.root
-mv  out/blueR$run.$seg\new.slope.root out/$slopeFile
+mv -vf out/blueR$run.$seg\new.slope.root out/$slopeFile
 
 if [ $? -ne 0 ] ; then 
    echo failed to find slope matrix  for run $run.$seg
-   mv out out-regAbort2-$run.$seg
+   mv -vf out $outputPath/out-regAbort2-$run.$seg
    echo abandon this run
    exit
 fi
@@ -64,11 +69,11 @@ fi
 
 #.......................................
 echo regPass2 started 
-time $src_dir/linRegBlue  $run $seg $mxEve  blueReg_full.conf $slopeFile  >& $logPath/logS2
+time $src_dir/linRegBlue  $run $seg $mxEve  $scriptPath/blueReg_full.conf $slopeFile  >& $logPath/logS2_${run}_${seg}
 
 if [ $? -ne 0 ] ; then 
    echo failed reg-pass2 for run $run.$seg
-   mv out out-regAbort3-$run.$seg
+   mv -vf out $outputPath/out-regAbort3-$run.$seg
    echo abandon this run
    exit
 fi
@@ -76,17 +81,21 @@ fi
 #.......................................
 echo regPass2 successful destDir=$destDir ....
 
+#mkdir $outputPath/lrb_bmod_$run_$seg
+
+echo "copying out to $outputPath/lrb_bmod_$run_$seg"
+mv -vf out $outputPath/lrb_bmod_$run_$seg
 
 #.......................................
-echo generate txt file for DB upload
+#echo generate txt file for DB upload
 
-root -b -q prCsvRecordTwo.C'('${run}.${seg}',"out/")'
-if [ $? -ne 0 ] ; then 
-   echo failed reg-pass2 for run $run.$seg
-   mv out out-regAbort3-$run.$seg
-   echo abandon this run
-   exit
-fi
+#root -b -q prCsvRecordTwo.C'('${run}.${seg}',"out/")'
+#if [ $? -ne 0 ] ; then 
+#   echo failed reg-pass2 for run $run.$seg
+#   mv out out-regAbort3-$run.$seg
+#   echo abandon this run
+#   exit
+#fi
 
 #mv out $destDir
 date
