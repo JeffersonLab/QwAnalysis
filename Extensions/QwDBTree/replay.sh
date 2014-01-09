2dffe64db8be4b066f54dd5105a4804e2c0fddd3
wsduvall <wsduvall@b9700a98-9937-0410-b777-f318d6eb5bff>
2014-01-09 17:02:48 +0000

    reply.sh script now will generate good/suspect/bad rootfiles. Replay to follow.
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@5538 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/QwDBTree/replay.sh
+++ Extensions/QwDBTree/replay.sh
@@ -34,10 +34,17 @@ mkdir -p /net/cdaq/cdaql5data/qweak/db_rootfiles/run2/parity
 mkdir -p /net/cdaq/cdaql5data/qweak/db_rootfiles/run2/yield
 mkdir -p /net/cdaq/cdaql5data/qweak/db_rootfiles/run2/al/yield
 mkdir -p /net/cdaq/cdaql5data/qweak/db_rootfiles/run2/al
+# Suspect and bad rootfiles
+mkdir -p /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect
+mkdir -p /net/cdaq/cdaql5data/qweak/db_rootfiles/bad
+cp -r /net/cdaq/cdaql5data/qweak/db_rootfiles/run1/ /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect
+cp -r /net/cdaq/cdaql5data/qweak/db_rootfiles/run2/ /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect
+cp -r /net/cdaq/cdaql5data/qweak/db_rootfiles/run1/ /net/cdaq/cdaql5data/qweak/db_rootfiles/bad
+cp -r /net/cdaq/cdaql5data/qweak/db_rootfiles/run2/ /net/cdaq/cdaql5data/qweak/db_rootfiles/bad
 
-# Run 1
-./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien0.list --mapdir mapfiles/run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/run1/wien0/parity/
-./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien0.list --mapdir mapfiles/y_run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/run1/wien0/yield/
+# Run 1 good
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5 --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien0.list --mapdir mapfiles/run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/run1/wien0/parity/
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5 --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien0.list --mapdir mapfiles/y_run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/run1/wien0/yield/
 ./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien1.list --mapdir mapfiles/run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/run1/wien1/parity/
 ./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien1.list --mapdir mapfiles/y_run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/run1/wien1/yield/
 ./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien2.list --mapdir mapfiles/run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/run1/wien2/parity/
@@ -50,7 +57,7 @@ mkdir -p /net/cdaq/cdaql5data/qweak/db_rootfiles/run2/al
 ./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien5.list --mapdir mapfiles/y_run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/run1/wien5/yield/
 ./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --mapdir mapfiles/run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/run1/parity/
 ./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --mapdir mapfiles/y_run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/run1/yield/
-# Run 2
+# Run 2 good
 ./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien6.list --mapdir mapfiles/run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/run2/wien6/parity/
 ./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien6.list --mapdir mapfiles/y_run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/run2/wien6/yield/
 ./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien7.list --mapdir mapfiles/run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/run2/wien7/parity/
@@ -67,8 +74,76 @@ mkdir -p /net/cdaq/cdaql5data/qweak/db_rootfiles/run2/al
 ./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target DS-4%-Aluminum --runlist mapfiles/lists/run2/al_ds_run2.list --mapdir mapfiles/y_run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/run2/al/yield
 ./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target US-2%-Aluminum --runlist mapfiles/lists/run2/al_us_run2.list --mapdir mapfiles/run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/run2/al/
 ./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target US-2%-Aluminum --runlist mapfiles/lists/run2/al_us_run2.list --mapdir mapfiles/y_run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/run2/al/yield
+# Run 1 suspect
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5 --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien0.list --mapdir mapfiles/run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run1/wien0/parity/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5 --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien0.list --mapdir mapfiles/y_run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run1/wien0/yield/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien1.list --mapdir mapfiles/run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run1/wien1/parity/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien1.list --mapdir mapfiles/y_run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run1/wien1/yield/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien2.list --mapdir mapfiles/run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run1/wien2/parity/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien2.list --mapdir mapfiles/y_run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run1/wien2/yield/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien3.list --mapdir mapfiles/run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run1/wien3/parity/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien3.list --mapdir mapfiles/y_run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run1/wien3/yield/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien4.list --mapdir mapfiles/run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run1/wien4/parity/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien4.list --mapdir mapfiles/y_run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run1/wien4/yield/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien5.list --mapdir mapfiles/run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run1/wien5/parity/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien5.list --mapdir mapfiles/y_run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run1/wien5/yield/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --mapdir mapfiles/run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run1/parity/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --mapdir mapfiles/y_run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run1/yield/ --suspect
+# Run 2 suspect
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien6.list --mapdir mapfiles/run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run2/wien6/parity/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien6.list --mapdir mapfiles/y_run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run2/wien6/yield/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien7.list --mapdir mapfiles/run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run2/wien7/parity/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien7.list --mapdir mapfiles/y_run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run2/wien7/yield/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien8.list --mapdir mapfiles/run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run2/wien8/parity/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien8.list --mapdir mapfiles/y_run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run2/wien8/yield/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien9.list --mapdir mapfiles/run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run2/wien9/parity/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien9.list --mapdir mapfiles/y_run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run2/wien9/yield/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien10.list --mapdir mapfiles/run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run2/wien10/parity/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien10.list --mapdir mapfiles/y_run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run2/wien10/yield/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --mapdir mapfiles/run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run2/parity/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --mapdir mapfiles/y_run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run2/yield/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target DS-4%-Aluminum --runlist mapfiles/lists/run2/al_ds_run2.list --mapdir mapfiles/run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run2/al/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target DS-4%-Aluminum --runlist mapfiles/lists/run2/al_ds_run2.list --mapdir mapfiles/y_run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run2/al/yield --suspect
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target US-2%-Aluminum --runlist mapfiles/lists/run2/al_us_run2.list --mapdir mapfiles/run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run2/al/ --suspect
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target US-2%-Aluminum --runlist mapfiles/lists/run2/al_us_run2.list --mapdir mapfiles/y_run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect/run2/al/yield --suspect
+# Run 1 bad
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5 --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien0.list --mapdir mapfiles/run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run1/wien0/parity/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5 --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien0.list --mapdir mapfiles/y_run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run1/wien0/yield/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien1.list --mapdir mapfiles/run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run1/wien1/parity/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien1.list --mapdir mapfiles/y_run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run1/wien1/yield/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien2.list --mapdir mapfiles/run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run1/wien2/parity/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien2.list --mapdir mapfiles/y_run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run1/wien2/yield/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien3.list --mapdir mapfiles/run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run1/wien3/parity/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien3.list --mapdir mapfiles/y_run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run1/wien3/yield/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien4.list --mapdir mapfiles/run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run1/wien4/parity/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien4.list --mapdir mapfiles/y_run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run1/wien4/yield/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien5.list --mapdir mapfiles/run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run1/wien5/parity/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run1/wien5.list --mapdir mapfiles/y_run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run1/wien5/yield/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --mapdir mapfiles/run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run1/parity/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run1_pass5b --target HYDROGEN-CELL --mapdir mapfiles/y_run1/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run1/yield/ --bad
+# Run 2 bad
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien6.list --mapdir mapfiles/run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run2/wien6/parity/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien6.list --mapdir mapfiles/y_run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run2/wien6/yield/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien7.list --mapdir mapfiles/run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run2/wien7/parity/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien7.list --mapdir mapfiles/y_run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run2/wien7/yield/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien8.list --mapdir mapfiles/run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run2/wien8/parity/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien8.list --mapdir mapfiles/y_run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run2/wien8/yield/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien9.list --mapdir mapfiles/run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run2/wien9/parity/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien9.list --mapdir mapfiles/y_run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run2/wien9/yield/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien10.list --mapdir mapfiles/run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run2/wien10/parity/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --runlist mapfiles/lists/run2/wien10.list --mapdir mapfiles/y_run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run2/wien10/yield/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --mapdir mapfiles/run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run2/parity/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target HYDROGEN-CELL --mapdir mapfiles/y_run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run2/yield/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target DS-4%-Aluminum --runlist mapfiles/lists/run2/al_ds_run2.list --mapdir mapfiles/run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run2/al/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target DS-4%-Aluminum --runlist mapfiles/lists/run2/al_ds_run2.list --mapdir mapfiles/y_run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run2/al/yield --bad
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target US-2%-Aluminum --runlist mapfiles/lists/run2/al_us_run2.list --mapdir mapfiles/run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run2/al/ --bad
+./tree_fill --host 127.0.0.1 --db qw_run2_pass5b --target US-2%-Aluminum --runlist mapfiles/lists/run2/al_us_run2.list --mapdir mapfiles/y_run2/ --outdir /net/cdaq/cdaql5data/qweak/db_rootfiles/bad/run2/al/yield --bad
 
 rm -rf /group/qweak/www/html/private/db_rootfiles/run1
 rm -rf /group/qweak/www/html/private/db_rootfiles/run2
+rm -rf /group/qweak/www/html/private/db_rootfiles/suspect
+rm -rf /group/qweak/www/html/private/db_rootfiles/bad
 cp -r /net/cdaq/cdaql5data/qweak/db_rootfiles/run1 /group/qweak/www/html/private/db_rootfiles
 cp -r /net/cdaq/cdaql5data/qweak/db_rootfiles/run2 /group/qweak/www/html/private/db_rootfiles
+cp -r /net/cdaq/cdaql5data/qweak/db_rootfiles/suspect /group/qweak/www/html/private/db_rootfiles
+cp -r /net/cdaq/cdaql5data/qweak/db_rootfiles/bad /group/qweak/www/html/private/db_rootfiles
