9c4e012d2e1645a3588ad2f44ccf9de31aa28766
wdconinc <wdconinc@b9700a98-9937-0410-b777-f318d6eb5bff>
2014-03-31 01:49:18 +0000

    The macros submission script had the analysis pass hard-coded.  As we all know, hard-coding leads to errors.  The analysis pass now has to be specified on the command line.  If you don't specify it, the jobs will crash rather than producing output for the wrong pass.
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@5631 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Tracking/macros/submit_run.sh
+++ Extensions/Tracking/macros/submit_run.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 if [ $# -lt 1 ] ; then
-	echo "Usage: `basename $0` --run=<run> [pass-through options]"
+	echo "Usage: `basename $0` --run=<run> --pass=<pass> [pass-through options]"
 	exit
 fi
 
 DATE=`date +%Y%m%d`
 TIME=`date +%H%M%S`
 
-RUN=${1/--run=/}
-shift
+for arg in $* ; do
+	case $arg in
+	--run=*)
+		RUN=${arg/--run=/}
+		;;
+	--pass=*)
+		PASS=${arg/--pass=/}
+		;;
+	esac
+done
 
 rm -f ${QWANALYSIS}/Extensions/Tracking/macros/submit_run.sed
 echo "s|%QWANALYSIS%|${QWANALYSIS}|g"	>> ${QWANALYSIS}/Extensions/Tracking/macros/submit_run.sed
@@ -17,6 +25,7 @@ echo "s|%QWSCRATCH%|${QWSCRATCH}|g"	>> ${QWANALYSIS}/Extensions/Tracking/macros/
 echo "s|%USER%|${USER}|g"	>> ${QWANALYSIS}/Extensions/Tracking/macros/submit_run.sed
 echo "s|%DATE%|${DATE}|g"	>> ${QWANALYSIS}/Extensions/Tracking/macros/submit_run.sed
 echo "s|%TIME%|${TIME}|g"	>> ${QWANALYSIS}/Extensions/Tracking/macros/submit_run.sed
+echo "s|%PASS%|${PASS}|g"	>> ${QWANALYSIS}/Extensions/Tracking/macros/submit_run.sed
 echo "s|%RUN%|${RUN}|g"		>> ${QWANALYSIS}/Extensions/Tracking/macros/submit_run.sed
 echo "s|%OTHER%|$*|g"		>> ${QWANALYSIS}/Extensions/Tracking/macros/submit_run.sed
 
