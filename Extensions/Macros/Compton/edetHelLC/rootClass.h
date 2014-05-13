1d71aaaf346361cf931f05f2f0e144fb1d143331
narayan <narayan@b9700a98-9937-0410-b777-f318d6eb5bff>
2014-05-13 21:20:18 +0000

    The file rootClass.h is called by the macro, hence for keeping it complete, this header file should be made part of this package
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@5715 b9700a98-9937-0410-b777-f318d6eb5bff

---
+++ Extensions/Macros/Compton/edetHelLC/rootClass.h
@@ -0,0 +1,49 @@
+#ifndef __rootCLASS_H 
+#define __rootCLASS_H
+
+#include "math.h"
+#include "TCanvas.h"    
+#include "TChain.h"
+#include "TBranch.h"
+#include "TChain.h"
+#include "TCut.h"
+#include "TGaxis.h"
+#include "TH1D.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TF1.h"
+#include "TFile.h"
+#include "TLegend.h"
+#include "TMath.h"
+#include "TPaveStats.h"
+#include "TPaveText.h"
+#include "TProfile.h"
+#include "TROOT.h"
+#include "TTree.h"
+#include "TGraphErrors.h"
+#include "TStyle.h"
+#include "TCint.h"
+#include "TSystem.h"
+#include "TRandom.h"
+#include "TObject.h"
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <TLeaf.h>
+#include <TMultiGraph.h>
+#include "TMinuit.h"
+#include "TLatex.h"
+#include <algorithm>
+#include <set>
+using namespace std;
+
+char red[] = { 0x1b, '[', '1', ';', '3', '1', 'm', 0 };
+char green[] = { 0x1b, '[', '1', ';', '3', '2', 'm', 0 };
+char blue[] = { 0x1b, '[', '1', ';', '3', '4', 'm', 0 };
+char magenta[] = { 0x1b, '[', '1', ';', '3', '5', 'm', 0 };
+char normal[] = { 0x1b, '[', '0', ';', '3', '9', 'm', 0 };
+
+#endif
