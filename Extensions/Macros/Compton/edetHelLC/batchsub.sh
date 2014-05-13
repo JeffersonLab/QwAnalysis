980b531b2793811b4eaab5b517c82130fdba5470
narayan <narayan@b9700a98-9937-0410-b777-f318d6eb5bff>
2014-05-13 23:14:28 +0000

    the previous version of these scripts were still pointing to old directories of the analysis script. only that is updated the new analysis is being submitted with the current version
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@5716 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Macros/Compton/edetHelLC/batchsub.sh
+++ Extensions/Macros/Compton/edetHelLC/batchsub.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-cd /u/home/narayan/svn/QwAnalysis/Extensions/Macros/Compton/edetHelLC
-#for i in `seq 23444 24051`;
-for i in `seq 23098 25204`;
+#cd /u/home/narayan/svn/QwAnalysis/Extensions/Macros/Compton/edetHelLC
+cd /work/hallc/compton/users/narayan/svn/edetHelLC
+#for i in `seq 23446 23447`;
+#for i in `seq 23098 25204`;
+for i in `seq 22659 25546`;
 #for i in `seq 23220 23530`;
 #for i in `seq 23220 23232`;
 
 do
-  cat amar_submit_header.xml > amar_submit_$i.xml
+  cat batchsub_header.xml > batch_submit_$i.xml
   for seg in `ls /mss/hallc/qweak/polarimetry/rootfiles/pass2/Compton_Pass2b_$i.*.root`;
   do
-    echo '<Input src="mss:'$seg'" dest="'`basename $seg`'"/>' >> amar_submit_$i.xml
+    echo '<Input src="mss:'$seg'" dest="'`basename $seg`'"/>' >> batch_submit_$i.xml
   done
-  cat amar_submit_footer.xml >> amar_submit_$i.xml
-  sed -i -e 's/RUNNUM/'${i}'/g' -- amar_submit_$i.xml
-  jsub -xml amar_submit_$i.xml
-  #cat amar_submit_$i.xml 
-  rm amar_submit_$i.xml
+  cat batchsub_footer.xml >> batch_submit_$i.xml
+  sed -i -e 's/RUNNUM/'${i}'/g' -- batch_submit_$i.xml
+  jsub -xml batch_submit_$i.xml
+  rm batch_submit_$i.xml
 done
