e7ea10df2f6f69cf80c5ac82e30954d93e8456a0
wdconinc <wdconinc@b9700a98-9937-0410-b777-f318d6eb5bff>
2014-05-19 15:40:07 +0000

    Added a few plots (without much testing).
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@5731 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/QwDBLib/bcmdd.cpp
+++ Extensions/QwDBLib/bcmdd.cpp
@@ -50,25 +50,43 @@ int main(int argc, char* argv[]) {
 
     /* Define objects to hold each branch. Define the axis variables in constructor.*/
     QwTreeBranchPlot bcmdd("bcmdd", "runlet_id", "BCM asymmetry double difference (ppm)");
-    bcmdd.get_data_from_tree(tree_asym, "asym_bcmdd", "asym_bcmdd78");
+    bcmdd.get_data_from_tree(tree_asym, "asym_bcmdd");
     //bcmdd.set_weight_value(tree_asym, "asym_qwk_charge");
     bcmdd.CleanZeroes();
 
-
-
-
     /* Create TCanvas. */
-    TCanvas c1("c1");
+    TCanvas c_bcmdd_vs_runlet("c_bcmdd_vs_runlet");
     bcmdd.ValueRunletPlot();
-    c1.Update();
-    c1.Modified();
-    c1.Print("bcmdd_vs_runlet.png");
+    c_bcmdd_vs_runlet.Update();
+    c_bcmdd_vs_runlet.Modified();
+    c_bcmdd_vs_runlet.Print("bcmdd_vs_runlet.png");
 
-    TCanvas c2("c2");
+    TCanvas c_bcmdd_histogram("c_bcmdd_histogram");
     bcmdd.ValuePlot();
-    c2.Update();
-    c2.Modified();
-    c2.Print("bcmdd_histogram.png");
+    c_bcmdd_histogram.Update();
+    c_bcmdd_histogram.Modified();
+    c_bcmdd_histogram.Print("bcmdd_histogram.png");
+
+
+    // set the weight to the error of teh bcm DD asymmetry
+    bcmdd.set_weight_error(tree_asym, "asym_bcmdd");
+
+    TCanvas c_bcmdd_histogram_weighted_by_own_error("c_bcmdd_histogram_weighted_by_own_error");
+    bcmdd.ValuePlot();
+    c_bcmdd_histogram_weighted_by_own_error.Update();
+    c_bcmdd_histogram_weighted_by_own_error.Modified();
+    c_bcmdd_histogram_weighted_by_own_error.Print("bcmdd_weighted_by_own_error.png");
+
+
+    // set the weight to the error of MD all bars
+    bcmdd.set_weight_error(tree_asym, "asym_mdallbars");
+
+    TCanvas c_bcmdd_histogram_weighted_by_mdallbars_error("c_bcmdd_histogram_weighted_by_mdallbars_error");
+    bcmdd.ValuePlot();
+    c_bcmdd_histogram_weighted_by_mdallbars_error.Update();
+    c_bcmdd_histogram_weighted_by_mdallbars_error.Modified();
+    c_bcmdd_histogram_weighted_by_mdallbars_error.Print("bcmdd_weighted_by_mdallbars_error.png");
+
 
     /* Return */
     return 0;
