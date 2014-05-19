1f98f749790b0f4ac76126af1aaca594c6a9ca01
wdconinc <wdconinc@b9700a98-9937-0410-b777-f318d6eb5bff>
2014-05-19 14:50:28 +0000

    Backwards-compatibility fix.  If you have some macros that include QwTrackingTreeLine.h, then it will now include QwTreeLine.h through this header.
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@5730 b9700a98-9937-0410-b777-f318d6eb5bff

---
+++ Tracking/include/QwTrackingTreeLine.h
@@ -0,0 +1,14 @@
+/**
+ * \file   QwTrackingTreeLine.h
+ * \brief  Backwards compatible header file
+ *
+ * \author Wouter Deconinck <wdconinc@jlab.org>
+ * \date   Sun May 19 10:05:29 EDT 2014
+ */
+
+#ifndef QWTRACKINGTREELINE_H
+#define QWTRACKINGTREELINE_H
+
+#include "QwTreeLine.h"
+
+#endif // QWTRACKINGTREELINE_H
