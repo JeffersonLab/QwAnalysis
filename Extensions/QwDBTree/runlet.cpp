d6180cf0f35c710a90ac81f972149d763a8d9d6d
wsduvall <wsduvall@b9700a98-9937-0410-b777-f318d6eb5bff>
2014-05-25 20:54:32 +0000

    One day I'll get it right.
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@5760 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/QwDBTree/runlet.cpp
+++ Extensions/QwDBTree/runlet.cpp
@@ -153,7 +153,6 @@ TString QwRunlet::runlet_temp_table_create(TString reg_type, vector<TString> run
     }
 
     query += "ORDER BY runlet.run_number, runlet.segment_number);\n";
-    cout << query << endl;
 
     return query;
 }
@@ -217,7 +216,7 @@ TString QwRunlet::runlet_temp_table_unreg_create(TString reg_type, vector<TStrin
         if(db_name == "qw_run2_pass5b")
             query += "AND slow_controls_data.value <= -706.5 AND slow_controls_data.value >= -708.5\n";
         if(db_name == "qw_run1_pass5b")
-            query += "AND slow_controls_data.value <= -709.5 AND slow_controls_data.value >= -711.5.5\n";
+            query += "AND slow_controls_data.value <= -709.5 AND slow_controls_data.value >= -711.5\n";
     }
 
     /* if using runlist, cut on run number */
