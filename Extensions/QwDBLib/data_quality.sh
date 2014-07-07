8e66ebe577fef5e697722920d8da9776d9108a73
wdconinc <wdconinc@b9700a98-9937-0410-b777-f318d6eb5bff>
2014-07-07 02:59:40 +0000

    Stuff for the data quality website... in progress.
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@5810 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/QwDBLib/data_quality.sh
+++ Extensions/QwDBLib/data_quality.sh
@@ -5,49 +5,77 @@ db_rootfiles_path="/group/qweak/www/html/private/db_rootfiles"
 data_quality_path="/group/qweak/www/html/private/data_quality"
 #db_rootfiles_path=~/db_rootfiles
 #data_quality_path=.
-data_quality_path="output"
+#data_quality_path="output"
 
 # Plots to be generated
-plots="mdallpmtavg bcmdd bcm"
+plots="offoff/bcmdd on/bcmdd off/bcmdd"
+#plots="on/mdallpmtavg offoff/bcmdd on/bcmdd on/bcm"
 
+# Targets to be generated
+targets="HYDROGEN-CELL"
+#targets="HYDROGEN-CELL DS-4%-Aluminum US-2%-Aluminum"
+
+# Runs to loop over
+runs="run1 run2"
+
+# Setup LD library path
+basefile=`readlink -e $0`
+basedir=`dirname $basefile`
+export LD_LIBRARY_PATH=$basedir:${LD_LIBRARY_PATH}
 
 # Move to output directory
-mkdir -p ${data_quality_path}
-pushd ${data_quality_path}
+mkdir -p "${data_quality_path}"
+pushd "${data_quality_path}"
 
-# Loop over the two runing periods
-for run in 1 2 ; do
+# Loop over the two running periods
+for run in $runs ; do
+    mkdir -p "${data_quality_path}/${run}"
 
     # Loop over targets
-    for target in HYDROGEN-CELL ; do
+    for target in $targets ; do
+        mkdir -p "${data_quality_path}/${run}/${target}"
 
         # Loop over wiens in those runs
-        if [ ${run} == "1" ] ; then wiens="1 2 3 4 5" ; fi
-        if [ ${run} == "2" ] ; then wiens="6 7 8 9 10" ; fi
+        wiens="1 2 3 4 5 6 7 8 9 10"
+        if [ ${run} == "run1" ] ; then wiens="1 2 3 4 5" ; fi
+        if [ ${run} == "run2" ] ; then wiens="6 7 8 9 10" ; fi
         for wien in $wiens ; do
 
             echo Run $run -- Target $target -- Wien $wien
 
-            mkdir -p ${data_quality_path}/run${run}/${target}/
-            pushd ${data_quality_path}/run${run}/${target}/
+            mkdir -p "${data_quality_path}/${run}/${target}/wien${wien}"
+            pushd "${data_quality_path}/${run}/${target}/wien${wien}"
 
-            # Check whether the database ROOT file exists
-            file="${db_rootfiles_path}/run${run}/parity/${target}_on_tree.root"
-            echo ${file}
-            if [ -e ${file} ] ; then
+            # Copy html template
+            sed -e "s|%wien%|$wien|g" -e "s|%target%|$target|g" -e "s|%run%|$run|g" \
+                $basedir/templates/wien.html \
+                >  "${data_quality_path}/${run}/${target}/wien${wien}/index.html"
 
-                # Loop over plots
-                for plot in $plots ; do
+            # Loop over plots
+            for plot in $plots ; do
 
-                    $QWANALYSIS/Extensions/QwDBLib/${plots} "${file}" $wien
-                    #~/QwAnalysis/Extensions/QwDBLib/${plots} "${file}" $wien
+                mkdir -p $plots
+                pushd $plots
 
-                done # end of loop over plots
+                type=`dirname $plot`
+                macro=`basename $plot`
 
-            else 
-                echo "Did not find any rootfiles. This makes me a sad panda."
+                # Check whether the database ROOT file exists
+                for file in ${db_rootfiles_path}/${run}/*/${target}_${type}_tree.root ; do
+                    if [ -e "${file}" ] ; then
 
-            fi # end of if file exists
+                        echo "Analyzing ${file} with  $QWANALYSIS/Extensions/QwDBLib/${macro}"
+                        $QWANALYSIS/Extensions/QwDBLib/${macro} "${file}" "${wien}"
+
+                    else
+                        echo "Did not find any rootfiles in ${db_rootfiles_path}/${run}/*/${target}_${type}_tree.root. This makes me a sad panda."
+
+                    fi # end of if file exists
+                done
+
+                popd
+
+            done # end of loop over plots
 
             popd
 
