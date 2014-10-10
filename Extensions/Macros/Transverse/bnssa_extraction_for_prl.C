7ae2308a968fd39e98e155a602a25cdb3e0babf0
buddhini <buddhini@b9700a98-9937-0410-b777-f318d6eb5bff>
2014-10-10 15:59:30 +0000

    Final commit of the script used to extract the BNSSA in elastic e+p scattering. The numbers here are final.
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@5926 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Macros/Transverse/bnssa_extraction_for_prl.C
+++ Extensions/Macros/Transverse/bnssa_extraction_for_prl.C
@@ -6,6 +6,11 @@ Coppied from bnssa_extraction.C script I used exactly an year ago to get the fin
 elastic e+p BNSSA asymmetry presented in my thesis (Qweak-doc-1886-v1)
 This time around, it will have a better non-linearity error estimate based on the analysis presented i
 Qweak-doc-2013-v7.
+
+09-03-2014 Buddhini - Added beamline background and neutral background asymmetries and dilutions.
+ updated the inleasitc dilution.
+
+09-29-2014 Buddhini - Added an error for <Q^2> binning.
 */
 
 
@@ -16,34 +21,54 @@ Qweak-doc-2013-v7.
 // pg. 157
 // Run II (needed for inelastic asymmetry extraction)
 const Double_t P_II  = 0.8750;
-const Double_t dP_II = 0.0079;
+const Double_t dP_II = 0.0078;
 // Run I and Run II (average)
-const Double_t P_b  = 0.8804;
-const Double_t dP_b = 0.0087;
+const Double_t P_b  = 0.8772;
+const Double_t dP_b = 0.0072;
 
 // Aluminum dilution
 // pg. 160
 const Double_t f_al      = 0.033;
 const Double_t df_al     = 0.002;
 
-// Elastic dilution at the inelastic peak (at 6700A)
-// pg. 162
-const Double_t f_e  = 0.669;
-const Double_t df_e = 0.067;
+// Elastic dilution at the inelastic peak (at QTOR = 6700A) from GEANT III
+// https://qweak.jlab.org/elog/Analysis+%26+Simulation/837
+const Double_t f_e  = 0.7010;
+const Double_t df_e = 0.0131;
 
-// Inelastic dilution at the elastic peak (8901A)
+// Inelastic dilution at the elastic peak (QTOR = 8901A)
 // pg. 162
 const Double_t f_ie  = 0.0002;
 const Double_t df_ie = 0.0001;
 
-// Detector bias and radiative correction
+// Beamline background dilution at the elastic peak (QTOR = 8901A)
+// From PRL 111, 141803 (2013)[Qweak comissioning paper]
+const Double_t f_bb  = 0.002;
+const Double_t df_bb = 0.001;
+
+// Soft neutral background dilution (qtro transport channel) at the elastic peak 
+//(QTOR = 8901A) from PRL 111, 141803 (2013)[Qweak comissioning paper]
+const Double_t f_qtor  = 0.002;
+const Double_t df_qtor = 0.002;
+
+// Detector bias, Q2 bias and radiative corrections
 // pg. 167
-const Double_t R  = 0.9979;
-const Double_t dR = 0.0032;
+const Double_t R_det  = 0.9942;
+const Double_t dR_det = 0.0008;
+
+const Double_t R_rad  = 1.010;
+const Double_t dR_rad = 0.003;
+
+const Double_t R_bin  = 1.0000;
+const Double_t dR_bin = 0.0004;
+
+const Double_t R_q2  = 1.000;
+const Double_t dR_q2 = 0.012;
+
 
 // Acceptance averaging correction factor
 // pg. 148
-const Double_t f_acc = 0.9938; // assign a 50% error
+const Double_t f_acc = 0.9938; // assign a 10% error
 // Non-linearity factors
 // elog 1166
 const Double_t f_lh2_nonlin = 0.003;
@@ -70,6 +95,16 @@ const Double_t dA_lh2_ie_meas      = 0.445; //ppm
 // pg. 268
 const Double_t A_al_ds_ie_meas     = 8.419; //ppm  
 const Double_t dA_al_ds_ie_meas    = 0.984; //ppm  
+
+// beamline neutral background
+// from my analysis report
+const Double_t A_bb     = 0; //ppm  
+const Double_t dA_bb    = 0.110; //ppm  
+
+// QTOR transport channel neutral  background
+const Double_t A_qtor     = 0; //ppm  
+const Double_t dA_qtor    = 5; //ppm  
+
 
 // Rough estimate for the elastic BNSSA. This is needed
 // for the inelastic BNSSA calculation
@@ -103,6 +138,10 @@ Double_t B_n_msr   = 0.0;
 
 Double_t dA_msr_ep_sys =  0.0;
 
+// Experimental bias
+Double_t R  = 0.0;
+Double_t dR = 0.0;
+
 /////////////////////////////////////////////
 // Main function to extrac elastic e+p BNSSA
 /////////////////////////////////////////////
@@ -111,17 +150,10 @@ void elastic_bnsa_extraction(){
 
   gDirectory->Clear();
 
-  // Double_t Bmsr = -4.835;//ppm 
-  // Double_t dBmsr_stat = 0.057; //ppm
-  // Double_t dBmsr_non_linear = 0.096; //ppm i.e. 4.805x0.02
-  // Double_t dBmsr_reg_scheme = 0.029; //ppm
-  // Double_t dBmsr_acc_avg = 0.015; //ppm
-
-
   std::cout<<"######################################################################"<<std::endl;
   std::cout<<"Elastic aluminum Background Correction "<<std::endl;
   std::cout<<"######################################################################"<<std::endl;
-  //No systematic correction for reg-scheme dependance.
+  //No systematic correction for reg-scheme dependance is applied.
   A = aluminum_asymmetry(A_msr_dsal,dA_msr_dsal_stat,f_al_nonlin,P_b,dP_b);
   A_al_ep = *(A);
   dA_al_ep = *(A+1);
@@ -152,18 +184,28 @@ void elastic_bnsa_extraction(){
   std::cout<<"Acceptance corrected measured LH2-cell asymmetry (ppm)= "<<A_msr_ep_acc <<std::endl; 
 
   // systematics for non-linearity and acceptance averaging
-  Double_t dA_msr_ep_acc    = (A_msr_ep-A_msr_ep_acc)*0.5; //50% error on acceptance correction
+  Double_t dA_msr_ep_acc    = (A_msr_ep-A_msr_ep_acc)*0.1; //10% error on acceptance correction
   Double_t dA_msr_ep_nonlin = (A_msr_ep*f_lh2_nonlin);
   Double_t dA_msr_ep_sys    =  sqrt(pow(dA_msr_ep_nonlin,2) + pow(dA_reg,2) + pow(dA_msr_ep_acc,2));
 
-  A = two_bkg_correction(P_b, dP_b,
-			 A_msr_ep_acc, dA_msr_ep_stat,
-			 dA_reg,  dA_msr_ep_acc, dA_msr_ep_nonlin,
-			 A_al_ep, dA_al_ep,
-			 f_al, df_al,
-			 A_lh2_ie, dA_lh2_ie,
-			 f_ie, df_ie,
-			 R, dR);
+ 
+  // experimental bias
+  calculate_experimental_bias(R_det, dR_det, R_rad, dR_rad, R_bin, dR_bin, R_q2, dR_q2);
+  std::cout<<"Experimental bias ="<<R<<" +- "<<dR<<std::endl;
+
+  A = four_bkg_correction(P_b, dP_b,
+			  A_msr_ep_acc, dA_msr_ep_stat,
+			  dA_reg,  dA_msr_ep_acc, dA_msr_ep_nonlin,
+			  A_al_ep, dA_al_ep, // aluminum
+			  f_al, df_al,
+			  A_lh2_ie, dA_lh2_ie, // inelastics
+			  f_ie, df_ie,
+			  A_bb, dA_bb, // beamline neutrals
+			  f_bb, df_bb,
+			  A_qtor, dA_qtor, // qtor transport channel neutrals
+			  f_qtor, df_qtor,
+			  R, dR);
+
 
 
 }
@@ -198,7 +240,7 @@ Double_t * aluminum_asymmetry(Double_t A_ds4al_reg, Double_t dA_ds4al_stat,
   // Correct for upstream downstream acceptance difference
   Double_t A_al_phys = ( A_usal +  A_ds4al_acc)/2;
   // apply a 50% error on the correction
-  Double_t dA_us_ds_acceptance = (A_al_phys- A_ds4al_acc)*0.5; 
+  Double_t dA_us_ds_acceptance = (A_al_phys- A_ds4al_acc)*0.1; //10% error on the acceptance correction 
 
 
   Double_t dA_al_phys = sqrt(pow(dA_ds4al_tot,2)+pow(dA_us_ds_acceptance,2));
@@ -230,7 +272,7 @@ void inelastic_asymmetry(){
 
   // systematics for non-linearity and acceptance averaging
   Double_t dA_lh2_ie_nonlin = (A_lh2_ie_meas*f_lh2_nonlin);
-  Double_t dA_lh2_ie_acc = (A_lh2_ie_meas-A_lh2_ie_acc)*0.5; //50% error on correction
+  Double_t dA_lh2_ie_acc = (A_lh2_ie_meas-A_lh2_ie_acc)*0.1; //10% error on correction
   Double_t dA_lh2_ie_sys = sqrt(pow(dA_lh2_ie_nonlin,2)+pow(dA_lh2_ie_acc,2));
   dA_lh2_ie = sqrt(pow(dA_lh2_ie_meas,2)+pow(dA_lh2_ie_sys,2));
 
@@ -346,9 +388,10 @@ Double_t * two_bkg_correction(Double_t P_beam,Double_t dP_beam,
   std::cout<<"partial_RQ dRQ = "<<partial_RQ<<std::endl;
 
   Double_t  dA_c_sys = sqrt( pow(partial_P,2)+pow(partial_RQ,2)
-			     + pow(partial_A_p_sys1,2)+ pow(partial_A_p_sys2,2)+ pow(partial_A_p_sys3,2)
+			     + pow(partial_A_p_sys1,2)+ pow(partial_A_p_sys2,2)
+			     + pow(partial_A_p_sys3,2)
 			     + pow(partial_A_b1,2)+pow(partial_A_b2,2)
-			     +pow(partial_fb1,2)+pow(partial_fb2,2));
+			     + pow(partial_fb1,2)+pow(partial_fb2,2));
 
   Double_t dA_c = sqrt( pow(partial_A_p_stat,2)+ pow(dA_c_sys,2));
 
@@ -399,3 +442,176 @@ Double_t * two_bkg_correction(Double_t P_beam,Double_t dP_beam,
 
 }
 
+/**************************************************
+ Apply a four background correction
+ Errors are seperated into stat, sys1(from reg), 
+ sys2(from acceptance) and sys3 (from non-linearity)
+***************************************************/
+Double_t * four_bkg_correction(Double_t P_beam,Double_t dP_beam,  
+			       Double_t A_p,Double_t dA_p_stat, 
+			       Double_t dA_p_sys1, Double_t dA_p_sys2, Double_t dA_p_sys3,
+			       Double_t A_b1, Double_t dA_b1, //aluminum
+			       Double_t fb1,Double_t dfb1,
+			       Double_t A_b2, Double_t dA_b2, //inelastics
+			       Double_t fb2, Double_t dfb2,
+			       Double_t A_b3, Double_t dA_b3, //beamline 
+			       Double_t fb3,Double_t dfb3,
+			       Double_t A_b4, Double_t dA_b4, // soft neutrals
+			       Double_t fb4, Double_t dfb4,
+			       Double_t RQ, Double_t dRQ){
+  
+  // beam polarization of the physics measurement
+  std::cout<<"P_beam = "<<P_beam<<"+-"<<dP_beam<<std::endl;
+
+  // measured physics asymmetry(after regression, phi acceptance and non-linearity corrections)
+  // errors are seperated into stat, sys1(from reg), sys2(from acceptance) and sys3 (from non-linearity)
+  std::cout<<"A_p  = "<<A_p<<" +- "<<dA_p_stat<<" +- "<<dA_p_sys1<<"+-"<<dA_p_sys2<<"+-"<<dA_p_sys3<<std::endl;
+
+  // backgrounds
+  std::cout<<"A_b1 = "<<A_b1<<" +- "<<dA_b1<<std::endl;
+  std::cout<<"f_b1 = "<<fb1<<" +- "<<dfb1<<std::endl;
+  std::cout<<"A_b2 = "<<A_b2<<" +- "<<dA_b2<<std::endl;
+  std::cout<<"f_b2 = "<<fb2<<" +- "<<dfb2<<std::endl;
+  std::cout<<"A_b3 = "<<A_b3<<" +- "<<dA_b3<<std::endl;
+  std::cout<<"f_b3 = "<<fb3<<" +- "<<dfb3<<std::endl;
+  std::cout<<"A_b4 = "<<A_b4<<" +- "<<dA_b4<<std::endl;
+  std::cout<<"f_b4 = "<<fb4<<" +- "<<dfb4<<std::endl;
+
+  // kinematics+detector bias
+  std::cout<<"R   = "<<RQ<<" +- "<<dRQ<<std::endl;
+
+  static Double_t  param[2];
+
+  Double_t A_corrected = RQ*(((A_p/P_beam)-A_b1*fb1-A_b2*fb2-A_b3*fb3-A_b4*fb4)/(1-fb1-fb2-fb3-fb4));
+  std::cout<<"Polarization and background corrected asymmetry (ppm) = "<<A_corrected<<std::endl;
+
+  Double_t denom = (1-fb1-fb2-fb3-fb4);
+  std::cout<<"(1-f1-f2-f3-f4) ="<<denom<<std::endl;
+
+  Double_t partial_P = (-(A_p*RQ)/(pow(P_beam,2)*denom))*dP_beam;
+  std::cout<<"partial_P dP = "<<partial_P<<std::endl;
+
+  Double_t partial_A_p_stat = (RQ/(P_beam*denom))*dA_p_stat;
+  std::cout<<"partial_A_p_stat dA_p_stat = "<<partial_A_p_stat<<std::endl;
+
+  Double_t partial_A_p_sys1 = (RQ/(P_beam*denom))*dA_p_sys1;
+  std::cout<<"partial_A_p_sys1 dA_p_sys1 = "<<partial_A_p_sys1<<std::endl;
+  
+  Double_t partial_A_p_sys2 = (RQ/(P_beam*denom))*dA_p_sys2;
+  std::cout<<"partial_A_p_sys2 dA_p_sys2 = "<<partial_A_p_sys2<<std::endl;
+  
+  Double_t partial_A_p_sys3 = (RQ/(P_beam*denom))*dA_p_sys3;
+  std::cout<<"partial_A_p_sys3 dA_p_sys3 = "<<partial_A_p_sys3<<std::endl;
+
+  Double_t partial_A_b1 = ((-fb1*RQ)/denom)*dA_b1;
+  std::cout<<"partial_A_b1 dAb1 = "<<partial_A_b1<<std::endl;
+
+  Double_t partial_A_b2 = ((-fb2*RQ)/denom)*dA_b2;
+  std::cout<<"partial_A_b2 dAb2 = "<<partial_A_b2<<std::endl;
+
+  Double_t partial_A_b3 = ((-fb3*RQ)/denom)*dA_b3;
+  std::cout<<"partial_A_b3 dAb3 = "<<partial_A_b3<<std::endl;
+  
+  Double_t partial_A_b4 = ((-fb4*RQ)/denom)*dA_b4;
+  std::cout<<"partial_A_b4 dAb4 = "<<partial_A_b4<<std::endl;
+
+  Double_t partial_fb1 = (RQ*((A_p/P_beam)+(-1+fb2+fb3+fb4)*A_b1-fb2*A_b2-fb3*A_b3-fb4*A_b4)/pow(denom,2))*dfb1;
+  std::cout<<"partial_fb1 dfb1 = "<<partial_fb1<<std::endl;
+
+  Double_t partial_fb2 = (RQ*((A_p/P_beam)+(-1+fb1+fb3+fb4)*A_b2-fb1*A_b1-fb3*A_b3-fb4*A_b4)/pow(denom,2))*dfb2;
+  std::cout<<"partial_fb2 dfb2 = "<<partial_fb2<<std::endl;
+
+  Double_t partial_fb3 = (RQ*((A_p/P_beam)+(-1+fb1+fb2+fb4)*A_b3-fb1*A_b1-fb2*A_b2-fb4*A_b4)/pow(denom,2))*dfb3;
+  std::cout<<"partial_fb3 dfb3 = "<<partial_fb3<<std::endl;
+
+  Double_t partial_fb4 = (RQ*((A_p/P_beam)+(-1+fb1+fb2+fb3)*A_b4-fb1*A_b1-fb2*A_b2-fb3*A_b3)/pow(denom,2))*dfb4;
+  std::cout<<"partial_fb4 dfb4 = "<<partial_fb4<<std::endl;
+
+
+  Double_t partial_RQ = (A_p/RQ)*dRQ;
+  std::cout<<"partial_R dR = "<<partial_RQ<<std::endl;
+
+  Double_t  dA_c_sys = sqrt( pow(partial_P,2)+pow(partial_RQ,2)
+			     + pow(partial_A_p_sys1,2)+ pow(partial_A_p_sys2,2)
+			     + pow(partial_A_p_sys3,2)
+			     + pow(partial_A_b1,2)+pow(partial_A_b2,2)
+			     + pow(partial_A_b3,2)+pow(partial_A_b4,2)
+			     + pow(partial_fb1,2)+pow(partial_fb2,2)
+			     + pow(partial_fb3,2)+pow(partial_fb4,2));
+
+  Double_t dA_c = sqrt( pow(partial_A_p_stat,2)+ pow(dA_c_sys,2));
+
+  std::cout<<"\tStat error = "<<partial_A_p_stat<<std::endl;
+  std::cout<<"\tSys error = "<<dA_c_sys<<std::endl;
+  std::cout<<"\tTotal Error = "<<dA_c<<std::endl;
+
+  std::cout<<"\n###################################"<<std::endl;
+  std::cout<<" Relative Errors % "<<std::endl;
+  std::cout<<"###################################"<<std::endl;
+  std::cout<<"Statistics = "<< TMath::Abs(partial_A_p_stat/A_corrected)*100<<std::endl;
+  std::cout<<"Polarization = "<< TMath::Abs(partial_P/A_corrected)*100<<std::endl;
+  std::cout<<"System non-linearity = "<<TMath::Abs(partial_A_p_sys3/A_corrected)*100<<std::endl;
+  std::cout<<"Reg. scheme dependance = "<<TMath::Abs(partial_A_p_sys1/A_corrected)*100<<std::endl;
+  std::cout<<"Experimental bias = "<< TMath::Abs(partial_RQ/A_corrected)*100<<std::endl;
+  std::cout<<"Acceptance average = "<< TMath::Abs(partial_A_p_sys2/A_corrected)*100<<std::endl;
+  std::cout<<"Aluminum = "<< TMath::Abs(partial_A_b1/A_corrected)*100<<std::endl;
+  std::cout<<"Aluminum dilution = "<< TMath::Abs(partial_fb1/A_corrected)*100<<std::endl;
+  std::cout<<"Inelastic dilution= "<< TMath::Abs(partial_fb2/A_corrected)*100<<std::endl;
+  std::cout<<"Inelastic = "<< TMath::Abs(partial_A_b2/A_corrected)*100<<std::endl;
+  std::cout<<"Beamline background dilution= "<< TMath::Abs(partial_fb3/A_corrected)*100<<std::endl;
+  std::cout<<"Beamline background = "<< TMath::Abs(partial_A_b3/A_corrected)*100<<std::endl;
+  std::cout<<"Soft neutral background dilution= "<< TMath::Abs(partial_fb4/A_corrected)*100<<std::endl;
+  std::cout<<"Soft neutral background = "<< TMath::Abs(partial_A_b4/A_corrected)*100<<std::endl;
+
+
+  
+  std::cout<<"\n###################################"<<std::endl;
+  std::cout<<" Individual corrections (ppm) "<<std::endl;
+  std::cout<<"###################################"<<std::endl;
+
+  Double_t delta_A_polar = (RQ/denom)*((A_p/P_beam)-A_p);
+  std::cout<<" Beam polarization correction delta_A_polar = "
+	   <<delta_A_polar<<std::endl;
+
+  Double_t delta_A_b1 = (RQ/denom)*(-fb1*A_b1);
+  std::cout<<" Aluminum background correction delta_A_b1 = "
+	   <<delta_A_b1<<std::endl;
+
+  Double_t delta_A_b2 = (RQ/denom)*(-fb2*A_b2);
+  std::cout<<" Inelastic background correction delta_A_b2 = "
+	   <<delta_A_b2<<std::endl;
+
+  Double_t delta_A_b3 = (RQ/denom)*(-fb3*A_b3);
+  std::cout<<" Beamline background correction delta_A_b3 = "
+	   <<delta_A_b3<<std::endl;
+
+  Double_t delta_A_b4 = (RQ/denom)*(-fb4*A_b4);
+  std::cout<<" Soft neutral background correction delta_A_b4 = "
+	   <<delta_A_b4<<std::endl;
+
+  
+  Double_t delta_A_Q = ((RQ*A_p)/denom)-A_p;
+  std::cout<<" Experimental bias correction = "
+	   <<delta_A_Q<<std::endl;
+
+  param[0] = A_corrected;
+  param[1] = dA_c;
+
+  return param;
+
+}
+
+/**************************************************
+Calculate the experimental bias factor R = R_det * R_rad* R_q2
+***************************************************/
+
+void calculate_experimental_bias(Double_t r_det, Double_t dr_det,
+				 Double_t r_rad, Double_t dr_rad,
+				 Double_t r_bin, Double_t dr_bin,
+				 Double_t r_q2, Double_t dr_q2){
+
+
+  R = r_det * r_rad * r_bin * r_q2;
+  dR = sqrt(pow(r_det*r_rad*r_bin*dr_q2,2)+pow(r_det*r_rad*r_q2*dr_bin,2)+pow(r_det*r_bin*r_q2*dr_rad,2)+pow(r_rad*r_bin*r_q2*dr_det,2));
+
+}
