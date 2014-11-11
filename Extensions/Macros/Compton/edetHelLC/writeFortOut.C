7996c96edfbc628d699c88730e04eb8afb4a4e6a
narayan <narayan@b9700a98-9937-0410-b777-f318d6eb5bff>
2014-11-11 17:50:02 +0000

    the format of file output changed as: nCycle, strip, cnts(all 4), charge(all 4), time(Laser on and off), lasPowOn, lasPowOff
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@5981 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Macros/Compton/edetHelLC/writeFortOut.C
+++ Extensions/Macros/Compton/edetHelLC/writeFortOut.C
@@ -30,7 +30,7 @@ Int_t writeFortOut(TString file, Int_t nCycle,
     ////in the foramt requested by G
     for (Int_t s =startStrip; s <endStrip;s++) {
       //fOut<<"nCycle\t strip\t cntB1H1L1\t cntB1H1L0\t cntB1H0L1\t cntB1H0L0\t qH1L1\t qH1L0\t qH0L1\t qH0L0\t timeB1L1\t timeB1L0\n";
-      fOut<<Form("%3d\t%2d\t%10.1f\t%10.1f\t%10.1f\t%10.1f\t%10.2f\t%10.2f\t%10.2f\t%10.2f\t%10.1f\t%10.1f\t%.2f\t%.2f\n", nCycle+1, s+1, cntsB1H1L1[s], cntsB1H1L0[s], cntsB1H0L1[s], cntsB1H0L0[s], qH1L1, qH1L0, qH0L1, qH0L0, nHelB1L1/helRate, nHelB1L0/helRate, lasPowB1L1, lasPowB1L0);
+      fOut<<Form("%3d\t%2d\t%10.1f\t%10.1f\t%10.1f\t%10.1f\t%7.2f\t%7.2f\t%7.2f\t%7.2f\t%5.1f\t%5.1f\t%.2f\t%.2f\n", nCycle+1, s+1, cntsB1H1L1[s], cntsB1H1L0[s], cntsB1H0L1[s], cntsB1H0L0[s], qH1L1, qH1L0, qH0L1, qH0L0, nHelB1L1/helRate, nHelB1L0/helRate, lasPowB1L1, lasPowB1L0);
     }
     fOut.close();
   } else cout<<"Couldn't write to file"<<file<<endl;    
