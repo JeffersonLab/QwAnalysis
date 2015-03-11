f261f975ffe45c609d1931ecb417b7bff850d351
wdconinc <wdconinc@b9700a98-9937-0410-b777-f318d6eb5bff>
2015-03-11 01:03:44 +0000

    Improvements to index.html script for tracking macros.  Now it won't continuously step on itself while rewriting the index file...
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@6116 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Tracking/macros/utils/format_front_page.sh
+++ Extensions/Tracking/macros/utils/format_front_page.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 ################################################################################
 ## filename:      format_front_page.sh
 ## Summary:       This script formats the front Tracking Summary webpage. It
@@ -8,6 +9,7 @@
 ##                website rebuilds itself from that.
 ##
 ################################################################################
+
 ## Simple idealistic way to get parameters
 SQLITE=$1
 DBFILE=$2
@@ -15,72 +17,56 @@ RUNNUM=$3
 WEBPAGE=$4
 FIRST100K=$5
 DETECTOR=$6
+
 ################################################################################
+
 ## Configurations
 BASEDIR=${QWANALYSIS}/Extensions/Tracking/macros
+
 RUNLIST=${WEBPAGE}/_runlist_frontpage
 RUNLISTTEMP=`mktemp ${RUNLIST}.XXXXXX`
+
+INDEX=${WEBPAGE}/index.html
+INDEXTEMP=`mktemp ${INDEX}.XXXXXX`
+
 ################################################################################
+
 ## Add all existing website directories to run list
 for run in ${WEBPAGE}/run_* ; do
    basename ${run} | cut -d_ -f2
    ## Sort the list (because it loves to get unsorted)
-done | sort -nru > ${RUNLISTTEMP}
+done | sort -nru > ${RUNLISTTEMP}.list
 
 ## Move is more 'atomic' than long output redirects
-mv ${RUNLISTTEMP} ${RUNLIST}
+mv ${RUNLISTTEMP}.list ${RUNLIST}.list
+
+################################################################################
 
 ## Prepare the website
-echo "<!-- Runs -->" > ${WEBPAGE}/_front_page_runs.html
-for line in `cat ${RUNLIST}`
+echo "<!-- Runs -->" > ${RUNLISTTEMP}
+for line in `cat ${RUNLIST}.list`
 do
    run=`echo ${line} | awk -F. '/[0-9]/ {print $1}'`
-   first100=`echo ${line} | awk -F. '{print $2}'`
-   if [ "x${first100}" == "x" ] ; then
-      first100="no"
-   fi
-   #echo "Run: ${run}   First100K?: ${first100}"
-   ${BASEDIR}/utils/format_run_link.sh ${SQLITE} ${DBFILE} ${run} ${first100} ${DETECTOR} >> ${WEBPAGE}/_front_page_runs.html
+   ${BASEDIR}/utils/format_run_link.sh ${SQLITE} ${DBFILE} ${run} ${first100} ${DETECTOR} >> ${RUNLISTTEMP}
 done
 
+## Move to official location
+mv ${RUNLISTTEMP} ${WEBPAGE}/_front_page_runs.html
+
+################################################################################
+
 # Create the index.html file
-cat ${BASEDIR}/templates/header > ${WEBPAGE}/index.html
-echo "<table class=\"runlist_table\">" >> ${WEBPAGE}/index.html
+cat ${BASEDIR}/templates/header > ${INDEXTEMP}
+echo "<table class=\"runlist_table\">" >> ${INDEXTEMP}
 
 ## Copy over the front page runs to the front page
-cat ${WEBPAGE}/_front_page_runs.html >> ${WEBPAGE}/index.html
-echo "</table>" >> ${WEBPAGE}/index.html
+cat ${WEBPAGE}/_front_page_runs.html >> ${INDEXTEMP}
+echo "</table>" >> ${INDEXTEMP}
 
 ## Copy the footer
-cat ${BASEDIR}/templates/footer >> ${WEBPAGE}/index.html
-###############################################################################
-## 
+cat ${BASEDIR}/templates/footer >> ${INDEXTEMP}
 
-exit -1;
-
-# Delete from the first_100k list
-${WEBPAGE}/_runlist_first100k_frontpage
-
-## Store this run into the list of runs.
-echo $
-## Update the front page with this run
-## 
-## Update the website adding this run
-## Copy over the headers
-cat templates/header > ${WEBPAGE}/index.html
-echo "<table class=\"runlist_table\">" >> ${WEBPAGE}/index.html
-${BASEDIR}/utils/format_run_link.sh $SQLITE $DBFILE $RUNNUM ${DETECTOR} > ${WEBPAGE}/_front_page_runs_new.html
-
-## Copy over the old front page runs to this new page
-cat ${WEBPAGE}/_front_page_runs.html >> ${WEBPAGE}/_front_page_runs_new.html
-
-## Now make it the new front page runs
-mv ${WEBPAGE}/_front_page_runs_new.html ${WEBPAGE}/_front_page_runs.html
-
-## Copy over the front page runs to the front page
-cat ${WEBPAGE}/_front_page_runs.html >> ${WEBPAGE}/index.html
-echo "</table>" >> ${WEBPAGE}/index.html
-
-## Copy the footer
-cat templates/footer >> ${WEBPAGE}/index.html
-
+## Move to official location and make world readable, group writable, directory sticky
+mv ${INDEXTEMP} ${INDEX}
+chmod a+r ${INDEX}
+chmod g+w ${INDEX}
