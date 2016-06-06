4d8e87b5c103729ce923ce38809a2d08547d3977
wdconinc <wdconinc@b9700a98-9937-0410-b777-f318d6eb5bff>
2016-06-06 18:48:04 +0000

    Changes to how run numbers are assigned for simulated root files: - run number is automatically generated based on md5sum for new files (hopefully without collisions) - you can change the run number to something else in a shared file (location to be determined, but probably web dir) - you can add a customized description. in the absence of this, the filename is massaged into a useful format.
    git-svn-id: https://qweaksvn.jlab.org/repos/QwAnalysis/trunk@6248 b9700a98-9937-0410-b777-f318d6eb5bff

--- Extensions/Tracking/macros/simrun.sh
+++ Extensions/Tracking/macros/simrun.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
-let run=10
-rm ${QW_ROOTFILES}/Qweak_[0-9][0-9].root
-for i in /volatile/hallc/qweak/wdconinc/scratch/rootfiles/myTrackingRun_*_tracking.root ; do
-  ln -sf $i ${QW_ROOTFILES}
-  f=`basename $i _tracking.root`
-  echo "keyword=Run $run: ${f//_/ }"
-  ln -sf $i ${QW_ROOTFILES}/Qweak_$run.root
-  let run=$run+1
+masterlist=${QWSCRATCH}/work/simrun.list
+touch $masterlist
+for file in /volatile/hallc/qweak/wdconinc/scratch/rootfiles/myTrackingRun_*_tracking.root ; do
+  md5=`echo $file | md5sum | awk '{print$1}' | tr 'abcdef' 'ABCDEF'`
+
+  # Get run number
+  run=`grep $md5 $masterlist | awk '{print$1}'`
+  # If the run wasn't listed yet
+  if [ -z $run ] ; then
+    # Hopefully we don't get collisions in 32k runs
+    run=`echo "ibase=16; $md5 % 8000" | bc`
+    echo -e "$run\t$md5\t$file" >> $masterlist
+  fi
+
+  # Get description
+  desc=`grep $md5 $masterlist | awk '{print$4}'`
+  # If the info wasn't listed yet
+  if [ -z $inf ] ; then
+    f=`basename $file _tracking.root`
+    info="keyword=Run $run: ${f//_/ }"
+  else
+    info="keyword=Run $run: $desc"
+  fi
+  # Print information line
+  echo $info
+
+  # Proceed to make the requested link
+  ln -sf $file ${QW_ROOTFILES}
+  ln -sf $file ${QW_ROOTFILES}/Qweak_$run.root
+
 done > ${QWSCRATCH}/work/simrun.txt
